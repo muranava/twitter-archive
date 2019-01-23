f65bdb812aeb770c2ec65ea42a1101b8d8dc0df3
muranava <muraleeatjena@yahoo.co.uk>
2019-01-23 02:14:37 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17855,
-	"created_at": "Tue Jan 22 01:14:35 +0000 2019",
+	"tweets": 17857,
+	"created_at": "Wed Jan 23 01:14:36 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,257 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 22 16:54:23 +0000 2019",
+		"id": 1087755366815928300,
+		"id_str": "1087755366815928320",
+		"full_text": "How is it that #iatefl has a magazine called 'Voices' - yet denies #NNEST teachers and #ELT workers their own SIG? How does that work? #iateflvoices #tefl #tesol Perhaps try The ELT Worker instead ... https://t.co/B0UfpKzTOh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			224
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "iatefl",
+					"indices": [
+						15,
+						22
+					]
+				},
+				{
+					"text": "NNEST",
+					"indices": [
+						67,
+						73
+					]
+				},
+				{
+					"text": "ELT",
+					"indices": [
+						87,
+						91
+					]
+				},
+				{
+					"text": "iateflvoices",
+					"indices": [
+						135,
+						148
+					]
+				},
+				{
+					"text": "tefl",
+					"indices": [
+						149,
+						154
+					]
+				},
+				{
+					"text": "tesol",
+					"indices": [
+						155,
+						161
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/B0UfpKzTOh",
+					"expanded_url": "https://notesfrombelow.org/article/elt-worker-issue-2",
+					"display_url": "notesfrombelow.org/article/elt-wo…",
+					"indices": [
+						201,
+						224
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1069661253969293300,
+			"id_str": "1069661253969293312",
+			"name": "theeltworker",
+			"screen_name": "theeltworker",
+			"location": "Ireland",
+			"description": "tired of this crap...",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 48,
+			"friends_count": 133,
+			"listed_count": 0,
+			"created_at": "Mon Dec 03 18:34:51 +0000 2018",
+			"favourites_count": 18,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 38,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1069680211426508800/JfuVynv4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1069680211426508800/JfuVynv4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1069661253969293312/1543866777",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "How is it that #iatefl has a magazine called 'Voices' - yet denies #NNEST teachers and #ELT workers their own SIG? How does that work? #iateflvoices #tefl #tesol Perhaps try The ELT Worker instead ... https://t.co/B0UfpKzTOh"
+	},
+	{
+		"created_at": "Mon Jan 21 17:07:36 +0000 2019",
+		"id": 1087396304228151300,
+		"id_str": "1087396304228151296",
+		"full_text": "Five steps to leave ELT https://t.co/eOyjyCIxIc https://t.co/wFH6blDtXh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eOyjyCIxIc",
+					"expanded_url": "http://www.youtube.com/watch?v=FYH8DsU2WCk",
+					"display_url": "youtube.com/watch?v=FYH8Ds…",
+					"indices": [
+						24,
+						47
+					]
+				},
+				{
+					"url": "https://t.co/wFH6blDtXh",
+					"expanded_url": "http://www.teachersasworkers.org/five-steps-to-leave-elt/",
+					"display_url": "teachersasworkers.org/five-steps-to-…",
+					"indices": [
+						48,
+						71
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3152637711,
+			"id_str": "3152637711",
+			"name": "teachers_as_workers",
+			"screen_name": "taw_sig",
+			"location": "everywhere",
+			"description": "The mind is not a vessel that needs filling, but wood that needs igniting.(Plutarch)",
+			"url": "https://t.co/m5fCLqrXgN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/m5fCLqrXgN",
+							"expanded_url": "http://www.teachersasworkers.org",
+							"display_url": "teachersasworkers.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1494,
+			"friends_count": 1067,
+			"listed_count": 22,
+			"created_at": "Thu Apr 09 20:23:00 +0000 2015",
+			"favourites_count": 274,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1489,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/586464982428069888/7tz5UtJi_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/586464982428069888/7tz5UtJi_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3152637711/1428611383",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Five steps to leave ELT https://t.co/eOyjyCIxIc https://t.co/wFH6blDtXh"
+	},
+	{
 		"created_at": "Mon Jan 21 17:07:36 +0000 2019",
 		"id": 1087396304228151300,
 		"id_str": "1087396304228151296",
