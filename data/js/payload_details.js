100ba0eff3b3463e53787e1f3bfd5976031dd3cd
muranava <muraleeatjena@yahoo.co.uk>
2019-04-08 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18044,
-	"created_at": "Sun Apr 07 01:14:36 +0000 2019",
+	"tweets": 18045,
+	"created_at": "Mon Apr 08 01:14:35 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sun Apr 07 21:24:05 +0000 2019",
+		"id": 1115002329420447700,
+		"id_str": "1115002329420447744",
+		"full_text": "@ClareSealy @DavidN_Cahn @Mr_PaintPots @juliangrenier @yrotitna @NatureNurtures_ yes &amp; the difference between \"some things\" and \"other things\" is a matter of extent not principle - the \"given a...environment\" in say writing are more stringent than \"given a...environment\" in speaking",
+		"truncated": false,
+		"display_text_range": [
+			81,
+			287
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ClareSealy",
+					"name": "Clare Sealy",
+					"id": 798004184,
+					"id_str": "798004184",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "DavidN_Cahn",
+					"name": "David Cahn",
+					"id": 793903916194885600,
+					"id_str": "793903916194885632",
+					"indices": [
+						12,
+						24
+					]
+				},
+				{
+					"screen_name": "Mr_PaintPots",
+					"name": "David Wright",
+					"id": 37700052,
+					"id_str": "37700052",
+					"indices": [
+						25,
+						38
+					]
+				},
+				{
+					"screen_name": "juliangrenier",
+					"name": "Julian Grenier",
+					"id": 2304548662,
+					"id_str": "2304548662",
+					"indices": [
+						39,
+						53
+					]
+				},
+				{
+					"screen_name": "yrotitna",
+					"name": "Yrotitna 🇪🇺Ferry Services Ltd 🇪🇺",
+					"id": 141721603,
+					"id_str": "141721603",
+					"indices": [
+						54,
+						63
+					]
+				},
+				{
+					"screen_name": "NatureNurtures_",
+					"name": "Nature Nurtures",
+					"id": 2829431944,
+					"id_str": "2829431944",
+					"indices": [
+						64,
+						80
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1114856421370282000,
+		"in_reply_to_status_id_str": "1114856421370281985",
+		"in_reply_to_user_id": 798004184,
+		"in_reply_to_user_id_str": "798004184",
+		"in_reply_to_screen_name": "ClareSealy",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1778,
+			"friends_count": 937,
+			"listed_count": 102,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 8207,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1700,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ClareSealy @DavidN_Cahn @Mr_PaintPots @juliangrenier @yrotitna @NatureNurtures_ yes &amp; the difference between \"some things\" and \"other things\" is a matter of extent not principle - the \"given a...environment\" in say writing are more stringent than \"given a...environment\" in speaking"
+	},
+	{
 		"created_at": "Sat Apr 06 13:41:06 +0000 2019",
 		"id": 1114174364889636900,
 		"id_str": "1114523425178095622",
