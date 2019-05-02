4aaf344029f4c25c0c940db1c124a0d6388e95c0
muranava <muraleeatjena@yahoo.co.uk>
2019-05-02 03:14:37 +0200

    Added 2 tweets

---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,319 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Wed May 01 19:54:09 +0000 2019",
+		"id": 1123641747798732800,
+		"id_str": "1123677005310984193",
+		"full_text": "Questioned by MPs in the House of Commons, UK Immigration Minister announces further delay to expected announcement on students who had their visas revoked after being accused of cheating in the Toeic English test https://t.co/SPcBlUHr9Q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			237
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SPcBlUHr9Q",
+					"expanded_url": "http://www.migrantvoice.org/home/pagenews/mps-question-immigration-minister-over-300419142346",
+					"display_url": "migrantvoice.org/home/pagenews/…",
+					"indices": [
+						214,
+						237
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 614186998,
+			"id_str": "614186998",
+			"name": "EL Gazette",
+			"screen_name": "ELGazette",
+			"location": "London",
+			"description": "News for the EFL and English medium education industries. Our Facebook: https://t.co/9A8W4ZDx4D",
+			"url": "http://t.co/J8tjrFSWRH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/J8tjrFSWRH",
+							"expanded_url": "http://www.elgazette.com",
+							"display_url": "elgazette.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/9A8W4ZDx4D",
+							"expanded_url": "https://goo.gl/5ygMqr",
+							"display_url": "goo.gl/5ygMqr",
+							"indices": [
+								72,
+								95
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 6888,
+			"friends_count": 5616,
+			"listed_count": 226,
+			"created_at": "Thu Jun 21 09:59:15 +0000 2012",
+			"favourites_count": 1343,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 20350,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B30000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/841278431098736641/AdArX5df_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/841278431098736641/AdArX5df_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/614186998/1489411446",
+			"profile_link_color": "B30000",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Questioned by MPs in the House of Commons, UK Immigration Minister announces further delay to expected announcement on students who had their visas revoked after being accused of cheating in the Toeic English test https://t.co/SPcBlUHr9Q"
+	},
+	{
+		"created_at": "Wed May 01 14:09:34 +0000 2019",
+		"id": 1123570674033942500,
+		"id_str": "1123590288629481472",
+		"full_text": "white women's tears endanger the lives and livelihoods of black women. \n\nlynsey sharpe came in 6th in rio, didn't have a piss icicle's chance in hell of winning dust let alone a medal but blamed her failure on caster semenya? https://t.co/1jF48HY56g",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			225
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123570254591012900,
+					"id_str": "1123570254591012864",
+					"indices": [
+						226,
+						249
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1123570254591012864/pu/img/Qy9rXD3A3TsybxG-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1123570254591012864/pu/img/Qy9rXD3A3TsybxG-.jpg",
+					"url": "https://t.co/1jF48HY56g",
+					"display_url": "pic.twitter.com/1jF48HY56g",
+					"expanded_url": "https://twitter.com/DanielleDASH/status/1123570674033942528/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 582,
+							"h": 318,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 582,
+							"h": 318,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 582,
+							"h": 318,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123570254591012900,
+					"id_str": "1123570254591012864",
+					"indices": [
+						226,
+						249
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1123570254591012864/pu/img/Qy9rXD3A3TsybxG-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1123570254591012864/pu/img/Qy9rXD3A3TsybxG-.jpg",
+					"url": "https://t.co/1jF48HY56g",
+					"display_url": "pic.twitter.com/1jF48HY56g",
+					"expanded_url": "https://twitter.com/DanielleDASH/status/1123570674033942528/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 582,
+							"h": 318,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 582,
+							"h": 318,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 582,
+							"h": 318,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							97,
+							53
+						],
+						"duration_millis": 64040,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1123570254591012864/pu/pl/hGNGIvfB0e-TmF_7.m3u8?tag=9"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1123570254591012864/pu/vid/328x180/YFHC5lDNCLI8MELP.mp4?tag=9"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1123570254591012864/pu/vid/582x318/PqbWciQDLqlsvzl-.mp4?tag=9"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 124248049,
+			"id_str": "124248049",
+			"name": "GIFLord DanielléDASH",
+			"screen_name": "DanielleDASH",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/Y50Cg7detw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Y50Cg7detw",
+							"expanded_url": "http://www.danielledash.com",
+							"display_url": "danielledash.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14608,
+			"friends_count": 795,
+			"listed_count": 334,
+			"created_at": "Thu Mar 18 19:22:36 +0000 2010",
+			"favourites_count": 37877,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 102196,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1123598703816859648/uBuRb5lX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123598703816859648/uBuRb5lX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/124248049/1555721317",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10492,
+		"favorite_count": 25240,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "white women's tears endanger the lives and livelihoods of black women. \n\nlynsey sharpe came in 6th in rio, didn't have a piss icicle's chance in hell of winning dust let alone a medal but blamed her failure on caster semenya? https://t.co/1jF48HY56g"
+	}
+]
