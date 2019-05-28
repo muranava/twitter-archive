8090ef8e207737b8a0c5bae3c730a1263d1b6c65
muranava <muraleeatjena@yahoo.co.uk>
2019-05-28 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18129,
-	"created_at": "Sun May 26 01:14:36 +0000 2019",
+	"tweets": 18130,
+	"created_at": "Tue May 28 01:14:36 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,155 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 27 20:05:11 +0000 2019",
+		"id": 1133073307799031800,
+		"id_str": "1133101865028055045",
+		"full_text": "#ELT worker 4 is out!\n\n- The Whistleblower\n- The Whiteboard Strikes Back! Interview w/ #TEFL Workers' Union\n- The adventures of TEFL Tony \n\nRetweet! @ELTAdvocacy @SLBCoop @tawsig @teflguild\n\nhttps://t.co/njnbMjgLXI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			214
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ELT",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "TEFL",
+					"indices": [
+						87,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ELTAdvocacy",
+					"name": "ELT ADVOCACY Ireland",
+					"id": 712775553821048800,
+					"id_str": "712775553821048832",
+					"indices": [
+						149,
+						161
+					]
+				},
+				{
+					"screen_name": "SLBCoop",
+					"name": "Serveis Lingüístics de Barcelona",
+					"id": 2365399801,
+					"id_str": "2365399801",
+					"indices": [
+						162,
+						170
+					]
+				},
+				{
+					"screen_name": "Tawsig",
+					"name": "Tawsig",
+					"id": 1546060946,
+					"id_str": "1546060946",
+					"indices": [
+						171,
+						178
+					]
+				},
+				{
+					"screen_name": "teflguild",
+					"name": "TEFL Guild",
+					"id": 732157129818316800,
+					"id_str": "732157129818316802",
+					"indices": [
+						179,
+						189
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/njnbMjgLXI",
+					"expanded_url": "https://notesfrombelow.org/article/elt-worker-issue-4",
+					"display_url": "notesfrombelow.org/article/elt-wo…",
+					"indices": [
+						191,
+						214
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1069661253969293300,
+			"id_str": "1069661253969293312",
+			"name": "theeltworker",
+			"screen_name": "theeltworker",
+			"location": "Ireland",
+			"description": "tired of this crap...",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 95,
+			"friends_count": 174,
+			"listed_count": 1,
+			"created_at": "Mon Dec 03 18:34:51 +0000 2018",
+			"favourites_count": 28,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 58,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1069680211426508800/JfuVynv4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1069680211426508800/JfuVynv4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1069661253969293312/1543866777",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#ELT worker 4 is out!\n\n- The Whistleblower\n- The Whiteboard Strikes Back! Interview w/ #TEFL Workers' Union\n- The adventures of TEFL Tony \n\nRetweet! @ELTAdvocacy @SLBCoop @tawsig @teflguild\n\nhttps://t.co/njnbMjgLXI"
+	},
+	{
 		"created_at": "Sat May 25 18:31:01 +0000 2019",
 		"id": 1132353393899577300,
 		"id_str": "1132353393899577345",
