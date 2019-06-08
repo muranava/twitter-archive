13b4bb37ced9712756c79de2b7fcbbf38bcabf36
muranava <muraleeatjena@yahoo.co.uk>
2019-06-08 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18145,
-	"created_at": "Wed Jun 05 01:14:36 +0000 2019",
+	"tweets": 18146,
+	"created_at": "Sat Jun 08 01:14:35 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 07 08:25:29 +0000 2019",
+		"id": 1136910755041857500,
+		"id_str": "1136912049223716864",
+		"full_text": "Milan Rai has long documented the UK’s postwar use of nuclear threats (something that cannot exist in the ‘mainstream’), most recently those towards Iraq. https://t.co/gpwWRmwcQt https://t.co/bqzKXQpuHX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gpwWRmwcQt",
+					"expanded_url": "https://bit.ly/2ZhOO53",
+					"display_url": "bit.ly/2ZhOO53",
+					"indices": [
+						155,
+						178
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1136910749664731100,
+					"id_str": "1136910749664731138",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/D8ceTZ8W4AIaPGx.png",
+					"media_url_https": "https://pbs.twimg.com/media/D8ceTZ8W4AIaPGx.png",
+					"url": "https://t.co/bqzKXQpuHX",
+					"display_url": "pic.twitter.com/bqzKXQpuHX",
+					"expanded_url": "https://twitter.com/markcurtis30/status/1136910755041857536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 622,
+							"h": 529,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 622,
+							"h": 529,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 622,
+							"h": 529,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1136910749664731100,
+					"id_str": "1136910749664731138",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/D8ceTZ8W4AIaPGx.png",
+					"media_url_https": "https://pbs.twimg.com/media/D8ceTZ8W4AIaPGx.png",
+					"url": "https://t.co/bqzKXQpuHX",
+					"display_url": "pic.twitter.com/bqzKXQpuHX",
+					"expanded_url": "https://twitter.com/markcurtis30/status/1136910755041857536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 622,
+							"h": 529,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 622,
+							"h": 529,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 622,
+							"h": 529,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2278053312,
+			"id_str": "2278053312",
+			"name": "Mark Curtis",
+			"screen_name": "markcurtis30",
+			"location": "Oxford, England",
+			"description": "Historian/analyst of UK foreign policy & int'l development. Books include Secret Affairs, Web of Deceit, Unpeople, Great Deception. Columnist, Middle East Eye.",
+			"url": "https://t.co/e5Jvcb9VCz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/e5Jvcb9VCz",
+							"expanded_url": "http://www.markcurtis.info",
+							"display_url": "markcurtis.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32094,
+			"friends_count": 727,
+			"listed_count": 438,
+			"created_at": "Sun Jan 05 20:16:24 +0000 2014",
+			"favourites_count": 11268,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 9933,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/916582367518756864/K2-D5R7A_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/916582367518756864/K2-D5R7A_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2278053312/1469811188",
+			"profile_link_color": "4A913C",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Milan Rai has long documented the UK’s postwar use of nuclear threats (something that cannot exist in the ‘mainstream’), most recently those towards Iraq. https://t.co/gpwWRmwcQt https://t.co/bqzKXQpuHX"
+	},
+	{
 		"created_at": "Tue Jun 04 16:28:38 +0000 2019",
 		"id": 1135681257440858100,
 		"id_str": "1135946473252839424",
