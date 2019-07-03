c55fcf0b9f82b561d85bc3aa6f65ee0d3364c21d
muranava <muraleeatjena@yahoo.co.uk>
2019-07-03 03:14:36 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18286,
-	"created_at": "Tue Jul 02 01:14:37 +0000 2019",
+	"tweets": 18288,
+	"created_at": "Wed Jul 03 01:14:35 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,347 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Tue Jul 02 12:09:48 +0000 2019",
+		"id": 1146025025755131900,
+		"id_str": "1146028194077843456",
+		"full_text": "“[Anti-capitalism is] the principled, analytical insistence that what isn’t Capitalism exists if and when the dichotomy employer-employee is removed.\n\nIt isn’t when workers have more income.\n\nIt isn’t when workers have more power.\n\nIt is when they are no longer ‘workers.’”\n\n(5/6) https://t.co/jmEB5Dnxuj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1146024893047300100,
+					"id_str": "1146024893047300096",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1146024893047300096/pu/img/5TGrrIlij_dPVd2V.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1146024893047300096/pu/img/5TGrrIlij_dPVd2V.jpg",
+					"url": "https://t.co/jmEB5Dnxuj",
+					"display_url": "pic.twitter.com/jmEB5Dnxuj",
+					"expanded_url": "https://twitter.com/BlackSocialists/status/1146025025755131904/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1146024893047300100,
+					"id_str": "1146024893047300096",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1146024893047300096/pu/img/5TGrrIlij_dPVd2V.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1146024893047300096/pu/img/5TGrrIlij_dPVd2V.jpg",
+					"url": "https://t.co/jmEB5Dnxuj",
+					"display_url": "pic.twitter.com/jmEB5Dnxuj",
+					"expanded_url": "https://twitter.com/BlackSocialists/status/1146025025755131904/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 130487,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1146024893047300096/pu/vid/480x270/RPM5jKgFWrqoFUjr.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1146024893047300096/pu/vid/640x360/X8s84RI1pF9zeuQ4.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1146024893047300096/pu/vid/1280x720/HiMXeVavpQFx02nG.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1146024893047300096/pu/pl/zURvhN6PvZGh7HXI.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1146020819006627800,
+		"in_reply_to_status_id_str": "1146020819006627841",
+		"in_reply_to_user_id": 942566438019231700,
+		"in_reply_to_user_id_str": "942566438019231744",
+		"in_reply_to_screen_name": "BlackSocialists",
+		"user": {
+			"id": 942566438019231700,
+			"id_str": "942566438019231744",
+			"name": "Black Socialists of America",
+			"screen_name": "BlackSocialists",
+			"location": "United States",
+			"description": "EST. 2017, we are a 501(c)(4) organization of anticapitalist Black Americans who believe in the core principles of Socialism as defined by Karl Marx.",
+			"url": "https://t.co/UFV401ejKe",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UFV401ejKe",
+							"expanded_url": "http://BlackSocialists.US",
+							"display_url": "BlackSocialists.US",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60960,
+			"friends_count": 7049,
+			"listed_count": 383,
+			"created_at": "Mon Dec 18 01:25:25 +0000 2017",
+			"favourites_count": 37922,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 19839,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1075041709715804160/v0Cb1jrv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1075041709715804160/v0Cb1jrv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/942566438019231744/1546149116",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 91,
+		"favorite_count": 259,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“[Anti-capitalism is] the principled, analytical insistence that what isn’t Capitalism exists if and when the dichotomy employer-employee is removed.\n\nIt isn’t when workers have more income.\n\nIt isn’t when workers have more power.\n\nIt is when they are no longer ‘workers.’”\n\n(5/6) https://t.co/jmEB5Dnxuj"
+	},
+	{
+		"created_at": "Tue Jul 02 10:56:44 +0000 2019",
+		"id": 1146009807712923600,
+		"id_str": "1146009807712923648",
+		"full_text": "@libcomorg apparently one famous case of using milkshakes against anti-fascists was CIA against Castro https://t.co/VVlz1bVgbz",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "libcomorg",
+					"name": "libcom dot org",
+					"id": 69545227,
+					"id_str": "69545227",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/VVlz1bVgbz",
+					"expanded_url": "https://www.reuters.com/article/us-cuba-cia/closest-cia-bid-to-kill-castro-was-poisoned-drink-idUSN0427935120070705",
+					"display_url": "reuters.com/article/us-cub…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1146000722296823800,
+		"in_reply_to_status_id_str": "1146000722296823809",
+		"in_reply_to_user_id": 69545227,
+		"in_reply_to_user_id_str": "69545227",
+		"in_reply_to_screen_name": "libcomorg",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1815,
+			"friends_count": 1033,
+			"listed_count": 102,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 8800,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1990,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@libcomorg apparently one famous case of using milkshakes against anti-fascists was CIA against Castro https://t.co/VVlz1bVgbz"
+	},
+	{
 		"created_at": "Mon Jul 01 20:52:54 +0000 2019",
 		"id": 1145797449556082700,
 		"id_str": "1145797449556082688",
