53c9de53a44bedf0c2a07f8af0614cb704a2e43c
muranava <muraleeatjena@yahoo.co.uk>
2019-07-25 03:14:37 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18319,
-	"created_at": "Sun Jul 21 01:14:35 +0000 2019",
+	"tweets": 18320,
+	"created_at": "Thu Jul 25 01:14:36 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Wed Jul 24 20:57:21 +0000 2019",
+		"id": 1153610796934533000,
+		"id_str": "1154133490599956480",
+		"full_text": "****** *PLEASE DISTRIBUTE* *****\n\nPublic Call Out: Support sacked English language teacher\n\n*What* : A trans-woman of colour has been sacked due to raising issues of racism and sexism at her language school.\n\n*When* : Today (23rd July)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			235
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1130578765472501800,
+			"id_str": "1130578765472501760",
+			"name": "TEFL Workers' Union",
+			"screen_name": "TeflUnion",
+			"location": "London",
+			"description": "We like our students. We like our jobs. We like our workmates. We don’t like struggling to pay the bills. We don’t like insecure employment. Join us!",
+			"url": "https://t.co/ngblfDEBVd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ngblfDEBVd",
+							"expanded_url": "https://www.facebook.com/teflworkers/",
+							"display_url": "facebook.com/teflworkers/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 256,
+			"friends_count": 29,
+			"listed_count": 2,
+			"created_at": "Mon May 20 20:59:17 +0000 2019",
+			"favourites_count": 16,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 62,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1130578765472501760/1558387728",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 118,
+		"favorite_count": 99,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "****** *PLEASE DISTRIBUTE* *****\n\nPublic Call Out: Support sacked English language teacher\n\n*What* : A trans-woman of colour has been sacked due to raising issues of racism and sexism at her language school.\n\n*When* : Today (23rd July)"
+	},
+	{
 		"created_at": "Sat Jul 20 12:38:50 +0000 2019",
 		"id": 1152558481049882600,
 		"id_str": "1152558481049882625",
@@ -163,7 +258,7 @@ Grailbird.data.tweets_2019_07 =
 				"name": "Parisa Mehran, PhD",
 				"screen_name": "ParisaMehran",
 				"location": "Osaka-fu, Japan",
-				"description": "Iranian #EFL 👩🏽‍🏫| Racial #Equity Advocate| Leading @WOCinELT together with #WOCinELT| @SietarKansaiJP Twitter Coord| Blogger @BydStereotypes",
+				"description": "Iranian #EFL 👩🏽🏫| Racial #Equity Advocate| Leading @WOCinELT together with #WOCinELT| @SietarKansaiJP Twitter Coord| Blogger @BydStereotypes",
 				"url": "https://t.co/n5GLjpenSJ",
 				"entities": {
 					"url": {
