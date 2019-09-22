e9d4de959e409b76b64175f85eff2d3a6f4000d2
muranava <muraleeatjena@yahoo.co.uk>
2019-09-22 03:14:37 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18462,
-	"created_at": "Sat Sep 21 01:14:36 +0000 2019",
+	"tweets": 18464,
+	"created_at": "Sun Sep 22 01:14:36 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sat Sep 21 08:36:14 +0000 2019",
+		"id": 1175126765347528700,
+		"id_str": "1175327868236251136",
+		"full_text": "I'm very pleased to announce the website for our Global English Education Project, a core piece of the SSHRC grant funded project with Jeff Bale and Eve Haque. https://t.co/CD8vP8N4gG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CD8vP8N4gG",
+					"expanded_url": "https://www.uwinnipeg.ca/global-english-education/index.html",
+					"display_url": "uwinnipeg.ca/global-english…",
+					"indices": [
+						160,
+						183
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2830990525,
+			"id_str": "2830990525",
+			"name": "Peter Ives",
+			"screen_name": "PeterRIves",
+			"location": "Winnipeg, Manitoba",
+			"description": "teaches politics & pol theory University of Winnipeg, scholarly work on Antonio Gramsci & 'global' English, tweets on politics, bikes, beer and other odds",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 389,
+			"friends_count": 287,
+			"listed_count": 11,
+			"created_at": "Thu Sep 25 02:32:15 +0000 2014",
+			"favourites_count": 1079,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 859,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/514967944657264640/FuKV5NRq_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/514967944657264640/FuKV5NRq_normal.jpeg",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 17,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'm very pleased to announce the website for our Global English Education Project, a core piece of the SSHRC grant funded project with Jeff Bale and Eve Haque. https://t.co/CD8vP8N4gG"
+	},
+	{
+		"created_at": "Sat Sep 21 08:28:32 +0000 2019",
+		"id": 1175113635171057700,
+		"id_str": "1175325929956134912",
+		"full_text": "Delfin English Language School Teachers to hold three-hour work stoppage in Dublin https://t.co/jxHOCKJg12",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jxHOCKJg12",
+					"expanded_url": "http://unitetheunionireland.org/2019/09/20/delfin-english-language-school-teachers-to-hold-three-hour-work-stoppage-in-dublin/",
+					"display_url": "unitetheunionireland.org/2019/09/20/del…",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 197063670,
+			"id_str": "197063670",
+			"name": "UNITE union Ireland",
+			"screen_name": "UniteunionROI",
+			"location": "Dublin City, Ireland",
+			"description": "Unite the Union is the biggest trade union in Britain and Ireland with 1.3 million members across all sectors.  RTs/Favs do not imply endorsements.",
+			"url": "http://t.co/IHGpQL0N9U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/IHGpQL0N9U",
+							"expanded_url": "http://www.unitetheunionireland.org",
+							"display_url": "unitetheunionireland.org",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3060,
+			"friends_count": 1232,
+			"listed_count": 49,
+			"created_at": "Thu Sep 30 15:27:10 +0000 2010",
+			"favourites_count": 56,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3543,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/704688399039537152/ajUHJXyS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/704688399039537152/ajUHJXyS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/197063670/1386878773",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Delfin English Language School Teachers to hold three-hour work stoppage in Dublin https://t.co/jxHOCKJg12"
+	},
+	{
 		"created_at": "Fri Sep 20 12:08:16 +0000 2019",
 		"id": 1175018836652908500,
 		"id_str": "1175018836652908544",
