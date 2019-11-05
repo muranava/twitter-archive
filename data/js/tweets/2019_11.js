71bc666d9eb946bd46fa59676607f6c85c1b9e43
muranava <muraleeatjena@yahoo.co.uk>
2019-11-05 02:14:37 +0100

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Mon Nov 04 18:56:12 +0000 2019",
+		"id": 1191399876996817000,
+		"id_str": "1191428953141075968",
+		"full_text": "Meet the Strikers!!! \nThis is Laurence.\nHe talks about his passion for his job and how the #tefl industry needs to adapt! \n#SupportDelfinTeachers https://t.co/lW8mVq6Ccb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tefl",
+					"indices": [
+						91,
+						96
+					]
+				},
+				{
+					"text": "SupportDelfinTeachers",
+					"indices": [
+						123,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1191399780217507800,
+					"id_str": "1191399780217507841",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1191399780217507841/pu/img/VhbWGXBWIosPfbrc.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1191399780217507841/pu/img/VhbWGXBWIosPfbrc.jpg",
+					"url": "https://t.co/lW8mVq6Ccb",
+					"display_url": "pic.twitter.com/lW8mVq6Ccb",
+					"expanded_url": "https://twitter.com/ELTuniondublin/status/1191399876996816897/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1191399780217507800,
+					"id_str": "1191399780217507841",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1191399780217507841/pu/img/VhbWGXBWIosPfbrc.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1191399780217507841/pu/img/VhbWGXBWIosPfbrc.jpg",
+					"url": "https://t.co/lW8mVq6Ccb",
+					"display_url": "pic.twitter.com/lW8mVq6Ccb",
+					"expanded_url": "https://twitter.com/ELTuniondublin/status/1191399876996816897/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 44983,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1191399780217507841/pu/vid/720x1280/UfKg5g0bP2Glis3p.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1191399780217507841/pu/pl/afnglMFzAyKJebI1.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1191399780217507841/pu/vid/360x640/NnmkKrQW3rN0Yf9s.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1191399780217507841/pu/vid/320x568/dyEJSfAYQsO2awxu.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1183334945713983500,
+			"id_str": "1183334945713983490",
+			"name": "ELTdublin",
+			"screen_name": "ELTuniondublin",
+			"location": "Dublin City, Ireland",
+			"description": "English Language Teachers on strike from Delfin English School. Tweet us! Greet us! And stand with us 29/10, 30/10 at 2 Parnell Square East",
+			"url": "https://t.co/yrJuWGrFQG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/yrJuWGrFQG",
+							"expanded_url": "http://instagram.com/dublineltunion",
+							"display_url": "instagram.com/dublineltunion",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 69,
+			"friends_count": 125,
+			"listed_count": 0,
+			"created_at": "Sun Oct 13 10:53:45 +0000 2019",
+			"favourites_count": 89,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 80,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1183336285190197248/F1UQky9d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1183336285190197248/F1UQky9d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1183334945713983490/1570964410",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Meet the Strikers!!! \nThis is Laurence.\nHe talks about his passion for his job and how the #tefl industry needs to adapt! \n#SupportDelfinTeachers https://t.co/lW8mVq6Ccb"
+	},
+	{
 		"created_at": "Sun Nov 03 21:46:52 +0000 2019",
 		"id": 1191108489130774500,
 		"id_str": "1191109513845387264",
