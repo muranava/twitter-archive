117b5b8e7d9997a6fbf94ba9d247de96cb59d75c
muranava <muraleeatjena@yahoo.co.uk>
2019-11-12 02:14:36 +0100

    Added 2 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,228 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Mon Nov 11 22:52:09 +0000 2019",
+		"id": 1194019443573248000,
+		"id_str": "1194025044915216384",
+		"full_text": "Boom!  Labour will fund mature students chance to retrain for a new career\n\n•SIX years free Undergraduate Uni Tuition\n•Maintenance Grants for living costs of poor\n•Free college education for mature students\n•6 yrs free education to do HNCs, Diplomas\n•*paid* time off to study",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 168090600,
+			"id_str": "168090600",
+			"name": "Tory Fibs",
+			"screen_name": "ToryFibs",
+			"location": "Belfast, Northern Ireland",
+			"description": "Labour. Grassroots Rebuttal Account. Socialist. Class based politics. NHS. Homelessness. Food Hunger. Low Pay. Childcare. Poverty. Peace. Tweets by Eoin. Irish",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 108944,
+			"friends_count": 17390,
+			"listed_count": 1140,
+			"created_at": "Sun Jul 18 10:32:37 +0000 2010",
+			"favourites_count": 56131,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63788,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "93F0A1",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/867400651617308672/xv8UxEBN_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/867400651617308672/xv8UxEBN_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/168090600/1496186488",
+			"profile_link_color": "F51B1B",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "6EC259",
+			"profile_text_color": "054009",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 432,
+		"favorite_count": 807,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Boom!  Labour will fund mature students chance to retrain for a new career\n\n•SIX years free Undergraduate Uni Tuition\n•Maintenance Grants for living costs of poor\n•Free college education for mature students\n•6 yrs free education to do HNCs, Diplomas\n•*paid* time off to study"
+	},
+	{
+		"created_at": "Sun Nov 10 18:54:42 +0000 2019",
+		"id": 1193602901492584400,
+		"id_str": "1193602901492584449",
+		"full_text": "A lost decade for LGBT issues inside ELT https://t.co/6vaAA6Jbr1 via @wordpressdotcom",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wordpressdotcom",
+					"name": "WordPress.com",
+					"id": 823905,
+					"id_str": "823905",
+					"indices": [
+						69,
+						85
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/6vaAA6Jbr1",
+					"expanded_url": "https://marxistelf.wordpress.com/2019/11/09/a-lost-decade-for-lgbt-issues-inside-elt/",
+					"display_url": "marxistelf.wordpress.com/2019/11/09/a-l…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1883,
+			"friends_count": 1179,
+			"listed_count": 103,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 9504,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2416,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A lost decade for LGBT issues inside ELT https://t.co/6vaAA6Jbr1 via @wordpressdotcom"
+	},
+	{
 		"created_at": "Thu Nov 07 19:50:25 +0000 2019",
 		"id": 1192529760938844200,
 		"id_str": "1192529760938844161",
