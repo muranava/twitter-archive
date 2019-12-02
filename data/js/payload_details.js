32b98ca1fe47dd06a68cd28e7d250e7d8326f9c4
muranava <muraleeatjena@yahoo.co.uk>
2019-12-02 02:14:37 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18688,
-	"created_at": "Sat Nov 30 01:14:36 +0000 2019",
+	"tweets": 18689,
+	"created_at": "Mon Dec 02 01:14:36 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,286 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 19:20:15 +0000 2019",
+		"id": 1201168095295033300,
+		"id_str": "1201219474872881152",
+		"full_text": "\"We try not to eat a lot in one day, even though most of us are really hungry.\"\n\nThis sentence should never be uttered by any child, let alone one living in the 5th richest country in the world. \n\nMake no mistake, the Tories are responsible for this.\n\nhttps://t.co/cOorZYZMeD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1200384589145006000,
+					"id_str": "1200384589145006080",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/EKifvXyW4AIBtYh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKifvXyW4AIBtYh.jpg",
+					"url": "https://t.co/cOorZYZMeD",
+					"display_url": "pic.twitter.com/cOorZYZMeD",
+					"expanded_url": "https://twitter.com/C4Dispatches/status/1200927491134820353/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1200927491134820400,
+					"source_status_id_str": "1200927491134820353",
+					"source_user_id": 78298637,
+					"source_user_id_str": "78298637",
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200384589145006000,
+					"id_str": "1200384589145006080",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/EKifvXyW4AIBtYh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKifvXyW4AIBtYh.jpg",
+					"url": "https://t.co/cOorZYZMeD",
+					"display_url": "pic.twitter.com/cOorZYZMeD",
+					"expanded_url": "https://twitter.com/C4Dispatches/status/1200927491134820353/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1200927491134820400,
+					"source_status_id_str": "1200927491134820353",
+					"source_user_id": 78298637,
+					"source_user_id_str": "78298637",
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 128560,
+						"variants": [
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1200384589145006080/vid/320x320/oo26Jjf1n1VXU1V3.mp4?tag=13"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1200384589145006080/vid/720x720/7ZEO6VA9Dq2aXl0a.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1200384589145006080/pl/icG1iFXlCqcei68v.m3u8?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1200384589145006080/vid/480x480/EsYsPLFmJGzaz4Cj.mp4?tag=13"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false,
+						"source_user": {
+							"id": 78298637,
+							"id_str": "78298637",
+							"name": "Channel 4 Dispatches",
+							"screen_name": "C4Dispatches",
+							"location": "UK",
+							"description": "Official account for @Channel4 award-winning current affairs series. Catchup on @All4 - also follow @Channel4News and @UnreportedWorld on Twitter, FB and YT",
+							"url": "https://t.co/9L0b6SGorJ",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/9L0b6SGorJ",
+											"expanded_url": "http://www.channel4.com/programmes/dispatches",
+											"display_url": "channel4.com/programmes/dis…",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 64567,
+							"friends_count": 7959,
+							"listed_count": 654,
+							"created_at": "Tue Sep 29 12:51:14 +0000 2009",
+							"favourites_count": 260,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": false,
+							"verified": true,
+							"statuses_count": 4167,
+							"lang": null,
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "1B1C17",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/1071088107280322561/ti6NN8f9_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/1071088107280322561/ti6NN8f9_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/78298637/1544202296",
+							"profile_link_color": "E24718",
+							"profile_sidebar_border_color": "141A15",
+							"profile_sidebar_fill_color": "10100F",
+							"profile_text_color": "82857E",
+							"profile_use_background_image": true,
+							"has_extended_profile": false,
+							"default_profile": false,
+							"default_profile_image": false,
+							"can_media_tag": true,
+							"followed_by": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 117777690,
+			"id_str": "117777690",
+			"name": "Jeremy Corbyn",
+			"screen_name": "jeremycorbyn",
+			"location": "UK",
+			"description": "Leader of the Labour Party.",
+			"url": "https://t.co/Z3F4vFOsn9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z3F4vFOsn9",
+							"expanded_url": "https://volunteer.labour.org.uk/",
+							"display_url": "volunteer.labour.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2231271,
+			"friends_count": 2594,
+			"listed_count": 7777,
+			"created_at": "Fri Feb 26 15:45:23 +0000 2010",
+			"favourites_count": 248,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13610,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1197846676578426880/EiaPjwTi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1197846676578426880/EiaPjwTi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/117777690/1506521898",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10098,
+		"favorite_count": 22954,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"We try not to eat a lot in one day, even though most of us are really hungry.\"\n\nThis sentence should never be uttered by any child, let alone one living in the 5th richest country in the world. \n\nMake no mistake, the Tories are responsible for this.\n\nhttps://t.co/cOorZYZMeD"
+	}
+]
