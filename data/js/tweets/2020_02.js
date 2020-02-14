59cae9c6fbc3e6a48928973ff1974774753b12f4
muranava <muraleeatjena@yahoo.co.uk>
2020-02-14 02:14:36 +0100

    Added 1 tweets

--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Thu Feb 13 13:59:58 +0000 2020",
+		"id": 1227955578305163300,
+		"id_str": "1227955578305163265",
+		"full_text": "Levelt's speech model gets a dunking by Bennett &amp; Hacker https://t.co/FloOJPzkTP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1227955573922111500,
+					"id_str": "1227955573922111489",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/EQqTFqeWoAEyKm6.png",
+					"media_url_https": "https://pbs.twimg.com/media/EQqTFqeWoAEyKm6.png",
+					"url": "https://t.co/FloOJPzkTP",
+					"display_url": "pic.twitter.com/FloOJPzkTP",
+					"expanded_url": "https://twitter.com/muranava/status/1227955578305163265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 146,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 703,
+							"h": 151,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 703,
+							"h": 151,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1227955573922111500,
+					"id_str": "1227955573922111489",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/EQqTFqeWoAEyKm6.png",
+					"media_url_https": "https://pbs.twimg.com/media/EQqTFqeWoAEyKm6.png",
+					"url": "https://t.co/FloOJPzkTP",
+					"display_url": "pic.twitter.com/FloOJPzkTP",
+					"expanded_url": "https://twitter.com/muranava/status/1227955578305163265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 146,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 703,
+							"h": 151,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 703,
+							"h": 151,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1227955573766922200,
+					"id_str": "1227955573766922240",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/EQqTFp5WoAA-ETh.png",
+					"media_url_https": "https://pbs.twimg.com/media/EQqTFp5WoAA-ETh.png",
+					"url": "https://t.co/FloOJPzkTP",
+					"display_url": "pic.twitter.com/FloOJPzkTP",
+					"expanded_url": "https://twitter.com/muranava/status/1227955578305163265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 734,
+							"h": 195,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 734,
+							"h": 195,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 181,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1913,
+			"friends_count": 1182,
+			"listed_count": 103,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 10178,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 308,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Levelt's speech model gets a dunking by Bennett &amp; Hacker https://t.co/FloOJPzkTP"
+	},
+	{
 		"created_at": "Wed Feb 12 18:57:53 +0000 2020",
 		"id": 1227583108800819200,
 		"id_str": "1227668162776313856",
