8757e98d2caf9407d890cad520369bfb9062f63e
muranava <muraleeatjena@yahoo.co.uk>
2020-04-02 03:14:37 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19020,
-	"created_at": "Wed Apr 01 01:14:36 +0000 2020",
+	"tweets": 19022,
+	"created_at": "Thu Apr 02 01:14:36 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,387 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 21:18:49 +0000 2020",
+		"id": 1245460635678322700,
+		"id_str": "1245460635678322688",
+		"full_text": "@leoselivan @sandymillin @OxfordWordsAPI Thx for the mention : )",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			64
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "leoselivan",
+					"name": "Lexical Leo",
+					"id": 408365496,
+					"id_str": "408365496",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "sandymillin",
+					"name": "Sandy Millin 🇪🇺",
+					"id": 144236944,
+					"id_str": "144236944",
+					"indices": [
+						12,
+						24
+					]
+				},
+				{
+					"screen_name": "OxfordWordsAPI",
+					"name": "OxDictionaries API",
+					"id": 844181073261219800,
+					"id_str": "844181073261219840",
+					"indices": [
+						25,
+						40
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1245449711907147800,
+		"in_reply_to_status_id_str": "1245449711907147777",
+		"in_reply_to_user_id": 408365496,
+		"in_reply_to_user_id_str": "408365496",
+		"in_reply_to_screen_name": "leoselivan",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1924,
+			"friends_count": 1247,
+			"listed_count": 103,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 10576,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 452,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@leoselivan @sandymillin @OxfordWordsAPI Thx for the mention : )"
+	},
+	{
+		"created_at": "Wed Apr 01 19:30:52 +0000 2020",
+		"id": 1245419281757532200,
+		"id_str": "1245433469246808064",
+		"full_text": "Dear LingTwitter, may I suggest u bring ur 💀skeleton💀to work?\n\nHere’s a moment from my new video on basics of phonetics! Doing what I can while staying socially &amp; physically distant.\n\nPlz check it out 👌🏽 https://t.co/9ftMHWKakC\n#linguistics #onlineteaching #phonetics #introling https://t.co/FkObPQaqCg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "linguistics",
+					"indices": [
+						232,
+						244
+					]
+				},
+				{
+					"text": "onlineteaching",
+					"indices": [
+						245,
+						260
+					]
+				},
+				{
+					"text": "phonetics",
+					"indices": [
+						261,
+						271
+					]
+				},
+				{
+					"text": "introling",
+					"indices": [
+						272,
+						282
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9ftMHWKakC",
+					"expanded_url": "https://youtu.be/lR5LHZ1Uaw0",
+					"display_url": "youtu.be/lR5LHZ1Uaw0",
+					"indices": [
+						208,
+						231
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1245419248983216000,
+					"id_str": "1245419248983216130",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1245419248983216130/pu/img/Gb42hp1qRGsudoK0.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1245419248983216130/pu/img/Gb42hp1qRGsudoK0.jpg",
+					"url": "https://t.co/FkObPQaqCg",
+					"display_url": "pic.twitter.com/FkObPQaqCg",
+					"expanded_url": "https://twitter.com/jamieisjames/status/1245419281757532160/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 406,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 406,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245419248983216000,
+					"id_str": "1245419248983216130",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1245419248983216130/pu/img/Gb42hp1qRGsudoK0.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1245419248983216130/pu/img/Gb42hp1qRGsudoK0.jpg",
+					"url": "https://t.co/FkObPQaqCg",
+					"display_url": "pic.twitter.com/FkObPQaqCg",
+					"expanded_url": "https://twitter.com/jamieisjames/status/1245419281757532160/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 406,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 406,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							360,
+							203
+						],
+						"duration_millis": 9707,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1245419248983216130/pu/pl/gLz4hyKbeW14oxtz.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245419248983216130/pu/vid/720x406/lIGD-IAOfMy8LdaI.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245419248983216130/pu/vid/478x270/pH_HP02lPtYNGPKD.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2208007390,
+			"id_str": "2208007390",
+			"name": "Jamie A. Thomas 🧟‍♀️",
+			"screen_name": "jamieisjames",
+			"location": "postapocalyptic ‘Murica",
+			"description": "~~zombie linguist rawrrr ~~ the undead? social justice? language? always relevant. books 👇🏽#ZombiesSpeakSwahili & #EmbodiedDifference",
+			"url": "https://t.co/brQZqBWXR4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/brQZqBWXR4",
+							"expanded_url": "https://vocalfriespod.fireside.fm/46",
+							"display_url": "vocalfriespod.fireside.fm/46",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1451,
+			"friends_count": 2181,
+			"listed_count": 53,
+			"created_at": "Wed Dec 04 22:27:48 +0000 2013",
+			"favourites_count": 13597,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7323,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/961389346082447360/T0uHBolA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/961389346082447360/T0uHBolA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2208007390/1540303223",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 18,
+		"favorite_count": 97,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dear LingTwitter, may I suggest u bring ur 💀skeleton💀to work?\n\nHere’s a moment from my new video on basics of phonetics! Doing what I can while staying socially &amp; physically distant.\n\nPlz check it out 👌🏽 https://t.co/9ftMHWKakC\n#linguistics #onlineteaching #phonetics #introling https://t.co/FkObPQaqCg"
+	}
+]
