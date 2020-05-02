0078169c883d925df8fcde7c93b71b7acd124742
muranava <muraleeatjena@yahoo.co.uk>
2020-05-02 03:14:38 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19201,
-	"created_at": "Fri May 01 01:14:39 +0000 2020",
+	"tweets": 19202,
+	"created_at": "Sat May 02 01:14:36 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_05.js
@@ -0,0 +1,199 @@
+Grailbird.data.tweets_2020_05 = 
+[
+	{
+		"created_at": "Fri May 01 13:03:16 +0000 2020",
+		"id": 1256146396098498600,
+		"id_str": "1256207560090300417",
+		"full_text": "May Day celebrates the history of the workers’ movement in which education workers have always played an active part.  \nAs the current crisis develops, we must be prepared to fight for ourselves, our students and our communities.   \n\n#IWD2020 #IWW #Solidarity https://t.co/w8UikiGcX8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "IWD2020",
+					"indices": [
+						234,
+						242
+					]
+				},
+				{
+					"text": "IWW",
+					"indices": [
+						243,
+						247
+					]
+				},
+				{
+					"text": "Solidarity",
+					"indices": [
+						248,
+						259
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1256146394383028200,
+					"id_str": "1256146394383028224",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/EW66fYcWkAAPyJV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW66fYcWkAAPyJV.jpg",
+					"url": "https://t.co/w8UikiGcX8",
+					"display_url": "pic.twitter.com/w8UikiGcX8",
+					"expanded_url": "https://twitter.com/TeflUnion/status/1256146396098498560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 423,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 423,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256146394383028200,
+					"id_str": "1256146394383028224",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/EW66fYcWkAAPyJV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW66fYcWkAAPyJV.jpg",
+					"url": "https://t.co/w8UikiGcX8",
+					"display_url": "pic.twitter.com/w8UikiGcX8",
+					"expanded_url": "https://twitter.com/TeflUnion/status/1256146396098498560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 423,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 423,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1130578765472501800,
+			"id_str": "1130578765472501760",
+			"name": "TEFL Workers' Union",
+			"screen_name": "TeflUnion",
+			"location": "London",
+			"description": "We like our students. We like our jobs. We like our workmates. We don’t like struggling to pay the bills. We don’t like insecure employment. Join us!",
+			"url": "https://t.co/ngblfDEBVd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ngblfDEBVd",
+							"expanded_url": "https://www.facebook.com/teflworkers/",
+							"display_url": "facebook.com/teflworkers/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 831,
+			"friends_count": 98,
+			"listed_count": 9,
+			"created_at": "Mon May 20 20:59:17 +0000 2019",
+			"favourites_count": 40,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 350,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1130578765472501760/1558387728",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 35,
+		"favorite_count": 46,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "May Day celebrates the history of the workers’ movement in which education workers have always played an active part.  \nAs the current crisis develops, we must be prepared to fight for ourselves, our students and our communities.   \n\n#IWD2020 #IWW #Solidarity https://t.co/w8UikiGcX8"
+	}
+]
