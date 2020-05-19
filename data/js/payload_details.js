418ded4b9304fb41ab13b66afe16daff9a3ae393
muranava <muraleeatjena@yahoo.co.uk>
2020-05-19 03:14:37 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19242,
-	"created_at": "Mon May 18 01:14:36 +0000 2020",
+	"tweets": 19244,
+	"created_at": "Tue May 19 01:14:36 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Mon May 18 17:58:45 +0000 2020",
+		"id": 1262442514868572200,
+		"id_str": "1262442514868572168",
+		"full_text": "Working in an ESOL training centre – my experience https://t.co/WUUC7rAJJQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WUUC7rAJJQ",
+					"expanded_url": "https://iww.org.uk/news/working-in-an-esol-training-centre-my-experience/",
+					"display_url": "iww.org.uk/news/working-i…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1931,
+			"friends_count": 1328,
+			"listed_count": 101,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 11248,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 712,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Working in an ESOL training centre – my experience https://t.co/WUUC7rAJJQ"
+	},
+	{
+		"created_at": "Mon May 18 17:55:20 +0000 2020",
+		"id": 1262291836112121900,
+		"id_str": "1262441657880006658",
+		"full_text": "HELP NEEDED! Solidarity from home! 📱💻\nAn IWW TEFL teacher was sacked at the beginning of lockdown, and cannot get furlough. @TeflUnion are calling for us all to put pressure on Promise Training Centre to give our member compensation. \n\nhttps://t.co/TqdAzBV7jY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TeflUnion",
+					"name": "TEFL Workers' Union",
+					"id": 1130578765472501800,
+					"id_str": "1130578765472501760",
+					"indices": [
+						124,
+						134
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/TqdAzBV7jY",
+					"expanded_url": "https://iww.org.uk/news/first-day-of-action-against-promise-training-centre",
+					"display_url": "iww.org.uk/news/first-day…",
+					"indices": [
+						236,
+						259
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 762385810784129000,
+			"id_str": "762385810784129024",
+			"name": "IWW Cymru",
+			"screen_name": "IWWCymru",
+			"location": "Wales",
+			"description": "Cymru-Wales branch of the Industrial Workers of the World. Mewn undeb mae nerth! (IWW WISE-RA)",
+			"url": "https://t.co/8mmxhNLRmb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8mmxhNLRmb",
+							"expanded_url": "https://IWW.org.uk/cymru",
+							"display_url": "IWW.org.uk/cymru",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1597,
+			"friends_count": 624,
+			"listed_count": 26,
+			"created_at": "Sun Aug 07 20:31:56 +0000 2016",
+			"favourites_count": 1436,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1800,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220000975869693955/jye-vdlZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220000975869693955/jye-vdlZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/762385810784129024/1586790266",
+			"profile_link_color": "C01000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "HELP NEEDED! Solidarity from home! 📱💻\nAn IWW TEFL teacher was sacked at the beginning of lockdown, and cannot get furlough. @TeflUnion are calling for us all to put pressure on Promise Training Centre to give our member compensation. \n\nhttps://t.co/TqdAzBV7jY"
+	},
+	{
 		"created_at": "Sun May 17 10:59:30 +0000 2020",
 		"id": 1261827888489300000,
 		"id_str": "1261974618572800000",
