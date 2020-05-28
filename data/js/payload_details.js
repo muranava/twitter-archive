bce39587011d0b453c28d2b60b9d07fb4728cccb
muranava <muraleeatjena@yahoo.co.uk>
2020-05-28 03:14:37 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19266,
-	"created_at": "Wed May 27 01:14:36 +0000 2020",
+	"tweets": 19268,
+	"created_at": "Thu May 28 01:14:36 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,259 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Wed May 27 22:18:54 +0000 2020",
+		"id": 1265766245049282600,
+		"id_str": "1265769474692694017",
+		"full_text": "New on Leoxicon: a speaking/(micro-)writing activity based on memorable Photos of the Week: https://t.co/BfgwRI8FC7 #AusELT #tefl #tesol",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AusELT",
+					"indices": [
+						116,
+						123
+					]
+				},
+				{
+					"text": "tefl",
+					"indices": [
+						124,
+						129
+					]
+				},
+				{
+					"text": "tesol",
+					"indices": [
+						130,
+						136
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BfgwRI8FC7",
+					"expanded_url": "https://bit.ly/2LYAhq3",
+					"display_url": "bit.ly/2LYAhq3",
+					"indices": [
+						92,
+						115
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 408365496,
+			"id_str": "408365496",
+			"name": "Lexical Leo",
+			"screen_name": "leoselivan",
+			"location": "Tel Aviv",
+			"description": "EFL teacher, trainer, blogger, speaker, author (Lexical Grammar). Interested in language, language learning, language teaching (but not only).",
+			"url": "https://t.co/ipK41edSFQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ipK41edSFQ",
+							"expanded_url": "http://leoxicon.blogspot.com",
+							"display_url": "leoxicon.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5899,
+			"friends_count": 478,
+			"listed_count": 177,
+			"created_at": "Wed Nov 09 10:06:49 +0000 2011",
+			"favourites_count": 62,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10121,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "67634F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/460546508601819136/12ivDBb__normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/460546508601819136/12ivDBb__normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/408365496/1527883504",
+			"profile_link_color": "767A5B",
+			"profile_sidebar_border_color": "FAF8F0",
+			"profile_sidebar_fill_color": "D9D0B2",
+			"profile_text_color": "83A391",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New on Leoxicon: a speaking/(micro-)writing activity based on memorable Photos of the Week: https://t.co/BfgwRI8FC7 #AusELT #tefl #tesol"
+	},
+	{
+		"created_at": "Wed May 27 20:56:43 +0000 2020",
+		"id": 1265748793259102200,
+		"id_str": "1265748793259102213",
+		"full_text": "@tesolmatthew cheers Matthew! hope you are well : ) nice list!",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			62
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tesolmatthew",
+					"name": "Matthew Noble",
+					"id": 1519875330,
+					"id_str": "1519875330",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1265700967640477700,
+		"in_reply_to_status_id_str": "1265700967640477696",
+		"in_reply_to_user_id": 1519875330,
+		"in_reply_to_user_id_str": "1519875330",
+		"in_reply_to_screen_name": "tesolmatthew",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1937,
+			"friends_count": 1333,
+			"listed_count": 101,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 11330,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 745,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@tesolmatthew cheers Matthew! hope you are well : ) nice list!"
+	},
+	{
 		"created_at": "Tue May 26 13:53:46 +0000 2020",
 		"id": 1265272460095570000,
 		"id_str": "1265279968189136898",
