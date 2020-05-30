f9fdc7c84e2c1fc0b69396bb3c8790c60654fefe
muranava <muraleeatjena@yahoo.co.uk>
2020-05-30 03:14:37 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19268,
-	"created_at": "Thu May 28 01:14:36 +0000 2020",
+	"tweets": 19269,
+	"created_at": "Sat May 30 01:14:36 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 29 20:33:48 +0000 2020",
+		"id": 1266440348605255700,
+		"id_str": "1266467803722375175",
+		"full_text": "Nouvelle expertise raciste où les médecins utilisent les termes « sujets de race noire » bâclée par des médecins incompétents. Cette expertise est contraire à tous les principes de la médecine et nous allons saisir le conseil de l’Ordre pour les dénoncer. https://t.co/jwwZNp4try",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1266440186675765200,
+					"id_str": "1266440186675765249",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1266440186675765249/pu/img/Rdb2mCK-SyaHRiza.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1266440186675765249/pu/img/Rdb2mCK-SyaHRiza.jpg",
+					"url": "https://t.co/jwwZNp4try",
+					"display_url": "pic.twitter.com/jwwZNp4try",
+					"expanded_url": "https://twitter.com/laveritepradama/status/1266440348605255685/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1266440186675765200,
+					"id_str": "1266440186675765249",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1266440186675765249/pu/img/Rdb2mCK-SyaHRiza.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1266440186675765249/pu/img/Rdb2mCK-SyaHRiza.jpg",
+					"url": "https://t.co/jwwZNp4try",
+					"display_url": "pic.twitter.com/jwwZNp4try",
+					"expanded_url": "https://twitter.com/laveritepradama/status/1266440348605255685/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 91036,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1266440186675765249/pu/vid/720x1280/q3R7PXvuLuZ9X09r.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1266440186675765249/pu/vid/320x568/9Zy7zsaN1WNLj1uJ.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1266440186675765249/pu/pl/OgRjm4OJZcBzVzVd.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1266440186675765249/pu/vid/360x640/YzrsjKW2RJdW4HM3.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 783710695716159500,
+			"id_str": "783710695716159489",
+			"name": "La Vérité Pour Adama",
+			"screen_name": "laveritepradama",
+			"location": "Beaumont-sur-Oise, France",
+			"description": "Soutenir la famille Traoré👉https://t.co/09LuUpUBxs #JusticePourAdama #JeSuisAdama #AdamaTraore #JeSoutiensLaFamilleTraoré https://t.co/VxEvoy2pmI",
+			"url": "https://t.co/wKzIEmpPCt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wKzIEmpPCt",
+							"expanded_url": "https://m.facebook.com/La-vérité-pour-Adama-160752057668634/",
+							"display_url": "m.facebook.com/La-vérité-pour…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/09LuUpUBxs",
+							"expanded_url": "http://okpal.com/adama-traore/#/",
+							"display_url": "okpal.com/adama-traore/#/",
+							"indices": [
+								27,
+								50
+							]
+						},
+						{
+							"url": "https://t.co/VxEvoy2pmI",
+							"expanded_url": "https://youtu.be/Ec69MnqYJog",
+							"display_url": "youtu.be/Ec69MnqYJog",
+							"indices": [
+								122,
+								145
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 20302,
+			"friends_count": 4407,
+			"listed_count": 75,
+			"created_at": "Wed Oct 05 16:49:25 +0000 2016",
+			"favourites_count": 13041,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10972,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/810122770470342657/_wKoTYuB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/810122770470342657/_wKoTYuB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/783710695716159489/1479885525",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3670,
+		"favorite_count": 4239,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Nouvelle expertise raciste où les médecins utilisent les termes « sujets de race noire » bâclée par des médecins incompétents. Cette expertise est contraire à tous les principes de la médecine et nous allons saisir le conseil de l’Ordre pour les dénoncer. https://t.co/jwwZNp4try"
+	},
+	{
 		"created_at": "Wed May 27 22:18:54 +0000 2020",
 		"id": 1265766245049282600,
 		"id_str": "1265769474692694017",
