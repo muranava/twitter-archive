fa8b496a350fea4c08de3bbe8872a180197f3feb
muranava <muraleeatjena@yahoo.co.uk>
2020-06-01 03:14:37 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19272,
-	"created_at": "Sun May 31 01:14:36 +0000 2020",
+	"tweets": 19273,
+	"created_at": "Mon Jun 01 01:14:36 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 31 11:58:22 +0000 2020",
+		"id": 1266765901908639700,
+		"id_str": "1267062865493667840",
+		"full_text": "I am reading a book in which this is considered an acceptable approach to referencing. https://t.co/C3FoPLKzXr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1266765896309186600,
+					"id_str": "1266765896309186563",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/EZR03v8WoAMpXEH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZR03v8WoAMpXEH.jpg",
+					"url": "https://t.co/C3FoPLKzXr",
+					"display_url": "pic.twitter.com/C3FoPLKzXr",
+					"expanded_url": "https://twitter.com/ashcroftethics/status/1266765901908639745/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1266765896309186600,
+					"id_str": "1266765896309186563",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/EZR03v8WoAMpXEH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZR03v8WoAMpXEH.jpg",
+					"url": "https://t.co/C3FoPLKzXr",
+					"display_url": "pic.twitter.com/C3FoPLKzXr",
+					"expanded_url": "https://twitter.com/ashcroftethics/status/1266765901908639745/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 406131568,
+			"id_str": "406131568",
+			"name": "Richard Ashcroft",
+			"screen_name": "ashcroftethics",
+			"location": "London",
+			"description": "Humble administrator. Sometime bioethicist. Utopia, music, books, books, books. NUFC, Surrey CCC. He/him",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8732,
+			"friends_count": 8122,
+			"listed_count": 305,
+			"created_at": "Sun Nov 06 08:56:29 +0000 2011",
+			"favourites_count": 40106,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 94333,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1194320731120766977/IZ4itVAa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1194320731120766977/IZ4itVAa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/406131568/1562523841",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 115,
+		"favorite_count": 706,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I am reading a book in which this is considered an acceptable approach to referencing. https://t.co/C3FoPLKzXr"
+	},
+	{
 		"created_at": "Sat May 30 20:02:29 +0000 2020",
 		"id": 1266822307965800400,
 		"id_str": "1266822307965800454",
