3c09691f3217bf422248020e989d50d91e848d7a
muranava <muraleeatjena@yahoo.co.uk>
2020-06-28 03:14:41 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19450,
-	"created_at": "Sat Jun 27 01:14:38 +0000 2020",
+	"tweets": 19451,
+	"created_at": "Sun Jun 28 01:14:40 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,151 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sat Jun 27 12:54:47 +0000 2020",
+		"id": 1276861534254358500,
+		"id_str": "1276861534254358530",
+		"full_text": "@bernywern @JtuckerJames some reactionaries who admit that language is ambiguous &amp; imprecise end up casting it as an original sin  - an interesting point (if i understand it correctly)from @ElSandifer in her book Neoreaction a Bailisk",
+		"truncated": false,
+		"display_text_range": [
+			25,
+			238
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bernywern",
+					"name": "Bernard Andrews",
+					"id": 411800297,
+					"id_str": "411800297",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "JtuckerJames",
+					"name": "James Tucker",
+					"id": 1798629030,
+					"id_str": "1798629030",
+					"indices": [
+						11,
+						24
+					]
+				},
+				{
+					"screen_name": "ElSandifer",
+					"name": "El Cops Are Bastards",
+					"id": 15374714,
+					"id_str": "15374714",
+					"indices": [
+						193,
+						204
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1276779243268116500,
+		"in_reply_to_status_id_str": "1276779243268116481",
+		"in_reply_to_user_id": 411800297,
+		"in_reply_to_user_id_str": "411800297",
+		"in_reply_to_screen_name": "bernywern",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1954,
+			"friends_count": 1452,
+			"listed_count": 101,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 12019,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 954,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@bernywern @JtuckerJames some reactionaries who admit that language is ambiguous &amp; imprecise end up casting it as an original sin  - an interesting point (if i understand it correctly)from @ElSandifer in her book Neoreaction a Bailisk"
+	},
+	{
 		"created_at": "Fri Jun 26 21:06:00 +0000 2020",
 		"id": 1276622765592924200,
 		"id_str": "1276622765592924161",
