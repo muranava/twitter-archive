10ecb4067c67fcb51c948e9fb9b018c32d24b5a3
muranava <muraleeatjena@yahoo.co.uk>
2020-07-14 03:14:38 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19500,
-	"created_at": "Mon Jul 13 01:14:40 +0000 2020",
+	"tweets": 19502,
+	"created_at": "Tue Jul 14 01:14:37 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Mon Jul 13 20:02:23 +0000 2020",
+		"id": 1282767350819889200,
+		"id_str": "1282767350819889152",
+		"full_text": "Language is both arbitrary and non-arbitrary https://t.co/8I9CLaC6ll",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8I9CLaC6ll",
+					"expanded_url": "https://nextcloud.englishup.me/sites/rethunk/arbitrary",
+					"display_url": "nextcloud.englishup.me/sites/rethunk/…",
+					"indices": [
+						45,
+						68
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1985,
+			"friends_count": 1519,
+			"listed_count": 102,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 12351,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1019,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Language is both arbitrary and non-arbitrary https://t.co/8I9CLaC6ll"
+	},
+	{
+		"created_at": "Mon Jul 13 11:47:10 +0000 2020",
+		"id": 1282642056851202000,
+		"id_str": "1282642722306891777",
+		"full_text": "FREE ESOL &amp; Cooking Class Online Wednesday (Livestream) &amp; Thursday (Zoom)\nAll #ESOL levels welcome!  Register in link on YouTube\nhttps://t.co/q5flIadDnL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ESOL",
+					"indices": [
+						86,
+						91
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/q5flIadDnL",
+					"expanded_url": "https://www.youtube.com/watch?v=xgXWl0O0_7w",
+					"display_url": "youtube.com/watch?v=xgXWl0…",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4111421320,
+			"id_str": "4111421320",
+			"name": "Heart & Parcel",
+			"screen_name": "heartandparcel",
+			"location": "Manchester, England",
+			"description": "Bringing women together, cooking together, developing ESOL communication skills together. We made a cookbook. 📗👉🥟  https://t.co/dFGExF6sDY…",
+			"url": "https://t.co/RCFSIg6LCR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/RCFSIg6LCR",
+							"expanded_url": "http://www.heartandparcel.org",
+							"display_url": "heartandparcel.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/dFGExF6sDY",
+							"expanded_url": "http://heartandparcel.org/heart-parcel-c",
+							"display_url": "heartandparcel.org/heart-parcel-c",
+							"indices": [
+								115,
+								138
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1727,
+			"friends_count": 1323,
+			"listed_count": 31,
+			"created_at": "Wed Nov 04 10:39:25 +0000 2015",
+			"favourites_count": 4791,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/693716539414073344/AwP8_vbD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/693716539414073344/AwP8_vbD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4111421320/1575740103",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "FREE ESOL &amp; Cooking Class Online Wednesday (Livestream) &amp; Thursday (Zoom)\nAll #ESOL levels welcome!  Register in link on YouTube\nhttps://t.co/q5flIadDnL"
+	},
+	{
 		"created_at": "Sun Jul 12 15:31:15 +0000 2020",
 		"id": 1282336729786130400,
 		"id_str": "1282336729786130432",
