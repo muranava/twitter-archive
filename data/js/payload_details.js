283432dc3fb79b994a28ee44019eeeff26919304
muranava <muraleeatjena@yahoo.co.uk>
2020-08-02 03:14:37 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19567,
-	"created_at": "Fri Jul 31 01:14:38 +0000 2020",
+	"tweets": 19568,
+	"created_at": "Sun Aug 02 01:14:36 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_08.js
@@ -0,0 +1,234 @@
+Grailbird.data.tweets_2020_08 = 
+[
+	{
+		"created_at": "Sat Aug 01 21:41:08 +0000 2020",
+		"id": 1289516269977337900,
+		"id_str": "1289677568749207557",
+		"full_text": "\"We have professionalised &amp; CPD’d ourselves as much as humanly possible. Our directors have told us nothing of substance. Our qualifications won’t pay rent or put food on the table.\"\n\nGreat article from @taw_sig:\n\nhttps://t.co/CiV1s4wP3S\n\n#solidarity #tradeunion #ELT #TEFL #ESL https://t.co/MItOdvRYYb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "solidarity",
+					"indices": [
+						243,
+						254
+					]
+				},
+				{
+					"text": "tradeunion",
+					"indices": [
+						255,
+						266
+					]
+				},
+				{
+					"text": "ELT",
+					"indices": [
+						267,
+						271
+					]
+				},
+				{
+					"text": "TEFL",
+					"indices": [
+						272,
+						277
+					]
+				},
+				{
+					"text": "ESL",
+					"indices": [
+						278,
+						282
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "taw_sig",
+					"name": "teachers_as_workers",
+					"id": 3152637711,
+					"id_str": "3152637711",
+					"indices": [
+						207,
+						215
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/CiV1s4wP3S",
+					"expanded_url": "http://www.teachersasworkers.org/where-is-teacher-solidarity/",
+					"display_url": "teachersasworkers.org/where-is-teach…",
+					"indices": [
+						218,
+						241
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1289516268106719200,
+					"id_str": "1289516268106719232",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/EeVINrSWoAApLV2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeVINrSWoAApLV2.jpg",
+					"url": "https://t.co/MItOdvRYYb",
+					"display_url": "pic.twitter.com/MItOdvRYYb",
+					"expanded_url": "https://twitter.com/TeflUnion/status/1289516269977337856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 940,
+							"h": 788,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 570,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 940,
+							"h": 788,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1289516268106719200,
+					"id_str": "1289516268106719232",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/EeVINrSWoAApLV2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeVINrSWoAApLV2.jpg",
+					"url": "https://t.co/MItOdvRYYb",
+					"display_url": "pic.twitter.com/MItOdvRYYb",
+					"expanded_url": "https://twitter.com/TeflUnion/status/1289516269977337856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 940,
+							"h": 788,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 570,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 940,
+							"h": 788,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1130578765472501800,
+			"id_str": "1130578765472501760",
+			"name": "TEFL Workers' Union",
+			"screen_name": "TeflUnion",
+			"location": "London",
+			"description": "We like our students. We like our jobs. We like our workmates. We don’t like struggling to pay the bills. We don’t like insecure employment. Join us!",
+			"url": "https://t.co/ngblfDEBVd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ngblfDEBVd",
+							"expanded_url": "https://www.facebook.com/teflworkers/",
+							"display_url": "facebook.com/teflworkers/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1094,
+			"friends_count": 99,
+			"listed_count": 10,
+			"created_at": "Mon May 20 20:59:17 +0000 2019",
+			"favourites_count": 73,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 533,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1130578765472501760/1558387728",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 13,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"We have professionalised &amp; CPD’d ourselves as much as humanly possible. Our directors have told us nothing of substance. Our qualifications won’t pay rent or put food on the table.\"\n\nGreat article from @taw_sig:\n\nhttps://t.co/CiV1s4wP3S\n\n#solidarity #tradeunion #ELT #TEFL #ESL https://t.co/MItOdvRYYb"
+	}
+]
