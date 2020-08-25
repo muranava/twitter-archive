d87ea08c63237f34f8d9b90119384eef5e1485ad
muranava <muraleeatjena@yahoo.co.uk>
2020-08-25 03:14:36 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19579,
-	"created_at": "Sat Aug 22 01:14:35 +0000 2020",
+	"tweets": 19581,
+	"created_at": "Tue Aug 25 01:14:35 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,259 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Mon Aug 24 16:45:29 +0000 2020",
+		"id": 1297927369622196200,
+		"id_str": "1297938089558577156",
+		"full_text": "NEW VIDEO: Black Lives Matter and The Economics Profession ft. Carolina Alves (@cacrisalves)\n\nhttps://t.co/40Cyl57xcb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cacrisalves",
+					"name": "Carolina Alves",
+					"id": 978592070138060800,
+					"id_str": "978592070138060802",
+					"indices": [
+						79,
+						91
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/40Cyl57xcb",
+					"expanded_url": "https://www.youtube.com/watch?v=eFdUYZfBcjo",
+					"display_url": "youtube.com/watch?v=eFdUYZ…",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 189948728,
+			"id_str": "189948728",
+			"name": "Unlearning Economics",
+			"screen_name": "UnlearnEcon",
+			"location": "UK",
+			"description": "Anti-anti-anti-economics, pro-pluralism",
+			"url": "https://t.co/M95wvZRpqo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/M95wvZRpqo",
+							"expanded_url": "https://www.youtube.com/channel/UC4V_jMdRbbTrmBVJB6FDzgw?view_as=subscriber",
+							"display_url": "youtube.com/channel/UC4V_j…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 28396,
+			"friends_count": 790,
+			"listed_count": 648,
+			"created_at": "Sun Sep 12 17:36:25 +0000 2010",
+			"favourites_count": 23835,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11258,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1246039662050729985/gbcGGKXm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1246039662050729985/gbcGGKXm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/189948728/1586604010",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "NEW VIDEO: Black Lives Matter and The Economics Profession ft. Carolina Alves (@cacrisalves)\n\nhttps://t.co/40Cyl57xcb"
+	},
+	{
+		"created_at": "Mon Aug 24 09:42:14 +0000 2020",
+		"id": 1297831574961086500,
+		"id_str": "1297831574961086464",
+		"full_text": "@mikercameron Does story cite any data for teachers as vectors such as this one for children as vectors? https://t.co/tdXP48alQ3",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			128
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mikercameron",
+					"name": "Mike Cameron 🖋",
+					"id": 14138494,
+					"id_str": "14138494",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tdXP48alQ3",
+					"expanded_url": "https://www.jpeds.com/article/S0022-3476(20)31023-4/fulltext",
+					"display_url": "jpeds.com/article/S0022-…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1297648534251020300,
+		"in_reply_to_status_id_str": "1297648534251020292",
+		"in_reply_to_user_id": 14138494,
+		"in_reply_to_user_id_str": "14138494",
+		"in_reply_to_screen_name": "mikercameron",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2002,
+			"friends_count": 1592,
+			"listed_count": 102,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 12716,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1115,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@mikercameron Does story cite any data for teachers as vectors such as this one for children as vectors? https://t.co/tdXP48alQ3"
+	},
+	{
 		"created_at": "Fri Aug 21 12:16:49 +0000 2020",
 		"id": 1296783313143832600,
 		"id_str": "1296783313143832576",
