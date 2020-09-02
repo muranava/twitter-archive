fbf1faabf3d9c46904e64c52dd4ebbd30a6d40c7
muranava <muraleeatjena@yahoo.co.uk>
2020-09-02 03:14:37 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19591,
-	"created_at": "Mon Aug 31 01:14:36 +0000 2020",
+	"tweets": 19593,
+	"created_at": "Wed Sep 02 01:14:36 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_09.js
@@ -0,0 +1,382 @@
+Grailbird.data.tweets_2020_09 = 
+[
+	{
+		"created_at": "Tue Sep 01 21:51:28 +0000 2020",
+		"id": 1300900466738581500,
+		"id_str": "1300914195555405824",
+		"full_text": "My round-up of the recent #EndofELT Twitter chat. What do you think? Will Covid spell the end of #ELT? https://t.co/ZQnCIuexvA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "EndofELT",
+					"indices": [
+						26,
+						35
+					]
+				},
+				{
+					"text": "ELT",
+					"indices": [
+						97,
+						101
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZQnCIuexvA",
+					"expanded_url": "http://www.teachersasworkers.org/will-covid-spell-the-end-of-elt/",
+					"display_url": "teachersasworkers.org/will-covid-spe…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 134211317,
+			"id_str": "134211317",
+			"name": "paulwalsh",
+			"screen_name": "josipa74",
+			"location": "berlin",
+			"description": "writer-teacher-precarious worker / bylines: @guardian\n@artmonthly @tribunemagazine @independent\n@opendemocracy @redpepper @ROAR_Magazine @bellacaledonia",
+			"url": "https://t.co/0wx2uSf84O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0wx2uSf84O",
+							"expanded_url": "https://www.paulwalshwriting.com/",
+							"display_url": "paulwalshwriting.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2113,
+			"friends_count": 4995,
+			"listed_count": 55,
+			"created_at": "Sat Apr 17 19:24:28 +0000 2010",
+			"favourites_count": 5245,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7874,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1228007865341874176/hnEskxS8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1228007865341874176/hnEskxS8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/134211317/1427244606",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My round-up of the recent #EndofELT Twitter chat. What do you think? Will Covid spell the end of #ELT? https://t.co/ZQnCIuexvA"
+	},
+	{
+		"created_at": "Tue Sep 01 09:23:03 +0000 2020",
+		"id": 1300155755375587300,
+		"id_str": "1300725849637953537",
+		"full_text": "I am giving away one of my copies of the book below. If interested  (blurb at link below), just like this post by 5 p.m. UK time on 1/9/2020. Will draw the name and then post out. @LingEdRoutledge @ProfMMcCarthy @Anne0Keeffe https://t.co/tVgtwZbSlq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			224
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LingEdRoutledge",
+					"name": "Elizabeth (Lizzie) Cox",
+					"id": 984741627297173500,
+					"id_str": "984741627297173504",
+					"indices": [
+						180,
+						196
+					]
+				},
+				{
+					"screen_name": "ProfMMcCarthy",
+					"name": "Prof Mike McCarthy",
+					"id": 831437002830651400,
+					"id_str": "831437002830651392",
+					"indices": [
+						197,
+						211
+					]
+				},
+				{
+					"screen_name": "Anne0Keeffe",
+					"name": "Anne O'Keeffe",
+					"id": 310322075,
+					"id_str": "310322075",
+					"indices": [
+						212,
+						224
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tVgtwZbSlq",
+					"expanded_url": "https://twitter.com/ELTResearch/status/1298243932682440704",
+					"display_url": "twitter.com/ELTResearch/st…",
+					"indices": [
+						225,
+						248
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3308043417,
+			"id_str": "3308043417",
+			"name": "Chris Jones",
+			"screen_name": "ELTResearch",
+			"location": "University of Liverpool, UK",
+			"description": "Senior Lecturer in TESOL/Applied Linguistics. Views are my own.",
+			"url": "https://t.co/d9Nx0Vxdmx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/d9Nx0Vxdmx",
+							"expanded_url": "https://www.researchgate.net/profile/Christian_Jones8",
+							"display_url": "researchgate.net/profile/Christ…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1912,
+			"friends_count": 1073,
+			"listed_count": 39,
+			"created_at": "Thu Jun 04 11:37:59 +0000 2015",
+			"favourites_count": 696,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2322,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/720360015383654400/Zq4CQgZv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/720360015383654400/Zq4CQgZv_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1298243932682440700,
+		"quoted_status_id_str": "1298243932682440704",
+		"quoted_status_permalink": {
+			"url": "https://t.co/tVgtwZbSlq",
+			"expanded": "https://twitter.com/ELTResearch/status/1298243932682440704",
+			"display": "twitter.com/ELTResearch/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Aug 25 13:00:48 +0000 2020",
+			"id": 1298243932682440700,
+			"id_str": "1298243932682440704",
+			"full_text": "Happy to see this book (co-authored with Shelley Byrne and Nicola Halenko) finally get a paperback release. \nSuccessful Spoken English: Findings from Learner Corpora https://t.co/GErMMsGZy1 via @AmazonUK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				203
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "AmazonUK",
+						"name": "Amazon.co.uk",
+						"id": 209004862,
+						"id_str": "209004862",
+						"indices": [
+							194,
+							203
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/GErMMsGZy1",
+						"expanded_url": "https://www.amazon.co.uk/dp/0367594773/ref=cm_sw_r_tw_dp_x_NQqrFb9ZZ2KXJ",
+						"display_url": "amazon.co.uk/dp/0367594773/…",
+						"indices": [
+							166,
+							189
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3308043417,
+				"id_str": "3308043417",
+				"name": "Chris Jones",
+				"screen_name": "ELTResearch",
+				"location": "University of Liverpool, UK",
+				"description": "Senior Lecturer in TESOL/Applied Linguistics. Views are my own.",
+				"url": "https://t.co/d9Nx0Vxdmx",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/d9Nx0Vxdmx",
+								"expanded_url": "https://www.researchgate.net/profile/Christian_Jones8",
+								"display_url": "researchgate.net/profile/Christ…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1912,
+				"friends_count": 1073,
+				"listed_count": 39,
+				"created_at": "Thu Jun 04 11:37:59 +0000 2015",
+				"favourites_count": 696,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2322,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/720360015383654400/Zq4CQgZv_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/720360015383654400/Zq4CQgZv_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 12,
+			"favorite_count": 33,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 5,
+		"favorite_count": 41,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I am giving away one of my copies of the book below. If interested  (blurb at link below), just like this post by 5 p.m. UK time on 1/9/2020. Will draw the name and then post out. @LingEdRoutledge @ProfMMcCarthy @Anne0Keeffe https://t.co/tVgtwZbSlq"
+	}
+]
