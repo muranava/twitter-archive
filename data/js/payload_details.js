08ba1960228eae4cb1b761213e0252bf765d91c9
muranava <muraleeatjena@yahoo.co.uk>
2020-11-18 02:14:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19730,
-	"created_at": "Tue Nov 17 01:14:36 +0000 2020",
+	"tweets": 19731,
+	"created_at": "Wed Nov 18 01:14:35 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Tue Nov 17 23:04:45 +0000 2020",
+		"id": 1328836502265213000,
+		"id_str": "1328836502265212928",
+		"full_text": "@bradfordlad5 @Charles68431225 @veggiesosage @simonmaginn @BBC @Keir_Starmer https://t.co/IWjEkla2c5 they took about 3 hours to change it",
+		"truncated": false,
+		"display_text_range": [
+			77,
+			137
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bradfordlad5",
+					"name": "John, a modest genius",
+					"id": 1059501314487533600,
+					"id_str": "1059501314487533568",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "Charles68431225",
+					"name": "Charles J - #MMT 🥀 🇵🇸 ♿🍀",
+					"id": 1143888390083010600,
+					"id_str": "1143888390083010560",
+					"indices": [
+						14,
+						30
+					]
+				},
+				{
+					"screen_name": "veggiesosage",
+					"name": "Veggiesosage Has Left the Building Occasionally",
+					"id": 2794563786,
+					"id_str": "2794563786",
+					"indices": [
+						31,
+						44
+					]
+				},
+				{
+					"screen_name": "simonmaginn",
+					"name": "simon maginn",
+					"id": 128402033,
+					"id_str": "128402033",
+					"indices": [
+						45,
+						57
+					]
+				},
+				{
+					"screen_name": "BBC",
+					"name": "BBC",
+					"id": 19701628,
+					"id_str": "19701628",
+					"indices": [
+						58,
+						62
+					]
+				},
+				{
+					"screen_name": "Keir_Starmer",
+					"name": "Keir Starmer",
+					"id": 2425571623,
+					"id_str": "2425571623",
+					"indices": [
+						63,
+						76
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/IWjEkla2c5",
+					"expanded_url": "https://www.newssniffer.co.uk/articles/2031119/diff/3/4",
+					"display_url": "newssniffer.co.uk/articles/20311…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1328741099754705000,
+		"in_reply_to_status_id_str": "1328741099754704902",
+		"in_reply_to_user_id": 1059501314487533600,
+		"in_reply_to_user_id_str": "1059501314487533568",
+		"in_reply_to_screen_name": "bradfordlad5",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2033,
+			"friends_count": 1763,
+			"listed_count": 103,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 13677,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1312,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@bradfordlad5 @Charles68431225 @veggiesosage @simonmaginn @BBC @Keir_Starmer https://t.co/IWjEkla2c5 they took about 3 hours to change it"
+	},
+	{
 		"created_at": "Mon Nov 16 11:47:39 +0000 2020",
 		"id": 1328261699061899300,
 		"id_str": "1328303715258933248",
