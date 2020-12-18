de3ece1d7b2ce91c44ee43411d422947ece2465d
muranava <muraleeatjena@yahoo.co.uk>
2020-12-18 02:14:37 +0100

    Added 1 tweets

--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 17 21:01:36 +0000 2020",
+		"id": 1339677147544301600,
+		"id_str": "1339677147544301572",
+		"full_text": "blimey first time for this message nice one @Nextclouders : ) https://t.co/WNACrAx5AL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Nextclouders",
+					"name": "Nextcloud 📱☁️💻",
+					"id": 734010048712675300,
+					"id_str": "734010048712675329",
+					"indices": [
+						44,
+						57
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1339677097154011100,
+					"id_str": "1339677097154011140",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/Epd9ONGXYAQplxQ.png",
+					"media_url_https": "https://pbs.twimg.com/media/Epd9ONGXYAQplxQ.png",
+					"url": "https://t.co/WNACrAx5AL",
+					"display_url": "pic.twitter.com/WNACrAx5AL",
+					"expanded_url": "https://twitter.com/muranava/status/1339677147544301572/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 351,
+							"h": 248,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 351,
+							"h": 248,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 351,
+							"h": 248,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1339677097154011100,
+					"id_str": "1339677097154011140",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/Epd9ONGXYAQplxQ.png",
+					"media_url_https": "https://pbs.twimg.com/media/Epd9ONGXYAQplxQ.png",
+					"url": "https://t.co/WNACrAx5AL",
+					"display_url": "pic.twitter.com/WNACrAx5AL",
+					"expanded_url": "https://twitter.com/muranava/status/1339677147544301572/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 351,
+							"h": 248,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 351,
+							"h": 248,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 351,
+							"h": 248,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2044,
+			"friends_count": 1862,
+			"listed_count": 103,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 14142,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1365,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "blimey first time for this message nice one @Nextclouders : ) https://t.co/WNACrAx5AL"
+	},
+	{
 		"created_at": "Wed Dec 16 21:13:33 +0000 2020",
 		"id": 1339312365276455000,
 		"id_str": "1339317768110415873",
@@ -38,7 +242,7 @@ Grailbird.data.tweets_2020_12 =
 			"name": "Uju Anya",
 			"screen_name": "UjuAnya",
 			"location": "State College, PA",
-			"description": "University professor. Critical applied linguist. Anti-racist, equity-minded, feminist pedagogy. 🏳️‍🌈⚢🏳️‍🌈 Inquiry: Blackness in multilingualism. Views=mine.",
+			"description": "University professor. Critical applied linguist. Anti-racist, equity-minded, feminist pedagogy. 🏳️🌈⚢🏳️🌈 Inquiry: Blackness in multilingualism. Views=mine.",
 			"url": "https://t.co/nSswwI4H9J",
 			"entities": {
 				"url": {
