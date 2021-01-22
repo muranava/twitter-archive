83fd6d002b0801f23c5e7e0ea69627c9b1a36c4b
muranava <muraleeatjena@yahoo.co.uk>
2021-01-22 02:14:38 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19834,
-	"created_at": "Wed Jan 20 01:14:36 +0000 2021",
+	"tweets": 19835,
+	"created_at": "Fri Jan 22 01:14:36 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Thu Jan 21 18:55:15 +0000 2021",
+		"id": 1352225670747840500,
+		"id_str": "1352328924282449928",
+		"full_text": "In neoliberal ideology, the capitalist is a worker (\"CEOs deserves their salaries for hard work and rare skills\") and the worker is a capitalist (\"enhance and maximize your human capital\").",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			189
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2921773240,
+			"id_str": "2921773240",
+			"name": "bue rübner",
+			"screen_name": "BueRubner",
+			"location": "",
+			"description": "Optimism of the will / Catastrophism of the intellect.\n\nVisiting Research Fellow Uni Hamburg, @fos_uhh, editor @viewpointmag",
+			"url": "https://t.co/Xx27YdR3jw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Xx27YdR3jw",
+							"expanded_url": "https://au.academia.edu/BueR%C3%BCbnerHansen",
+							"display_url": "au.academia.edu/BueR%C3%BCbner…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2864,
+			"friends_count": 1348,
+			"listed_count": 49,
+			"created_at": "Sun Dec 14 18:15:34 +0000 2014",
+			"favourites_count": 16327,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9542,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1301174252482289667/q3sCWTue_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1301174252482289667/q3sCWTue_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2921773240/1468864744",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 117,
+		"favorite_count": 653,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "In neoliberal ideology, the capitalist is a worker (\"CEOs deserves their salaries for hard work and rare skills\") and the worker is a capitalist (\"enhance and maximize your human capital\")."
+	},
+	{
 		"created_at": "Tue Jan 19 22:17:24 +0000 2021",
 		"id": 1351655022199763000,
 		"id_str": "1351655022199762947",
