8dfb1702f5636f36457c1f295da6bdeab8602189
muranava <muraleeatjena@yahoo.co.uk>
2021-04-27 03:14:36 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20101,
-	"created_at": "Mon Apr 26 01:14:35 +0000 2021",
+	"tweets": 20103,
+	"created_at": "Tue Apr 27 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,360 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Mon Apr 26 12:05:31 +0000 2021",
+		"id": 1386642714238890000,
+		"id_str": "1386652658925293570",
+		"full_text": "\"We did not even go out to the West Bank, how do we go out once to London? It's a great miracle.\" #drama @ELT @Palestine #Westbank #Gaza\nhttps://t.co/jks3YaqLLZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "drama",
+					"indices": [
+						98,
+						104
+					]
+				},
+				{
+					"text": "Westbank",
+					"indices": [
+						121,
+						130
+					]
+				},
+				{
+					"text": "Gaza",
+					"indices": [
+						131,
+						136
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "elt",
+					"name": "elton naicker",
+					"id": 131938942,
+					"id_str": "131938942",
+					"indices": [
+						105,
+						109
+					]
+				},
+				{
+					"screen_name": "palestine",
+					"name": "Palestine News",
+					"id": 2797701,
+					"id_str": "2797701",
+					"indices": [
+						110,
+						120
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/jks3YaqLLZ",
+					"expanded_url": "https://www.youtube.com/watch?v=RmlVoplvJ0E",
+					"display_url": "youtube.com/watch?v=RmlVop…",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1028389409400528900,
+			"id_str": "1028389409400528896",
+			"name": "The Hands Up Project",
+			"screen_name": "HandsUpProject",
+			"location": "Totnes",
+			"description": "UK  #charity doing #Online #storytelling & #English #language #learning with #Palestinian & #Syrian children in #Gaza, #TheWestBank & #Zaatari refugee camp",
+			"url": "https://t.co/2uVu7dqVwg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2uVu7dqVwg",
+							"expanded_url": "https://handsupproject.org/",
+							"display_url": "handsupproject.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 675,
+			"friends_count": 936,
+			"listed_count": 2,
+			"created_at": "Sat Aug 11 21:15:16 +0000 2018",
+			"favourites_count": 405,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1028970260152627202/6-dLLv0x_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1028970260152627202/6-dLLv0x_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1028389409400528896/1534022653",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"We did not even go out to the West Bank, how do we go out once to London? It's a great miracle.\" #drama @ELT @Palestine #Westbank #Gaza\nhttps://t.co/jks3YaqLLZ"
+	},
+	{
+		"created_at": "Mon Apr 26 08:19:43 +0000 2021",
+		"id": 1386591090216689700,
+		"id_str": "1386595835740311554",
+		"full_text": "TEFL Union's April Challenge:\n\n1. Meet with your workmates\n2. Choose one thing you'd like change about your job\n3. Contact us at tefl@iww.org.uk\n\nA union rep will organise a Zoom to discuss how the #union can support you all to make that one thing a reality. \n\n#TEFL #ELT #union https://t.co/EzmXp4kDbz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "union",
+					"indices": [
+						198,
+						204
+					]
+				},
+				{
+					"text": "TEFL",
+					"indices": [
+						261,
+						266
+					]
+				},
+				{
+					"text": "ELT",
+					"indices": [
+						267,
+						271
+					]
+				},
+				{
+					"text": "union",
+					"indices": [
+						272,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1386591088450867200,
+					"id_str": "1386591088450867202",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Ez4pP5vWEAI8XAp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ez4pP5vWEAI8XAp.jpg",
+					"url": "https://t.co/EzmXp4kDbz",
+					"display_url": "pic.twitter.com/EzmXp4kDbz",
+					"expanded_url": "https://twitter.com/TeflUnion/status/1386591090216689664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1386591088450867200,
+					"id_str": "1386591088450867202",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Ez4pP5vWEAI8XAp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ez4pP5vWEAI8XAp.jpg",
+					"url": "https://t.co/EzmXp4kDbz",
+					"display_url": "pic.twitter.com/EzmXp4kDbz",
+					"expanded_url": "https://twitter.com/TeflUnion/status/1386591090216689664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1130578765472501800,
+			"id_str": "1130578765472501760",
+			"name": "TEFL Workers' Union",
+			"screen_name": "TeflUnion",
+			"location": "London",
+			"description": "We like our students. We like our jobs. We like our workmates. We don’t like struggling to pay the bills. We don’t like insecure employment. Join us!",
+			"url": "https://t.co/ngblfDEBVd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ngblfDEBVd",
+							"expanded_url": "https://www.facebook.com/teflworkers/",
+							"display_url": "facebook.com/teflworkers/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1661,
+			"friends_count": 117,
+			"listed_count": 13,
+			"created_at": "Mon May 20 20:59:17 +0000 2019",
+			"favourites_count": 407,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1588,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1130578765472501760/1558387728",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TEFL Union's April Challenge:\n\n1. Meet with your workmates\n2. Choose one thing you'd like change about your job\n3. Contact us at tefl@iww.org.uk\n\nA union rep will organise a Zoom to discuss how the #union can support you all to make that one thing a reality. \n\n#TEFL #ELT #union https://t.co/EzmXp4kDbz"
+	},
+	{
 		"created_at": "Sun Apr 25 15:19:54 +0000 2021",
 		"id": 1386339191517335600,
 		"id_str": "1386339191517335563",
