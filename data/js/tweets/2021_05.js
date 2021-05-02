50d661a3c060dae4c7778e809f9cef95d743caf2
muranava <muraleeatjena@yahoo.co.uk>
2021-05-02 03:14:36 +0200

    Added 1 tweets

---
+++ data/js/tweets/2021_05.js
@@ -0,0 +1,170 @@
+Grailbird.data.tweets_2021_05 = 
+[
+	{
+		"created_at": "Sat May 01 12:21:39 +0000 2021",
+		"id": 1388468659014750200,
+		"id_str": "1388468659014750214",
+		"full_text": "@MflTransform @VEverettmfl @gianfrancocont9 @valleseco I wonder if the 90/10 high frequency/low freq ratio that AQA criticise was taken from Ogden's Basic English? https://t.co/7WoNE3UWzu",
+		"truncated": false,
+		"display_text_range": [
+			55,
+			187
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MflTransform",
+					"name": "Let's Transform MFL",
+					"id": 1009898196125061100,
+					"id_str": "1009898196125061120",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "VEverettmfl",
+					"name": "Mr E 🇬🇧",
+					"id": 365336458,
+					"id_str": "365336458",
+					"indices": [
+						14,
+						26
+					]
+				},
+				{
+					"screen_name": "gianfrancocont9",
+					"name": "Dr Gianfranco Conti",
+					"id": 2347690794,
+					"id_str": "2347690794",
+					"indices": [
+						27,
+						43
+					]
+				},
+				{
+					"screen_name": "valleseco",
+					"name": "Clare Seccombe 💡",
+					"id": 22238006,
+					"id_str": "22238006",
+					"indices": [
+						44,
+						54
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7WoNE3UWzu",
+					"expanded_url": "http://ogden.basic-english.org/basiceng.html",
+					"display_url": "ogden.basic-english.org/basiceng.html",
+					"indices": [
+						164,
+						187
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1388189380490272800,
+		"in_reply_to_status_id_str": "1388189380490272768",
+		"in_reply_to_user_id": 1009898196125061100,
+		"in_reply_to_user_id_str": "1009898196125061120",
+		"in_reply_to_screen_name": "MflTransform",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2075,
+			"friends_count": 2231,
+			"listed_count": 102,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 16471,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 469,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@MflTransform @VEverettmfl @gianfrancocont9 @valleseco I wonder if the 90/10 high frequency/low freq ratio that AQA criticise was taken from Ogden's Basic English? https://t.co/7WoNE3UWzu"
+	}
+]
