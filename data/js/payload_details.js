88040db98bf6ab98f2acbbd2d5762ac1aa8a8287
muranava <muraleeatjena@yahoo.co.uk>
2021-05-04 03:14:46 +0200

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20115,
-	"created_at": "Sun May 02 01:14:36 +0000 2021",
+	"tweets": 20118,
+	"created_at": "Tue May 04 01:14:46 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,432 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Mon May 03 15:55:23 +0000 2021",
+		"id": 1387674175893123000,
+		"id_str": "1389247224283009026",
+		"full_text": "#CPD #ELT #EFL #CELTA #DELTA We're running our very first one-day Lexical Lab online conference on Saturday June the 5th – and it's absolutely free! Register below, and we’ll send details of the Zoom link nearer the time. https://t.co/QDi8ShCTtX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "CPD",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "ELT",
+					"indices": [
+						5,
+						9
+					]
+				},
+				{
+					"text": "EFL",
+					"indices": [
+						10,
+						14
+					]
+				},
+				{
+					"text": "CELTA",
+					"indices": [
+						15,
+						21
+					]
+				},
+				{
+					"text": "DELTA",
+					"indices": [
+						22,
+						28
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QDi8ShCTtX",
+					"expanded_url": "https://www.lexicallab.com/conference/",
+					"display_url": "lexicallab.com/conference/",
+					"indices": [
+						222,
+						245
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 857732892,
+			"id_str": "857732892",
+			"name": "Lexical Lab",
+			"screen_name": "LexicalLab",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1575,
+			"friends_count": 969,
+			"listed_count": 41,
+			"created_at": "Tue Oct 02 09:07:46 +0000 2012",
+			"favourites_count": 72,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 961,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/559654844831506432/F4HWliCf_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/559654844831506432/F4HWliCf_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#CPD #ELT #EFL #CELTA #DELTA We're running our very first one-day Lexical Lab online conference on Saturday June the 5th – and it's absolutely free! Register below, and we’ll send details of the Zoom link nearer the time. https://t.co/QDi8ShCTtX"
+	},
+	{
+		"created_at": "Mon May 03 15:07:49 +0000 2021",
+		"id": 1389172452509589500,
+		"id_str": "1389235250652397571",
+		"full_text": "The 'trials' being carried out in the UK on thousands of people entering crowded indoor venues without masks &amp; social distancing are unethical. Don't just listen to me- listen to Prof Monks- the study lead, who himself accepts people are 'guinea pigs'\nhttps://t.co/MYIIQfEHk3 https://t.co/eHs9OiVMYC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MYIIQfEHk3",
+					"expanded_url": "https://www.youtube.com/watch?v=m0ctykhFRW4",
+					"display_url": "youtube.com/watch?v=m0ctyk…",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1389171472917270500,
+					"id_str": "1389171472917270528",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/E0dUGC9XEAAPhsj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E0dUGC9XEAAPhsj.jpg",
+					"url": "https://t.co/eHs9OiVMYC",
+					"display_url": "pic.twitter.com/eHs9OiVMYC",
+					"expanded_url": "https://twitter.com/dgurdasani1/status/1389172452509589505/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 754,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1616,
+							"h": 1016,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 428,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1389171472917270500,
+					"id_str": "1389171472917270528",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/E0dUGC9XEAAPhsj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E0dUGC9XEAAPhsj.jpg",
+					"url": "https://t.co/eHs9OiVMYC",
+					"display_url": "pic.twitter.com/eHs9OiVMYC",
+					"expanded_url": "https://twitter.com/dgurdasani1/status/1389172452509589505/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 754,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1616,
+							"h": 1016,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 428,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 920943710254313500,
+			"id_str": "920943710254313472",
+			"name": "Deepti Gurdasani",
+			"screen_name": "dgurdasani1",
+			"location": "",
+			"description": "Senior Lecturer @QMUL Epidemiology, statistical genetics, machine learning. Intersectional feminist. Advocating for a better culture in academia. All views mine",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 54295,
+			"friends_count": 3646,
+			"listed_count": 756,
+			"created_at": "Thu Oct 19 09:24:25 +0000 2017",
+			"favourites_count": 16458,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14697,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1384780148260605952/Y3TMGCUg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1384780148260605952/Y3TMGCUg_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 626,
+		"favorite_count": 1207,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The 'trials' being carried out in the UK on thousands of people entering crowded indoor venues without masks &amp; social distancing are unethical. Don't just listen to me- listen to Prof Monks- the study lead, who himself accepts people are 'guinea pigs'\nhttps://t.co/MYIIQfEHk3 https://t.co/eHs9OiVMYC"
+	},
+	{
+		"created_at": "Mon May 03 14:07:25 +0000 2021",
+		"id": 1389220051442708500,
+		"id_str": "1389220051442708484",
+		"full_text": "@MflTransform yes that's another issue, why so much weight given to \"objective\" measures over \"subjective\"? - one of the enduring word lists, GSL, has endure precisely because of the relative balance btw statistics and commonsense",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MflTransform",
+					"name": "Let's Transform MFL",
+					"id": 1009898196125061100,
+					"id_str": "1009898196125061120",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1389219085247000600,
+		"in_reply_to_status_id_str": "1389219085247000579",
+		"in_reply_to_user_id": 1009898196125061100,
+		"in_reply_to_user_id_str": "1009898196125061120",
+		"in_reply_to_screen_name": "MflTransform",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2074,
+			"friends_count": 2241,
+			"listed_count": 102,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 16485,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 473,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1801295431/wnw_sprite_normal.gif",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@MflTransform yes that's another issue, why so much weight given to \"objective\" measures over \"subjective\"? - one of the enduring word lists, GSL, has endure precisely because of the relative balance btw statistics and commonsense"
+	},
+	{
 		"created_at": "Sat May 01 12:21:39 +0000 2021",
 		"id": 1388468659014750200,
 		"id_str": "1388468659014750214",
