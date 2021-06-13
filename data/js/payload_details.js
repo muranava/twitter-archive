906a72c9ecbc34a20944c1a16b90342fac3fbf73
muranava <muraleeatjena@yahoo.co.uk>
2021-06-13 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20264,
-	"created_at": "Sat Jun 12 01:14:35 +0000 2021",
+	"tweets": 20265,
+	"created_at": "Sun Jun 13 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sat Jun 12 08:57:47 +0000 2021",
+		"id": 1403268373018972200,
+		"id_str": "1403637643175514114",
+		"full_text": "Supplier for Israel's largest arms company, APPH, tries to deny any association with the murderous Elbit Systems. Unsurprisingly, their comments to the BBC are filled with lies and attempts to cover their complicit in murder. THREAD https://t.co/43ecYrihCN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1403265320534610000,
+					"id_str": "1403265320534609920",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/E3lmX8wXEAA9Hij.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3lmX8wXEAA9Hij.jpg",
+					"url": "https://t.co/43ecYrihCN",
+					"display_url": "pic.twitter.com/43ecYrihCN",
+					"expanded_url": "https://twitter.com/Pal_action/status/1403268373018972160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1403265320534610000,
+					"id_str": "1403265320534609920",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/E3lmX8wXEAA9Hij.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3lmX8wXEAA9Hij.jpg",
+					"url": "https://t.co/43ecYrihCN",
+					"display_url": "pic.twitter.com/43ecYrihCN",
+					"expanded_url": "https://twitter.com/Pal_action/status/1403268373018972160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288252277187371000,
+			"id_str": "1288252277187371012",
+			"name": "Palestine Action",
+			"screen_name": "Pal_action",
+			"location": "",
+			"description": "A direct action network demanding an end to UK complicity with Israeli apartheid #ShutElbitDown",
+			"url": "https://t.co/TU8dkuMcaK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TU8dkuMcaK",
+							"expanded_url": "https://bit.ly/JoinResist",
+							"display_url": "bit.ly/JoinResist",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18305,
+			"friends_count": 289,
+			"listed_count": 94,
+			"created_at": "Tue Jul 28 23:17:42 +0000 2020",
+			"favourites_count": 4176,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 992,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344678880523771906/JDNolOpI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344678880523771906/JDNolOpI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288252277187371012/1607279419",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 98,
+		"favorite_count": 150,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Supplier for Israel's largest arms company, APPH, tries to deny any association with the murderous Elbit Systems. Unsurprisingly, their comments to the BBC are filled with lies and attempts to cover their complicit in murder. THREAD https://t.co/43ecYrihCN"
+	},
+	{
 		"created_at": "Fri Jun 11 11:08:54 +0000 2021",
 		"id": 1403274781374431200,
 		"id_str": "1403308252004241411",
