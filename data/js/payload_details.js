71a3316977a58dd173a72b0fe671be97555593a5
muranava <muraleeatjena@yahoo.co.uk>
2021-08-01 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20311,
-	"created_at": "Sat Jul 31 01:14:36 +0000 2021",
+	"tweets": 20312,
+	"created_at": "Sun Aug 01 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sat Jul 31 10:12:37 +0000 2021",
+		"id": 1421259450015420400,
+		"id_str": "1421413481388126211",
+		"full_text": "The \"Existential Risk\" grift: \"So-called rationalists have created a disturbing secular religion that looks like it addresses humanity’s deepest problems, but actually justifies pursuing the social preferences of elites.\" https://t.co/pCx8fgwjGZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pCx8fgwjGZ",
+					"expanded_url": "https://www.currentaffairs.org/2021/07/the-dangerous-ideas-of-longtermism-and-existential-risk",
+					"display_url": "currentaffairs.org/2021/07/the-da…",
+					"indices": [
+						222,
+						245
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 25821918,
+			"id_str": "25821918",
+			"name": "RationalWiki",
+			"screen_name": "RationalWiki",
+			"location": "All over the world",
+			"description": "Analyzing and refuting the anti-science movement, crank ideas, medical woo, charlatans, religious and political extremists...with goat.",
+			"url": "http://t.co/ILQnbFXdue",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/ILQnbFXdue",
+							"expanded_url": "http://rationalwiki.org",
+							"display_url": "rationalwiki.org",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12055,
+			"friends_count": 195,
+			"listed_count": 161,
+			"created_at": "Sun Mar 22 14:17:50 +0000 2009",
+			"favourites_count": 31084,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27783,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1868722111/600px-New_logo_large_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1868722111/600px-New_logo_large_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/25821918/1510344421",
+			"profile_link_color": "00D0F0",
+			"profile_sidebar_border_color": "03798B",
+			"profile_sidebar_fill_color": "C0ECF0",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 68,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The \"Existential Risk\" grift: \"So-called rationalists have created a disturbing secular religion that looks like it addresses humanity’s deepest problems, but actually justifies pursuing the social preferences of elites.\" https://t.co/pCx8fgwjGZ"
+	},
+	{
 		"created_at": "Fri Jul 30 20:03:54 +0000 2021",
 		"id": 1421197643691221000,
 		"id_str": "1421199895223054344",
