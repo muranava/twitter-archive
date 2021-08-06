74a557416252cee64e3d9ab8f882e3d7721a96bb
muranava <muraleeatjena@yahoo.co.uk>
2021-08-06 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20316,
-	"created_at": "Wed Aug 04 01:14:35 +0000 2021",
+	"tweets": 20317,
+	"created_at": "Fri Aug 06 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 05 15:59:22 +0000 2021",
+		"id": 1423312685148561400,
+		"id_str": "1423312685148561409",
+		"full_text": "\"What is left is a more individualistic anti-racist culture, which is keen on checking privilege and affirming the validity of other people’s experiences, but has trouble creating durable institutions or political programmes.\" describes some ling comments to Digby accent story https://t.co/yE2V07FVEC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/yE2V07FVEC",
+					"expanded_url": "https://twitter.com/BenHCarrington/status/1423234634905788417",
+					"display_url": "twitter.com/BenHCarrington…",
+					"indices": [
+						278,
+						301
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2088,
+			"friends_count": 2384,
+			"listed_count": 100,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 17567,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 711,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1419958763172282377/IOhC2pAI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1419958763172282377/IOhC2pAI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1423234634905788400,
+		"quoted_status_id_str": "1423234634905788417",
+		"quoted_status_permalink": {
+			"url": "https://t.co/yE2V07FVEC",
+			"expanded": "https://twitter.com/BenHCarrington/status/1423234634905788417",
+			"display": "twitter.com/BenHCarrington…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Aug 05 10:49:14 +0000 2021",
+			"id": 1423234634905788400,
+			"id_str": "1423234634905788417",
+			"full_text": "\"In 2019, Gilroy won the Holberg prize, the Nobel prize of the humanities. (Not a single British newspaper covered it.)\"\n\nWe sadly live in a shallow age wherein true intellectuals, like Gilroy, are less well known than your average \"twitter intellectual\".\n https://t.co/eL2BNZg4nf",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/eL2BNZg4nf",
+						"expanded_url": "https://www.theguardian.com/news/2021/aug/05/paul-gilroy-britain-scholar-race-humanism-vital-guide-age-of-crisis",
+						"display_url": "theguardian.com/news/2021/aug/…",
+						"indices": [
+							257,
+							280
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 496385648,
+				"id_str": "496385648",
+				"name": "Ben Carrington",
+				"screen_name": "BenHCarrington",
+				"location": "Los Angeles, CA",
+				"description": "A working-class hero is something to be.\nSouth Londoner (\"I'm so London, I'm so south\").\nOccasional Footballer & Cricketer.\nProfessor of Sociology & Journalism.",
+				"url": "http://t.co/rSJfHssN6l",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/rSJfHssN6l",
+								"expanded_url": "http://www.bencarrington.com",
+								"display_url": "bencarrington.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 14303,
+				"friends_count": 99,
+				"listed_count": 259,
+				"created_at": "Sat Feb 18 22:21:00 +0000 2012",
+				"favourites_count": 106260,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 28283,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/923600877276995584/YHtW0M0z_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/923600877276995584/YHtW0M0z_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/496385648/1456682806",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 20,
+			"favorite_count": 65,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"What is left is a more individualistic anti-racist culture, which is keen on checking privilege and affirming the validity of other people’s experiences, but has trouble creating durable institutions or political programmes.\" describes some ling comments to Digby accent story https://t.co/yE2V07FVEC"
+	},
+	{
 		"created_at": "Tue Aug 03 16:08:39 +0000 2021",
 		"id": 1422590243748855800,
 		"id_str": "1422590243748855812",
