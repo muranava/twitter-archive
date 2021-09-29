d0da67ddb86fa11cefd5788e92f6b1a2d1ec359c
muranava <muraleeatjena@yahoo.co.uk>
2021-09-29 03:14:36 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20361,
-	"created_at": "Tue Sep 28 01:14:35 +0000 2021",
+	"tweets": 20363,
+	"created_at": "Wed Sep 29 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,385 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Tue Sep 28 10:29:58 +0000 2021",
+		"id": 1442798733167665200,
+		"id_str": "1442798733167665154",
+		"full_text": "@RivkahBrown no suggestions but how about some pre-food/after-food reading? - The Whiteness of French Food. Law, Race, and Eating Culture in France https://t.co/1fhLKXNj33",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			171
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RivkahBrown",
+					"name": "Rivkah Brown",
+					"id": 585482512,
+					"id_str": "585482512",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1fhLKXNj33",
+					"expanded_url": "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3819684",
+					"display_url": "papers.ssrn.com/sol3/papers.cf…",
+					"indices": [
+						148,
+						171
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1442795778410487800,
+		"in_reply_to_status_id_str": "1442795778410487813",
+		"in_reply_to_user_id": 585482512,
+		"in_reply_to_user_id_str": "585482512",
+		"in_reply_to_screen_name": "RivkahBrown",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2093,
+			"friends_count": 2442,
+			"listed_count": 102,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 18062,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 776,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1419958763172282377/IOhC2pAI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1419958763172282377/IOhC2pAI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@RivkahBrown no suggestions but how about some pre-food/after-food reading? - The Whiteness of French Food. Law, Race, and Eating Culture in France https://t.co/1fhLKXNj33"
+	},
+	{
+		"created_at": "Tue Sep 28 09:52:10 +0000 2021",
+		"id": 1441355757413286000,
+		"id_str": "1442789221262106626",
+		"full_text": "Suhaiymah Manzoor-Kahn @thebrownhijabi has just performed the first reading of a specially commissioned poem @ #DwBW3, based on discussions @ #DwBW2 of healthcare for mobile &amp; marginalised populations \n\nThe vocabulary of neglect\n\nFull version here:\nhttps://t.co/owAicLh07J https://t.co/Ert8YbBfeJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DwBW3",
+					"indices": [
+						111,
+						117
+					]
+				},
+				{
+					"text": "DwBW2",
+					"indices": [
+						142,
+						148
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thebrownhijabi",
+					"name": "Suhaiymah Manzoor-Khan",
+					"id": 3339182475,
+					"id_str": "3339182475",
+					"indices": [
+						23,
+						38
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/owAicLh07J",
+					"expanded_url": "https://youtu.be/y_FNHJ3JhMQ",
+					"display_url": "youtu.be/y_FNHJ3JhMQ",
+					"indices": [
+						253,
+						276
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1441354907710152700,
+					"id_str": "1441354907710152707",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1441354907710152707/pu/img/oRGkCtMfvIznFgrd.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1441354907710152707/pu/img/oRGkCtMfvIznFgrd.jpg",
+					"url": "https://t.co/Ert8YbBfeJ",
+					"display_url": "pic.twitter.com/Ert8YbBfeJ",
+					"expanded_url": "https://twitter.com/doctors_borders/status/1441355757413285890/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1441354907710152700,
+					"id_str": "1441354907710152707",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1441354907710152707/pu/img/oRGkCtMfvIznFgrd.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1441354907710152707/pu/img/oRGkCtMfvIznFgrd.jpg",
+					"url": "https://t.co/Ert8YbBfeJ",
+					"display_url": "pic.twitter.com/Ert8YbBfeJ",
+					"expanded_url": "https://twitter.com/doctors_borders/status/1441355757413285890/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 124320,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1441354907710152707/pu/vid/1280x720/CAobsAtYglU4ngHP.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1441354907710152707/pu/pl/nLMa92HoWCBKo6nG.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1441354907710152707/pu/vid/640x360/GpcPc2IzNizcYwrF.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1441354907710152707/pu/vid/480x270/nh5MC1CmcPKlKgFM.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1187746538560340000,
+			"id_str": "1187746538560339970",
+			"name": "Health Mobilities & Borders",
+			"screen_name": "doctors_borders",
+			"location": "",
+			"description": "A @wellcometrust funded network (aka Doctors within Borders) for those interested in the healthcare experiences of migrants, refugees and others on the move.",
+			"url": "https://t.co/y7DLlwne5L",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y7DLlwne5L",
+							"expanded_url": "http://www.lancaster.ac.uk/doctors-within-borders",
+							"display_url": "lancaster.ac.uk/doctors-within…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 348,
+			"friends_count": 240,
+			"listed_count": 2,
+			"created_at": "Fri Oct 25 15:04:10 +0000 2019",
+			"favourites_count": 518,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 455,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1197881457684992000/Gst9giz0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1197881457684992000/Gst9giz0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1187746538560339970/1574336649",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 31,
+		"favorite_count": 82,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Suhaiymah Manzoor-Kahn @thebrownhijabi has just performed the first reading of a specially commissioned poem @ #DwBW3, based on discussions @ #DwBW2 of healthcare for mobile &amp; marginalised populations \n\nThe vocabulary of neglect\n\nFull version here:\nhttps://t.co/owAicLh07J https://t.co/Ert8YbBfeJ"
+	},
+	{
 		"created_at": "Mon Sep 27 21:54:04 +0000 2021",
 		"id": 1442608505622573000,
 		"id_str": "1442608505622573059",
