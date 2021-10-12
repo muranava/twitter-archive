c1eec3ecff15ea4b1bc0a3322327ee2be7ae421f
muranava <muraleeatjena@yahoo.co.uk>
2021-10-12 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20392,
-	"created_at": "Mon Oct 11 01:14:35 +0000 2021",
+	"tweets": 20393,
+	"created_at": "Tue Oct 12 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Mon Oct 11 11:01:32 +0000 2021",
+		"id": 1447449865210970000,
+		"id_str": "1447517720401039365",
+		"full_text": "The #TeflWorkersUnion is taking Anglo-Continental School in Bournemouth to tribunal alleging the school has been using bogus fixed-term contracts @angloowl: Permanent contracts now! Compensate staff unfairly made redundant! #tefl #elt @TeflUnion pls r/t",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TeflWorkersUnion",
+					"indices": [
+						4,
+						21
+					]
+				},
+				{
+					"text": "tefl",
+					"indices": [
+						224,
+						229
+					]
+				},
+				{
+					"text": "elt",
+					"indices": [
+						230,
+						234
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "angloowl",
+					"name": "Anglo-Continental",
+					"id": 255523629,
+					"id_str": "255523629",
+					"indices": [
+						146,
+						155
+					]
+				},
+				{
+					"screen_name": "TeflUnion",
+					"name": "TEFL Workers' Union",
+					"id": 1130578765472501800,
+					"id_str": "1130578765472501760",
+					"indices": [
+						235,
+						245
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 88202140,
+			"id_str": "88202140",
+			"name": "hugh dellar",
+			"screen_name": "hughdellar",
+			"location": "London, England",
+			"description": "Teacher, teacher trainer, writer, raconteur and general bon viveur.",
+			"url": "http://t.co/EgUuZbUBLl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/EgUuZbUBLl",
+							"expanded_url": "http://www.lexicallab.com",
+							"display_url": "lexicallab.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6068,
+			"friends_count": 2941,
+			"listed_count": 97,
+			"created_at": "Sat Nov 07 15:05:24 +0000 2009",
+			"favourites_count": 10166,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9585,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2040881292/Hugh_Dellar_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2040881292/Hugh_Dellar_photo_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "The #TeflWorkersUnion is taking Anglo-Continental School in Bournemouth to tribunal alleging the school has been using bogus fixed-term contracts @angloowl: Permanent contracts now! Compensate staff unfairly made redundant! #tefl #elt @TeflUnion pls r/t"
+	},
+	{
 		"created_at": "Sun Oct 10 17:02:24 +0000 2021",
 		"id": 1447246147974729700,
 		"id_str": "1447246147974729729",
