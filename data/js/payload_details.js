935fa0240a38342c0e4bf691321ce10c3c8a72a1
muranava <muraleeatjena@yahoo.co.uk>
2021-10-30 03:14:36 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20428,
-	"created_at": "Fri Oct 29 01:14:36 +0000 2021",
+	"tweets": 20430,
+	"created_at": "Sat Oct 30 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 30 00:11:23 +0000 2021",
+		"id": 1454239471256363000,
+		"id_str": "1454239471256363011",
+		"full_text": "\"In 2019 alone, the global sales of English Language Teaching (ELT) books published in the UK totaled 58.7 million units (Watson, 2021).\" https://t.co/xEuhtisZbP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			161
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xEuhtisZbP",
+					"expanded_url": "https://www.sciencedirect.com/science/article/pii/S0346251X2100230X#bib82",
+					"display_url": "sciencedirect.com/science/articl…",
+					"indices": [
+						138,
+						161
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/e8kZronG5C   Quick Cups of COCA ebook:https://t.co/5QyzQm27Ej",
+			"url": "http://t.co/efJ8SaX9fj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/efJ8SaX9fj",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/e8kZronG5C",
+							"expanded_url": "https://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/5QyzQm27Ej",
+							"expanded_url": "https://www.smashwords.com/books/view/618387",
+							"display_url": "smashwords.com/books/view/618…",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2095,
+			"friends_count": 2469,
+			"listed_count": 102,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 18405,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 860,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1419958763172282377/IOhC2pAI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1419958763172282377/IOhC2pAI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1456265654",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"In 2019 alone, the global sales of English Language Teaching (ELT) books published in the UK totaled 58.7 million units (Watson, 2021).\" https://t.co/xEuhtisZbP"
+	},
+	{
+		"created_at": "Fri Oct 29 09:20:28 +0000 2021",
+		"id": 1454010557204476000,
+		"id_str": "1454015264702488581",
+		"full_text": "1. Some thoughts on the 'metaverse' and the capitalist university.  First that capitalists and the capitalist university are constantly looking for ways to increase profit / surplus and the 'metaverse' will only succeed or fail by those criteria.  It's not inevitable.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 325419254,
+			"id_str": "325419254",
+			"name": "John Preston",
+			"screen_name": "Professor_John",
+			"location": "Colchester, England",
+			"description": "Professor of Sociology, University of Essex. Interests: Disasters, Existential Threat, Skills and Education. Working Class Academic. Own views.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3313,
+			"friends_count": 5001,
+			"listed_count": 32,
+			"created_at": "Tue Jun 28 08:04:04 +0000 2011",
+			"favourites_count": 19628,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6512,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "281B0E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1422505269934927872/LixhN1CA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1422505269934927872/LixhN1CA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/325419254/1630391696",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "76B78D",
+			"profile_sidebar_fill_color": "FDBC72",
+			"profile_text_color": "292323",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 12,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "1. Some thoughts on the 'metaverse' and the capitalist university.  First that capitalists and the capitalist university are constantly looking for ways to increase profit / surplus and the 'metaverse' will only succeed or fail by those criteria.  It's not inevitable."
+	},
+	{
 		"created_at": "Thu Oct 28 14:38:39 +0000 2021",
 		"id": 1453732951263367200,
 		"id_str": "1453732951263367175",
