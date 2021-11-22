5df9d7ce997f6870ad414bdda28d844a64601d19
muranava <muraleeatjena@yahoo.co.uk>
2021-11-22 02:14:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20461,
-	"created_at": "Sun Nov 21 01:14:35 +0000 2021",
+	"tweets": 20462,
+	"created_at": "Mon Nov 22 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sun Nov 21 17:11:44 +0000 2021",
+		"id": 1462066794252972000,
+		"id_str": "1462468785307271170",
+		"full_text": "A new documentary about the life and work of sociolinguist Jan Blommaert created by his friends and colleagues shortly before he passed away in January this year. Jan reflects on his past research at the intersection of \"language x power x activism\"\nhttps://t.co/Ct50ItP2pd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ct50ItP2pd",
+					"expanded_url": "https://www.docwerkers.be/en/docu/jan-blommaert/",
+					"display_url": "docwerkers.be/en/docu/jan-bl…",
+					"indices": [
+						250,
+						273
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 840514560968745000,
+			"id_str": "840514560968744961",
+			"name": "Gavin Lamb",
+			"screen_name": "gavinmlamb",
+			"location": "Oslo",
+			"description": "sociolinguist/discourse analyst | writing about ecolinguistics, ecocultural comm🐢 PhD @uhmanoa | postdoc @MultiLingOslo | neat stuff I find: https://t.co/hilFsHqpzu",
+			"url": "https://t.co/CHLeGw6GQ8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CHLeGw6GQ8",
+							"expanded_url": "https://www.gavinlamb.org/",
+							"display_url": "gavinlamb.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/hilFsHqpzu",
+							"expanded_url": "http://bit.ly/35EYBHi",
+							"display_url": "bit.ly/35EYBHi",
+							"indices": [
+								141,
+								164
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 595,
+			"friends_count": 621,
+			"listed_count": 10,
+			"created_at": "Sat Mar 11 10:47:42 +0000 2017",
+			"favourites_count": 2195,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 873,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1402835115907031043/3xFLwD8v_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1402835115907031043/3xFLwD8v_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/840514560968744961/1622234960",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24,
+		"favorite_count": 66,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A new documentary about the life and work of sociolinguist Jan Blommaert created by his friends and colleagues shortly before he passed away in January this year. Jan reflects on his past research at the intersection of \"language x power x activism\"\nhttps://t.co/Ct50ItP2pd"
+	},
+	{
 		"created_at": "Sat Nov 20 13:08:26 +0000 2021",
 		"id": 1462045169625227300,
 		"id_str": "1462045169625227268",
