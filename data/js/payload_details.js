990aff5aaad835d66630aef8c8a3aaa8b04fbe63
muranava <muraleeatjena@yahoo.co.uk>
2021-12-24 02:14:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20523,
-	"created_at": "Wed Dec 22 01:14:37 +0000 2021",
+	"tweets": 20524,
+	"created_at": "Fri Dec 24 01:14:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Thu Dec 23 16:29:34 +0000 2021",
+		"id": 1473961415371534300,
+		"id_str": "1474054584767328262",
+		"full_text": "So I tweeted this picture yesterday of myself striking and giving a speech during a demonstration on Dec 21st. Here's a 🧵 on what this is all about. 1/n https://t.co/vcjsoIKvUO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1473961409134608400,
+					"id_str": "1473961409134608385",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/media/FHSQF5waQAEiiRF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHSQF5waQAEiiRF.jpg",
+					"url": "https://t.co/vcjsoIKvUO",
+					"display_url": "pic.twitter.com/vcjsoIKvUO",
+					"expanded_url": "https://twitter.com/Za_Maikeru/status/1473961415371534340/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1515,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 449,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 792,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1473961409134608400,
+					"id_str": "1473961409134608385",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/media/FHSQF5waQAEiiRF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHSQF5waQAEiiRF.jpg",
+					"url": "https://t.co/vcjsoIKvUO",
+					"display_url": "pic.twitter.com/vcjsoIKvUO",
+					"expanded_url": "https://twitter.com/Za_Maikeru/status/1473961415371534340/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1515,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 449,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 792,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 703598479944208400,
+			"id_str": "703598479944208384",
+			"name": "Michael Brown",
+			"screen_name": "Za_Maikeru",
+			"location": "Tokyo",
+			"description": "ecolinguistics, corpus ling, language ed, (socio)ling variation, uni lecturer (in 日本), phd student, ‘he’ pronouns, climate organizer, union exec",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 379,
+			"friends_count": 702,
+			"listed_count": 12,
+			"created_at": "Sat Feb 27 15:12:04 +0000 2016",
+			"favourites_count": 20384,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1939,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1047074779063304194/Ef3AXGL1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1047074779063304194/Ef3AXGL1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/703598479944208384/1600736856",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 23,
+		"favorite_count": 60,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "So I tweeted this picture yesterday of myself striking and giving a speech during a demonstration on Dec 21st. Here's a 🧵 on what this is all about. 1/n https://t.co/vcjsoIKvUO"
+	},
+	{
 		"created_at": "Tue Dec 21 17:29:30 +0000 2021",
 		"id": 1473344890964783000,
 		"id_str": "1473344890964783110",
