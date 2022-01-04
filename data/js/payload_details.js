c4fccd6c62c233e6f67501affb9ab1140ede8288
muranava <muraleeatjena@yahoo.co.uk>
2022-01-04 02:14:36 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20535,
-	"created_at": "Sat Jan 01 01:14:35 +0000 2022",
+	"tweets": 20537,
+	"created_at": "Tue Jan 04 01:14:35 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_01.js
@@ -0,0 +1,435 @@
+Grailbird.data.tweets_2022_01 = 
+[
+	{
+		"created_at": "Mon Jan 03 13:48:33 +0000 2022",
+		"id": 1477941280013537300,
+		"id_str": "1478000332328341509",
+		"full_text": "“When I die, I want the white left to lower me into my grave, so they can let me down one last time.” - @SamanthaAsumadu  https://t.co/fajwW9FWxG #NationalityandBordersBill @therubykid @nurseboothroyd @suzanne_moore @GeorgeAylett @GeorgeMonbiot @tcmbigcheese @garyyounge @ukblm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NationalityandBordersBill",
+					"indices": [
+						146,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SamanthaAsumadu",
+					"name": "Samantha Asumadu",
+					"id": 1471938863480426500,
+					"id_str": "1471938863480426506",
+					"indices": [
+						104,
+						120
+					]
+				},
+				{
+					"screen_name": "therubykid",
+					"name": "Daniel Randall",
+					"id": 171456734,
+					"id_str": "171456734",
+					"indices": [
+						173,
+						184
+					]
+				},
+				{
+					"screen_name": "nurseboothroyd",
+					"name": "Mark Boothroyd #wearamask #NHSpay15",
+					"id": 1298778404,
+					"id_str": "1298778404",
+					"indices": [
+						185,
+						200
+					]
+				},
+				{
+					"screen_name": "suzanne_moore",
+					"name": "suzanne moore",
+					"id": 19654724,
+					"id_str": "19654724",
+					"indices": [
+						201,
+						215
+					]
+				},
+				{
+					"screen_name": "GeorgeAylett",
+					"name": "George Aylett",
+					"id": 470489295,
+					"id_str": "470489295",
+					"indices": [
+						216,
+						229
+					]
+				},
+				{
+					"screen_name": "GeorgeMonbiot",
+					"name": "George Monbiot",
+					"id": 198584761,
+					"id_str": "198584761",
+					"indices": [
+						230,
+						244
+					]
+				},
+				{
+					"screen_name": "tcmbigcheese",
+					"name": "pat younge",
+					"id": 90971293,
+					"id_str": "90971293",
+					"indices": [
+						245,
+						258
+					]
+				},
+				{
+					"screen_name": "garyyounge",
+					"name": "Gary Younge",
+					"id": 149272318,
+					"id_str": "149272318",
+					"indices": [
+						259,
+						270
+					]
+				},
+				{
+					"screen_name": "ukblm",
+					"name": "#BlackLivesMatterUK",
+					"id": 753996179298541600,
+					"id_str": "753996179298541568",
+					"indices": [
+						271,
+						277
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/fajwW9FWxG",
+					"expanded_url": "https://ceasefiremagazine.co.uk/nationality-borders-bill-uk-left/",
+					"display_url": "ceasefiremagazine.co.uk/nationality-bo…",
+					"indices": [
+						122,
+						145
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1471938863480426500,
+			"id_str": "1471938863480426506",
+			"name": "Samantha Asumadu",
+			"screen_name": "SamanthaAsumadu",
+			"location": "",
+			"description": "Radical organiser, writer and founder of @WritersofColour",
+			"url": "https://t.co/jan83h8AOd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jan83h8AOd",
+							"expanded_url": "https://linktr.ee/SamanthaAsumadu",
+							"display_url": "linktr.ee/SamanthaAsumadu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 553,
+			"friends_count": 586,
+			"listed_count": 1,
+			"created_at": "Fri Dec 17 20:22:36 +0000 2021",
+			"favourites_count": 418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1236,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1473073964314415105/dSkSW1uB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1473073964314415105/dSkSW1uB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1471938863480426506/1640679418",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 17,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“When I die, I want the white left to lower me into my grave, so they can let me down one last time.” - @SamanthaAsumadu  https://t.co/fajwW9FWxG #NationalityandBordersBill @therubykid @nurseboothroyd @suzanne_moore @GeorgeAylett @GeorgeMonbiot @tcmbigcheese @garyyounge @ukblm"
+	},
+	{
+		"created_at": "Mon Jan 03 12:16:07 +0000 2022",
+		"id": 1477969452310401000,
+		"id_str": "1477977071024586753",
+		"full_text": "Prince Andrew, who claims he had an \"overdose of adrenaline\" in the Falklands War, served on a ship which carried 12 nuclear weapons. \n\nSurely that would make anyone sweat? \n\nImportant new piece by @NortonTaylor for @declassifiedUK \n\nhttps://t.co/9v7KwFATrX @CNDuk @ScottishCND https://t.co/Mo2XhRz06X",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NortonTaylor",
+					"name": "Richard NortonTaylor",
+					"id": 135175358,
+					"id_str": "135175358",
+					"indices": [
+						198,
+						211
+					]
+				},
+				{
+					"screen_name": "declassifiedUK",
+					"name": "Declassified UK",
+					"id": 1084523178444099600,
+					"id_str": "1084523178444099584",
+					"indices": [
+						216,
+						231
+					]
+				},
+				{
+					"screen_name": "CNDuk",
+					"name": "CND",
+					"id": 27670162,
+					"id_str": "27670162",
+					"indices": [
+						258,
+						264
+					]
+				},
+				{
+					"screen_name": "ScottishCND",
+					"name": "Scottish CND",
+					"id": 137092309,
+					"id_str": "137092309",
+					"indices": [
+						265,
+						277
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9v7KwFATrX",
+					"expanded_url": "https://declassifieduk.org/uk-deployed-31-nuclear-weapons-during-falklands-war/",
+					"display_url": "declassifieduk.org/uk-deployed-31…",
+					"indices": [
+						234,
+						257
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1477969445545029600,
+					"id_str": "1477969445545029632",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FILNYU6XsAALUi5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FILNYU6XsAALUi5.jpg",
+					"url": "https://t.co/Mo2XhRz06X",
+					"display_url": "pic.twitter.com/Mo2XhRz06X",
+					"expanded_url": "https://twitter.com/pmillerinfo/status/1477969452310401024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 420,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1039,
+							"h": 1682,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 741,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1477969445545029600,
+					"id_str": "1477969445545029632",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FILNYU6XsAALUi5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FILNYU6XsAALUi5.jpg",
+					"url": "https://t.co/Mo2XhRz06X",
+					"display_url": "pic.twitter.com/Mo2XhRz06X",
+					"expanded_url": "https://twitter.com/pmillerinfo/status/1477969452310401024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 420,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1039,
+							"h": 1682,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 741,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2594641380,
+			"id_str": "2594641380",
+			"name": "Phil Miller",
+			"screen_name": "pmillerinfo",
+			"location": "",
+			"description": "Chief reporter and videos for @DeclassifiedUK.\n*To contact me securely* please use Protonmail and email: phil@declassifieduk.org",
+			"url": "https://t.co/nVdP30pKUG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nVdP30pKUG",
+							"expanded_url": "https://declassifieduk.org/author/phil-miller/",
+							"display_url": "declassifieduk.org/author/phil-mi…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12888,
+			"friends_count": 1451,
+			"listed_count": 87,
+			"created_at": "Sun Jun 29 11:32:29 +0000 2014",
+			"favourites_count": 1536,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5530,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1397879910711111683/NWWoT9gX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1397879910711111683/NWWoT9gX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2594641380/1631725270",
+			"profile_link_color": "89C9FA",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 36,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Prince Andrew, who claims he had an \"overdose of adrenaline\" in the Falklands War, served on a ship which carried 12 nuclear weapons. \n\nSurely that would make anyone sweat? \n\nImportant new piece by @NortonTaylor for @declassifiedUK \n\nhttps://t.co/9v7KwFATrX @CNDuk @ScottishCND https://t.co/Mo2XhRz06X"
+	}
+]
