aa73183c8d0dfc3c2ffbe9273164c84a603f043b
muranava <muraleeatjena@yahoo.co.uk>
2022-01-11 02:14:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20543,
-	"created_at": "Sun Jan 09 01:14:35 +0000 2022",
+	"tweets": 20544,
+	"created_at": "Tue Jan 11 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,128 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Mon Jan 10 14:28:05 +0000 2022",
+		"id": 1480542942506930200,
+		"id_str": "1480546994091401217",
+		"full_text": "BREAKING: Elbit Ferranti forced to sell and shut down their israeli weapons factory. After dozens of activists took action to occupy, blockade and destroy the site; we’ve #ShutElbitDown PERMANENTLY in #Oldham! https://t.co/eT6CmUOA8i",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			233
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ShutElbitDown",
+					"indices": [
+						171,
+						185
+					]
+				},
+				{
+					"text": "Oldham",
+					"indices": [
+						201,
+						208
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eT6CmUOA8i",
+					"expanded_url": "https://www.palestineaction.org/victory-in-oldham/",
+					"display_url": "palestineaction.org/victory-in-old…",
+					"indices": [
+						210,
+						233
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288252277187371000,
+			"id_str": "1288252277187371012",
+			"name": "Palestine Action",
+			"screen_name": "Pal_action",
+			"location": "",
+			"description": "A direct action network demanding an end to British complicity with Israeli apartheid #ShutElbitDown",
+			"url": "https://t.co/J55oNf6FzS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/J55oNf6FzS",
+							"expanded_url": "http://linktr.ee/pal_action",
+							"display_url": "linktr.ee/pal_action",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23367,
+			"friends_count": 316,
+			"listed_count": 134,
+			"created_at": "Tue Jul 28 23:17:42 +0000 2020",
+			"favourites_count": 5059,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1731,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344678880523771906/JDNolOpI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344678880523771906/JDNolOpI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288252277187371012/1627656494",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1890,
+		"favorite_count": 4618,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BREAKING: Elbit Ferranti forced to sell and shut down their israeli weapons factory. After dozens of activists took action to occupy, blockade and destroy the site; we’ve #ShutElbitDown PERMANENTLY in #Oldham! https://t.co/eT6CmUOA8i"
+	},
+	{
 		"created_at": "Sat Jan 08 13:27:29 +0000 2022",
 		"id": 1479805512325685200,
 		"id_str": "1479806967321350145",
