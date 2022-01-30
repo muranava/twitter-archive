2a60213a3b93d6b9f0dd3c5ab98d3db5c557c3d0
muranava <muraleeatjena@yahoo.co.uk>
2022-01-30 02:14:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20563,
-	"created_at": "Fri Jan 28 01:14:35 +0000 2022",
+	"tweets": 20564,
+	"created_at": "Sun Jan 30 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sat Jan 29 09:15:43 +0000 2022",
+		"id": 1487255903057367000,
+		"id_str": "1487353753200123904",
+		"full_text": "@BBCNews Peel fans should try this work of genius:\nhttps://t.co/6cqPVHIom8",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BBCNews",
+					"name": "BBC News (UK)",
+					"id": 612473,
+					"id_str": "612473",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/6cqPVHIom8",
+					"expanded_url": "https://www.monkeon.co.uk/johnpeelroulette/",
+					"display_url": "monkeon.co.uk/johnpeelroulet…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1487240256298664000,
+		"in_reply_to_status_id_str": "1487240256298663937",
+		"in_reply_to_user_id": 612473,
+		"in_reply_to_user_id_str": "612473",
+		"in_reply_to_screen_name": "BBCNews",
+		"user": {
+			"id": 2520357289,
+			"id_str": "2520357289",
+			"name": "Hugh Thompson",
+			"screen_name": "hughegthompson",
+			"location": "Liverpool - ish",
+			"description": "Bloke, expecting something to turn up, and it usually does.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 493,
+			"friends_count": 339,
+			"listed_count": 2,
+			"created_at": "Sat May 24 12:17:13 +0000 2014",
+			"favourites_count": 41094,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23081,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/470182984109129728/pKlzHI1u_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/470182984109129728/pKlzHI1u_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2520357289/1400937715",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@BBCNews Peel fans should try this work of genius:\nhttps://t.co/6cqPVHIom8"
+	},
+	{
 		"created_at": "Thu Jan 27 14:23:04 +0000 2022",
 		"id": 1486706327179214800,
 		"id_str": "1486706327179214851",
