3920a2a8d565815c868afd7154d2810a25be96bd
muranava <muraleeatjena@yahoo.co.uk>
2022-02-04 02:14:36 +0100

    Added 1 tweets

---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,96 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Thu Feb 03 12:00:04 +0000 2022",
+		"id": 1489204747039035400,
+		"id_str": "1489207053939888128",
+		"full_text": "New blog post: https://t.co/2RBkAP04Ck",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2RBkAP04Ck",
+					"expanded_url": "https://adaptivelearninginelt.wordpress.com/2022/02/03/the-long-covid-of-education/",
+					"display_url": "adaptivelearninginelt.wordpress.com/2022/02/03/the…",
+					"indices": [
+						15,
+						38
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 847708264666062800,
+			"id_str": "847708264666062848",
+			"name": "philipjkerr",
+			"screen_name": "philipjohnkerr",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 444,
+			"friends_count": 155,
+			"listed_count": 7,
+			"created_at": "Fri Mar 31 07:12:54 +0000 2017",
+			"favourites_count": 631,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 485,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New blog post: https://t.co/2RBkAP04Ck"
+	}
+]
