14360b0f81c4f42f9ab7db0ae768e54d8c924dc7
muranava <muraleeatjena@yahoo.co.uk>
2022-03-31 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20637,
-	"created_at": "Sat Mar 26 01:14:35 +0000 2022",
+	"tweets": 20638,
+	"created_at": "Thu Mar 31 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 30 16:48:53 +0000 2022",
+		"id": 1509209672464715800,
+		"id_str": "1509211069235048451",
+		"full_text": "Streamlining makes things easier to say fast. Some kinds of words are specially vulnerable to such simplifications - adverbs are one kind. For example, 'properly' may come out as 'proply' or 'It's actually' as 'sachly'. More here: https://t.co/gC41FUIwmb     #pronunciation https://t.co/DrF7Ragdh5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "pronunciation",
+					"indices": [
+						259,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gC41FUIwmb",
+					"expanded_url": "http://pronpack.com/41597-2/",
+					"display_url": "pronpack.com/41597-2/",
+					"indices": [
+						231,
+						254
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1509209392704540700,
+					"id_str": "1509209392704540684",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FPHJ8k_WUAw6QOd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPHJ8k_WUAw6QOd.jpg",
+					"url": "https://t.co/DrF7Ragdh5",
+					"display_url": "pic.twitter.com/DrF7Ragdh5",
+					"expanded_url": "https://twitter.com/HancockMcDonald/status/1509209672464715788/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1920,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1509209392704540700,
+					"id_str": "1509209392704540684",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FPHJ8k_WUAw6QOd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPHJ8k_WUAw6QOd.jpg",
+					"url": "https://t.co/DrF7Ragdh5",
+					"display_url": "pic.twitter.com/DrF7Ragdh5",
+					"expanded_url": "https://twitter.com/HancockMcDonald/status/1509209672464715788/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1920,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 552929354,
+			"id_str": "552929354",
+			"name": "Hancock McDonald ELT",
+			"screen_name": "HancockMcDonald",
+			"location": "UK",
+			"description": "Mark Hancock, Annie McDonald: ELT teachers and authors. Books include English Result and Pronunciation Games.",
+			"url": "http://t.co/txStd2Bowi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/txStd2Bowi",
+							"expanded_url": "http://hancockmcdonald.com",
+							"display_url": "hancockmcdonald.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1842,
+			"friends_count": 839,
+			"listed_count": 30,
+			"created_at": "Fri Apr 13 19:00:04 +0000 2012",
+			"favourites_count": 692,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 927,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2158919436/HM_portrait_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2158919436/HM_portrait_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/552929354/1398324578",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Streamlining makes things easier to say fast. Some kinds of words are specially vulnerable to such simplifications - adverbs are one kind. For example, 'properly' may come out as 'proply' or 'It's actually' as 'sachly'. More here: https://t.co/gC41FUIwmb     #pronunciation https://t.co/DrF7Ragdh5"
+	},
+	{
 		"created_at": "Fri Mar 25 13:26:59 +0000 2022",
 		"id": 1507334126721974300,
 		"id_str": "1507348321916047368",
