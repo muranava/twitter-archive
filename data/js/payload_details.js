85c5df4aa74ffdcbc390bb93533e58bcffee47aa
muranava <muraleeatjena@yahoo.co.uk>
2022-04-02 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20638,
-	"created_at": "Thu Mar 31 01:14:35 +0000 2022",
+	"tweets": 20639,
+	"created_at": "Sat Apr 02 01:14:35 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_04.js
@@ -0,0 +1,219 @@
+Grailbird.data.tweets_2022_04 = 
+[
+	{
+		"created_at": "Fri Apr 01 20:47:12 +0000 2022",
+		"id": 1509971892257632300,
+		"id_str": "1509995821852471299",
+		"full_text": "The moment Amazon workers at the JFK8 warehouse declared victory in their vote to form the first Amazon union in the United States https://t.co/pi6MlbK3lr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			130
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1509971625214681000,
+					"id_str": "1509971625214681097",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1509971625214681097/pu/img/6UgpXzkPRs5IpC7e.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1509971625214681097/pu/img/6UgpXzkPRs5IpC7e.jpg",
+					"url": "https://t.co/pi6MlbK3lr",
+					"display_url": "pic.twitter.com/pi6MlbK3lr",
+					"expanded_url": "https://twitter.com/BTnewsroom/status/1509971892257632257/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1509971625214681000,
+					"id_str": "1509971625214681097",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1509971625214681097/pu/img/6UgpXzkPRs5IpC7e.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1509971625214681097/pu/img/6UgpXzkPRs5IpC7e.jpg",
+					"url": "https://t.co/pi6MlbK3lr",
+					"display_url": "pic.twitter.com/pi6MlbK3lr",
+					"expanded_url": "https://twitter.com/BTnewsroom/status/1509971892257632257/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 85135,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1509971625214681097/pu/vid/480x270/Y_z_QnQlBWJT8pB-.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1509971625214681097/pu/vid/640x360/S5Lf4_Q29GIIVdDs.mp4?tag=14"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1509971625214681097/pu/vid/1280x720/LKHKU_nbG57M2JAq.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1509971625214681097/pu/pl/P2JuATFIDDN8gnk0.m3u8?tag=14&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1217207858435186700,
+			"id_str": "1217207858435186689",
+			"name": "BreakThrough News",
+			"screen_name": "BTnewsroom",
+			"location": "",
+			"description": "Media arm for the movement. Don't just watch us, join us. https://t.co/vbYSC8wOB3",
+			"url": "https://t.co/wP5bsbms04",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wP5bsbms04",
+							"expanded_url": "http://patreon.com/BreakThroughNews",
+							"display_url": "patreon.com/BreakThroughNe…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vbYSC8wOB3",
+							"expanded_url": "http://BreakThroughNews.org",
+							"display_url": "BreakThroughNews.org",
+							"indices": [
+								58,
+								81
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 56728,
+			"friends_count": 201,
+			"listed_count": 385,
+			"created_at": "Tue Jan 14 22:12:32 +0000 2020",
+			"favourites_count": 588,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5186,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1240344537958092812/Z-BrvhbV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1240344537958092812/Z-BrvhbV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1217207858435186689/1610128938",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 208,
+		"favorite_count": 672,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The moment Amazon workers at the JFK8 warehouse declared victory in their vote to form the first Amazon union in the United States https://t.co/pi6MlbK3lr"
+	}
+]
