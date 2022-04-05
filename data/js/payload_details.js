7538b40944d196162d93c123dedc383c2cc63020
muranava <muraleeatjena@yahoo.co.uk>
2022-04-05 03:14:35 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20640,
-	"created_at": "Sun Apr 03 01:14:35 +0000 2022",
+	"tweets": 20641,
+	"created_at": "Tue Apr 05 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Mon Apr 04 13:23:28 +0000 2022",
+		"id": 1510828862598434800,
+		"id_str": "1510971314974216197",
+		"full_text": "PRINCE AT AGE 11: WCCO stumbled upon a priceless film in our archives while looking through footage of the 1970 Minneapolis teachers strike. See the journey @Jeff_Wagner4 took to solve a Minnesota music mystery. |   https://t.co/dz7grOfpQW https://t.co/mnX082M0bc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			239
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Jeff_Wagner4",
+					"name": "Jeff Wagner",
+					"id": 277538408,
+					"id_str": "277538408",
+					"indices": [
+						157,
+						170
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/dz7grOfpQW",
+					"expanded_url": "https://cbsloc.al/36LHSGJ",
+					"display_url": "cbsloc.al/36LHSGJ",
+					"indices": [
+						216,
+						239
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1510828858966163500,
+					"id_str": "1510828858966163456",
+					"indices": [
+						240,
+						263
+					],
+					"media_url": "http://pbs.twimg.com/media/FPeK168WQAAm_Wf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPeK168WQAAm_Wf.jpg",
+					"url": "https://t.co/mnX082M0bc",
+					"display_url": "pic.twitter.com/mnX082M0bc",
+					"expanded_url": "https://twitter.com/WCCO/status/1510828862598434823/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1510828858966163500,
+					"id_str": "1510828858966163456",
+					"indices": [
+						240,
+						263
+					],
+					"media_url": "http://pbs.twimg.com/media/FPeK168WQAAm_Wf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPeK168WQAAm_Wf.jpg",
+					"url": "https://t.co/mnX082M0bc",
+					"display_url": "pic.twitter.com/mnX082M0bc",
+					"expanded_url": "https://twitter.com/WCCO/status/1510828862598434823/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://www.socialnewsdesk.com\" rel=\"nofollow\">SocialNewsDesk</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13000872,
+			"id_str": "13000872",
+			"name": "WCCO - CBS Minnesota",
+			"screen_name": "WCCO",
+			"location": "Minneapolis, MN",
+			"description": "Thanks for your interest in WCCO | CBS News Minnesota - the place for local news updates! Want more? Head to https://t.co/ecqUashNGT or our WCCO-TV Facebook page.",
+			"url": "https://t.co/snyfTfFZsh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/snyfTfFZsh",
+							"expanded_url": "http://wcco.com/contact",
+							"display_url": "wcco.com/contact",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ecqUashNGT",
+							"expanded_url": "http://WCCO.com",
+							"display_url": "WCCO.com",
+							"indices": [
+								109,
+								132
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 244049,
+			"friends_count": 6850,
+			"listed_count": 2174,
+			"created_at": "Sun Feb 03 05:22:48 +0000 2008",
+			"favourites_count": 487,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 162278,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B8BAC1",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1280449371906043904/-UwL30Ys_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1280449371906043904/-UwL30Ys_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13000872/1618512652",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "EFEFF0",
+			"profile_text_color": "666666",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2062,
+		"favorite_count": 6607,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "PRINCE AT AGE 11: WCCO stumbled upon a priceless film in our archives while looking through footage of the 1970 Minneapolis teachers strike. See the journey @Jeff_Wagner4 took to solve a Minnesota music mystery. |   https://t.co/dz7grOfpQW https://t.co/mnX082M0bc"
+	},
+	{
 		"created_at": "Sat Apr 02 07:23:50 +0000 2022",
 		"id": 1510151341250859000,
 		"id_str": "1510156033070702595",
