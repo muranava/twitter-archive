6857b7f19c74fa3d4ba24a0849c58b7789d36602
muranava <muraleeatjena@yahoo.co.uk>
2022-04-21 03:14:38 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20673,
-	"created_at": "Tue Apr 19 01:14:35 +0000 2022",
+	"tweets": 20674,
+	"created_at": "Thu Apr 21 01:14:37 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,225 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Wed Apr 20 18:34:42 +0000 2022",
+		"id": 1516726739413749800,
+		"id_str": "1516847843054698499",
+		"full_text": "👀 UK MPs and Lords have asked 1,055 written questions on Ukraine since the beginning of the year, compared to just 46 on Yemen.\n\n#DCUKparliament https://t.co/RuBgeE1R2Y",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DCUKparliament",
+					"indices": [
+						129,
+						144
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1516726052743176200,
+					"id_str": "1516726052743176193",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/FQx-TxtWYAEYZtC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQx-TxtWYAEYZtC.jpg",
+					"url": "https://t.co/RuBgeE1R2Y",
+					"display_url": "pic.twitter.com/RuBgeE1R2Y",
+					"expanded_url": "https://twitter.com/declassifiedUK/status/1516726739413749767/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 806,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1221,
+							"h": 820,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 457,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1516726052743176200,
+					"id_str": "1516726052743176193",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/FQx-TxtWYAEYZtC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQx-TxtWYAEYZtC.jpg",
+					"url": "https://t.co/RuBgeE1R2Y",
+					"display_url": "pic.twitter.com/RuBgeE1R2Y",
+					"expanded_url": "https://twitter.com/declassifiedUK/status/1516726739413749767/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 806,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1221,
+							"h": 820,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 457,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1516726084053708800,
+					"id_str": "1516726084053708805",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/FQx-VmWXMAUFYjY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQx-VmWXMAUFYjY.jpg",
+					"url": "https://t.co/RuBgeE1R2Y",
+					"display_url": "pic.twitter.com/RuBgeE1R2Y",
+					"expanded_url": "https://twitter.com/declassifiedUK/status/1516726739413749767/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 810,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1216,
+							"h": 821,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 459,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1084523178444099600,
+			"id_str": "1084523178444099584",
+			"name": "Declassified UK",
+			"screen_name": "declassifiedUK",
+			"location": "United Kingdom",
+			"description": "Uncovering Britain's role in the world. Public service journalism investigating UK foreign, military, intelligence & economic policies. Edited by @MarkCurtis30",
+			"url": "https://t.co/LuLfFeMKvi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LuLfFeMKvi",
+							"expanded_url": "https://linktr.ee/DeclassifiedUK",
+							"display_url": "linktr.ee/DeclassifiedUK",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 82074,
+			"friends_count": 1074,
+			"listed_count": 600,
+			"created_at": "Sun Jan 13 18:50:50 +0000 2019",
+			"favourites_count": 2234,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 6810,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1313032041617817600/8FKh5TW4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1313032041617817600/8FKh5TW4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1084523178444099584/1601879931",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 49,
+		"favorite_count": 67,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "👀 UK MPs and Lords have asked 1,055 written questions on Ukraine since the beginning of the year, compared to just 46 on Yemen.\n\n#DCUKparliament https://t.co/RuBgeE1R2Y"
+	},
+	{
 		"created_at": "Mon Apr 18 08:29:56 +0000 2022",
 		"id": 1515963395580776400,
 		"id_str": "1515970872527437825",
