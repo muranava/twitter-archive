8c4b0f7a30de8ded7d5ab7bacf7c8cf3eae10846
muranava <muraleeatjena@yahoo.co.uk>
2022-05-02 03:14:36 +0200

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20688,
-	"created_at": "Sun May 01 01:14:35 +0000 2022",
+	"tweets": 20691,
+	"created_at": "Mon May 02 01:14:35 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,389 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Sun May 01 13:20:18 +0000 2022",
+		"id": 1520040153582125000,
+		"id_str": "1520754991186817036",
+		"full_text": "This week the Tories have pushed through a series of deeply reactionary laws that will:\n\n▪️Accelerate NHS privatisation\n▪️Rip up rights for refugees  \n▪️Criminalise 'noisy' protests\n▪️Disenfranchise citizens through mandatory voter ID\n\nWe're in a very dangerous moment.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 545081356,
+			"id_str": "545081356",
+			"name": "Richard Burgon MP",
+			"screen_name": "RichardBurgon",
+			"location": "Leeds, West Yorkshire",
+			"description": "Labour MP for East Leeds. Leeds born and bred. Secretary of the Socialist Campaign Group of Labour MPs🌹 E-mail for constituents: richard@richardburgon.com",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 169692,
+			"friends_count": 2172,
+			"listed_count": 1066,
+			"created_at": "Wed Apr 04 12:16:05 +0000 2012",
+			"favourites_count": 6708,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 18553,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FAFAFA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1339534463173332992/oKvptzuI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1339534463173332992/oKvptzuI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/545081356/1624470309",
+			"profile_link_color": "07090A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1406,
+		"favorite_count": 3392,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "This week the Tories have pushed through a series of deeply reactionary laws that will:\n\n▪️Accelerate NHS privatisation\n▪️Rip up rights for refugees  \n▪️Criminalise 'noisy' protests\n▪️Disenfranchise citizens through mandatory voter ID\n\nWe're in a very dangerous moment."
+	},
+	{
+		"created_at": "Sun May 01 10:36:09 +0000 2022",
+		"id": 1520478824881959000,
+		"id_str": "1520713680152838147",
+		"full_text": "Research in brief: Communicative and task-based language teaching in East Asian classrooms https://t.co/lUVEy7KXD0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lUVEy7KXD0",
+					"expanded_url": "https://eltplanning.com/2022/04/30/research-in-brief-communicative-and-task-based-language-teaching-in-east-asian-classrooms/",
+					"display_url": "eltplanning.com/2022/04/30/res…",
+					"indices": [
+						91,
+						114
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2303885810,
+			"id_str": "2303885810",
+			"name": "TESOL Webinars",
+			"screen_name": "ESLWebinars",
+			"location": "Worldwide",
+			"description": "Explore resources, tips, & tools for teachers with American TESOL Webinars @TESOL @AmericanTESOL Soc Med Mgr @ShellTerrell https://t.co/xOopPpief2",
+			"url": "https://t.co/fwZDkSPH9k",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fwZDkSPH9k",
+							"expanded_url": "http://www.americantesol.com",
+							"display_url": "americantesol.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/xOopPpief2",
+							"expanded_url": "http://Americantesol.com/tesol-lectures.htm",
+							"display_url": "Americantesol.com/tesol-lectures…",
+							"indices": [
+								123,
+								146
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 5900,
+			"friends_count": 4602,
+			"listed_count": 93,
+			"created_at": "Tue Jan 21 23:57:21 +0000 2014",
+			"favourites_count": 169,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 16425,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/644160170554191872/KLuQ3RYk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/644160170554191872/KLuQ3RYk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2303885810/1442940262",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Research in brief: Communicative and task-based language teaching in East Asian classrooms https://t.co/lUVEy7KXD0"
+	},
+	{
+		"created_at": "Sun May 01 10:28:27 +0000 2022",
+		"id": 1520501312944808000,
+		"id_str": "1520711742443110401",
+		"full_text": "Russia tv goes after the foggy chip-and-fish munching marshlanders \n\n(via @francis_scarr) https://t.co/vr2RmEUz1Y",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "francis_scarr",
+					"name": "Francis Scarr",
+					"id": 824325992827252700,
+					"id_str": "824325992827252736",
+					"indices": [
+						74,
+						88
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1520501306238161000,
+					"id_str": "1520501306238160896",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/FRnn4c7X0AAM1Ln.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRnn4c7X0AAM1Ln.jpg",
+					"url": "https://t.co/vr2RmEUz1Y",
+					"display_url": "pic.twitter.com/vr2RmEUz1Y",
+					"expanded_url": "https://twitter.com/shaunwalker7/status/1520501312944807936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1019,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1739,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 577,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1520501306238161000,
+					"id_str": "1520501306238160896",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/FRnn4c7X0AAM1Ln.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRnn4c7X0AAM1Ln.jpg",
+					"url": "https://t.co/vr2RmEUz1Y",
+					"display_url": "pic.twitter.com/vr2RmEUz1Y",
+					"expanded_url": "https://twitter.com/shaunwalker7/status/1520501312944807936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1019,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1739,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 577,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 29695330,
+			"id_str": "29695330",
+			"name": "Shaun Walker",
+			"screen_name": "shaunwalker7",
+			"location": "Budapest, Hungary",
+			"description": "Covering central/eastern Europe for The Guardian. Author of The Long Hangover: Putin's New Russia and the Ghosts of the Past.\nshaun.walker@theguardian.com",
+			"url": "https://t.co/eBkmdCCl6X",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/eBkmdCCl6X",
+							"expanded_url": "https://global.oup.com/academic/product/the-long-hangover-9780190659240?cc=us&lang=en&#",
+							"display_url": "global.oup.com/academic/produ…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 210141,
+			"friends_count": 2933,
+			"listed_count": 5807,
+			"created_at": "Wed Apr 08 11:33:26 +0000 2009",
+			"favourites_count": 19043,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 24891,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/969182103177965569/AcFFjx7h_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/969182103177965569/AcFFjx7h_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/29695330/1438443207",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 181,
+		"favorite_count": 985,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Russia tv goes after the foggy chip-and-fish munching marshlanders \n\n(via @francis_scarr) https://t.co/vr2RmEUz1Y"
+	}
+]
