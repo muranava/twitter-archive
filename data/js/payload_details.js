ac0719dfc0c14380a3e06ef9995c213e495407b1
muranava <muraleeatjena@yahoo.co.uk>
2022-05-12 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20693,
-	"created_at": "Wed May 11 01:14:35 +0000 2022",
+	"tweets": 20694,
+	"created_at": "Thu May 12 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Thu May 12 00:39:45 +0000 2022",
+		"id": 1524549859457609700,
+		"id_str": "1524549859457609729",
+		"full_text": "@timnitGebru predictable (pun intended) stuff, from Artificial Whiteness: https://t.co/fEKbwNWEao",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			73
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "timnitGebru",
+					"name": "Timnit Gebru",
+					"id": 359831209,
+					"id_str": "359831209",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1524549813739659300,
+					"id_str": "1524549813739659264",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/FShJ-mpXIAA6NVT.png",
+					"media_url_https": "https://pbs.twimg.com/media/FShJ-mpXIAA6NVT.png",
+					"url": "https://t.co/fEKbwNWEao",
+					"display_url": "pic.twitter.com/fEKbwNWEao",
+					"expanded_url": "https://twitter.com/muranava/status/1524549859457609729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 735,
+							"h": 187,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 735,
+							"h": 187,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 173,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1524549813739659300,
+					"id_str": "1524549813739659264",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/FShJ-mpXIAA6NVT.png",
+					"media_url_https": "https://pbs.twimg.com/media/FShJ-mpXIAA6NVT.png",
+					"url": "https://t.co/fEKbwNWEao",
+					"display_url": "pic.twitter.com/fEKbwNWEao",
+					"expanded_url": "https://twitter.com/muranava/status/1524549859457609729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 735,
+							"h": 187,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 735,
+							"h": 187,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 173,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1524510150962294800,
+		"in_reply_to_status_id_str": "1524510150962294784",
+		"in_reply_to_user_id": 359831209,
+		"in_reply_to_user_id_str": "359831209",
+		"in_reply_to_screen_name": "timnitGebru",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/8AwAmEhKna   Quick Cups of COCA ebook:https://t.co/QCudhNHZcw…",
+			"url": "https://t.co/mMLqfjUvXb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mMLqfjUvXb",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/8AwAmEhKna",
+							"expanded_url": "http://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/QCudhNHZcw",
+							"expanded_url": "http://smashwords.com/books/view/618",
+							"display_url": "smashwords.com/books/view/618",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2112,
+			"friends_count": 2602,
+			"listed_count": 100,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 20086,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1192,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1419958763172282377/IOhC2pAI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1419958763172282377/IOhC2pAI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1644829779",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@timnitGebru predictable (pun intended) stuff, from Artificial Whiteness: https://t.co/fEKbwNWEao"
+	},
+	{
 		"created_at": "Tue May 10 18:07:44 +0000 2022",
 		"id": 1523654922067189800,
 		"id_str": "1524088817585692672",
