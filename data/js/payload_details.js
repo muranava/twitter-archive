ee1873e472cb3769dc7ff2063da228047415e570
muranava <muraleeatjena@yahoo.co.uk>
2022-05-25 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20708,
-	"created_at": "Tue May 24 01:14:35 +0000 2022",
+	"tweets": 20709,
+	"created_at": "Wed May 25 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 24 11:03:31 +0000 2022",
+		"id": 1528613175125581800,
+		"id_str": "1529055486141681664",
+		"full_text": "Interview with Geoff Jordan Part I https://t.co/TmAKCTT5ul",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			58
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TmAKCTT5ul",
+					"expanded_url": "https://www.teachersasworkers.org/interview-with-geoff-jordan-part-i/",
+					"display_url": "teachersasworkers.org/interview-with…",
+					"indices": [
+						35,
+						58
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3152637711,
+			"id_str": "3152637711",
+			"name": "teachers_as_workers",
+			"screen_name": "taw_sig",
+			"location": "everywhere",
+			"description": "The mind is not a vessel that needs filling, but wood that needs igniting.(Plutarch)",
+			"url": "https://t.co/m5fCLqrXgN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/m5fCLqrXgN",
+							"expanded_url": "http://www.teachersasworkers.org",
+							"display_url": "teachersasworkers.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1799,
+			"friends_count": 1069,
+			"listed_count": 24,
+			"created_at": "Thu Apr 09 20:23:00 +0000 2015",
+			"favourites_count": 258,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1548,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/586464982428069888/7tz5UtJi_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/586464982428069888/7tz5UtJi_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3152637711/1428611383",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Interview with Geoff Jordan Part I https://t.co/TmAKCTT5ul"
+	},
+	{
 		"created_at": "Mon May 23 09:37:01 +0000 2022",
 		"id": 1528671331000193000,
 		"id_str": "1528671331000193027",
