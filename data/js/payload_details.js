0d9db105db98fcbc78d47fb8306e9ce5daa15d7c
muranava <muraleeatjena@yahoo.co.uk>
2022-06-12 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20724,
-	"created_at": "Fri Jun 10 01:14:35 +0000 2022",
+	"tweets": 20725,
+	"created_at": "Sun Jun 12 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sat Jun 11 19:44:17 +0000 2022",
+		"id": 1535680342899794000,
+		"id_str": "1535709525524860928",
+		"full_text": "Today Peckham withdrew consent. The community stood together against brutal Police immigration raids.\nEventually the man being detained was released &amp; the Police had to do the walk of shame out of Peckham. Huge well done to all who mobilised to protect this man.\n#WithdrawConsent https://t.co/uC6EnOiypE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WithdrawConsent",
+					"indices": [
+						267,
+						283
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1535676249112289300,
+					"id_str": "1535676249112289280",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1535676249112289280/pu/img/QDgStUQ0QLaDo0ay.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1535676249112289280/pu/img/QDgStUQ0QLaDo0ay.jpg",
+					"url": "https://t.co/uC6EnOiypE",
+					"display_url": "pic.twitter.com/uC6EnOiypE",
+					"expanded_url": "https://twitter.com/OWC2022/status/1535680342899793922/video/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1535676249112289300,
+					"id_str": "1535676249112289280",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1535676249112289280/pu/img/QDgStUQ0QLaDo0ay.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1535676249112289280/pu/img/QDgStUQ0QLaDo0ay.jpg",
+					"url": "https://t.co/uC6EnOiypE",
+					"display_url": "pic.twitter.com/uC6EnOiypE",
+					"expanded_url": "https://twitter.com/OWC2022/status/1535680342899793922/video/1",
+					"type": "video",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 138199,
+						"variants": [
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1535676249112289280/pu/vid/320x320/SffbQqW_-zAjkk8l.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1535676249112289280/pu/pl/NPWfMe6-Tmgq_oy1.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1535676249112289280/pu/vid/540x540/nC2qTRNfDsCU44Wv.mp4?tag=12"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1535676249112289280/pu/vid/640x640/l-UNgyZZewlNvZrl.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1511053235573952500,
+			"id_str": "1511053235573952521",
+			"name": "Operation Withdraw Consent",
+			"screen_name": "OWC2022",
+			"location": "United Kingdom",
+			"description": "If you would like to support our call to withdraw consent to the current model of policing in the UK then follow.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 686,
+			"friends_count": 88,
+			"listed_count": 1,
+			"created_at": "Mon Apr 04 18:49:14 +0000 2022",
+			"favourites_count": 420,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 346,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1514313064635678725/pw3kSITO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1514313064635678725/pw3kSITO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1511053235573952521/1649875373",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 94,
+		"favorite_count": 274,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today Peckham withdrew consent. The community stood together against brutal Police immigration raids.\nEventually the man being detained was released &amp; the Police had to do the walk of shame out of Peckham. Huge well done to all who mobilised to protect this man.\n#WithdrawConsent https://t.co/uC6EnOiypE"
+	},
+	{
 		"created_at": "Thu Jun 09 11:04:02 +0000 2022",
 		"id": 1534853133268144000,
 		"id_str": "1534853823969341441",
