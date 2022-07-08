d682666a7649e603dee6532fd143d6e423467d46
muranava <muraleeatjena@yahoo.co.uk>
2022-07-08 03:14:35 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20752,
-	"created_at": "Thu Jul 07 01:14:35 +0000 2022",
+	"tweets": 20754,
+	"created_at": "Fri Jul 08 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,330 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Thu Jul 07 17:58:21 +0000 2022",
+		"id": 1545096247425605600,
+		"id_str": "1545104951378165760",
+		"full_text": "Police at the face recognition surveillance deployment in central London just wrongfully arrested a young Black woman after an obvious misidentification. The woman they ‘matched’ her with was significantly older &amp; didn’t look similar at all. but they still arrested her. https://t.co/vVtbN2w9hp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1545094610933915600,
+					"id_str": "1545094610933915650",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FXFHXMUWIAIQO9H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXFHXMUWIAIQO9H.jpg",
+					"url": "https://t.co/vVtbN2w9hp",
+					"display_url": "pic.twitter.com/vVtbN2w9hp",
+					"expanded_url": "https://twitter.com/g__ferris/status/1545096247425605634/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 1079,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 1079,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 473,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1545094610933915600,
+					"id_str": "1545094610933915650",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FXFHXMUWIAIQO9H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXFHXMUWIAIQO9H.jpg",
+					"url": "https://t.co/vVtbN2w9hp",
+					"display_url": "pic.twitter.com/vVtbN2w9hp",
+					"expanded_url": "https://twitter.com/g__ferris/status/1545096247425605634/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 1079,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 1079,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 473,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1545094611890225200,
+					"id_str": "1545094611890225155",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FXFHXP4WQAMDBCr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXFHXP4WQAMDBCr.jpg",
+					"url": "https://t.co/vVtbN2w9hp",
+					"display_url": "pic.twitter.com/vVtbN2w9hp",
+					"expanded_url": "https://twitter.com/g__ferris/status/1545096247425605634/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1126,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 638,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1921,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 545060251,
+			"id_str": "545060251",
+			"name": "Griff Ferris",
+			"screen_name": "g__ferris",
+			"location": "¯\\_(ツ)_/¯",
+			"description": "justice, abolition, state abuse of data + tech // law @fairtrials // @blkprotestlegal @sdetsup // member @ldnrentersunion @lswunited // views my own. (he/him)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3523,
+			"friends_count": 1829,
+			"listed_count": 23,
+			"created_at": "Wed Apr 04 11:40:44 +0000 2012",
+			"favourites_count": 57754,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3381,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1453735551958355988/F6-E1nY2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1453735551958355988/F6-E1nY2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/545060251/1638373834",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 488,
+		"favorite_count": 821,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Police at the face recognition surveillance deployment in central London just wrongfully arrested a young Black woman after an obvious misidentification. The woman they ‘matched’ her with was significantly older &amp; didn’t look similar at all. but they still arrested her. https://t.co/vVtbN2w9hp"
+	},
+	{
+		"created_at": "Thu Jul 07 11:19:13 +0000 2022",
+		"id": 1545004504705372200,
+		"id_str": "1545004504705372165",
+		"full_text": "A split screen version of Ish, featuring a comedian &amp; a linguist https://t.co/jkRYvUWUI9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jkRYvUWUI9",
+					"expanded_url": "https://nextcloud.englishup.me/apps/sharingpath/muranext/Videos/Mix-Ish.webm",
+					"display_url": "nextcloud.englishup.me/apps/sharingpa…",
+					"indices": [
+						69,
+						92
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "The Shed:https://t.co/8AwAmEhKna   Quick Cups of COCA ebook:https://t.co/QCudhNHZcw…",
+			"url": "https://t.co/mMLqfjUvXb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mMLqfjUvXb",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/8AwAmEhKna",
+							"expanded_url": "http://eflnotes.wordpress.com/the-shed/",
+							"display_url": "eflnotes.wordpress.com/the-shed/",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/QCudhNHZcw",
+							"expanded_url": "http://smashwords.com/books/view/618",
+							"display_url": "smashwords.com/books/view/618",
+							"indices": [
+								60,
+								83
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2114,
+			"friends_count": 2624,
+			"listed_count": 98,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 20325,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1270,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537034675062972417/_29H68uG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537034675062972417/_29H68uG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1644829779",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A split screen version of Ish, featuring a comedian &amp; a linguist https://t.co/jkRYvUWUI9"
+	},
+	{
 		"created_at": "Wed Jul 06 20:10:45 +0000 2022",
 		"id": 1544775883369730000,
 		"id_str": "1544775883369730049",
