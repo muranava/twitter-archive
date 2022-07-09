f362fbd81ab2572c25fe2dc9dc9798264ddb49a8
muranava <muraleeatjena@yahoo.co.uk>
2022-07-09 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20754,
-	"created_at": "Fri Jul 08 01:14:35 +0000 2022",
+	"tweets": 20755,
+	"created_at": "Sat Jul 09 01:14:35 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 08 04:46:55 +0000 2022",
+		"id": 1545252025440542700,
+		"id_str": "1545268168305905669",
+		"full_text": "7 reasons why students don't talk in breakout rooms - https://t.co/i8m6W1ntOs I'd love to hear other reasons teachers think students don't talk.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/i8m6W1ntOs",
+					"expanded_url": "https://www.hongkongtesol.com/blog/mastering-breakout-rooms-7-common-mistakes-and-how-fix-them",
+					"display_url": "hongkongtesol.com/blog/mastering…",
+					"indices": [
+						54,
+						77
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3153403829,
+			"id_str": "3153403829",
+			"name": "Ross Thorburn",
+			"screen_name": "ross_thorburn",
+			"location": "Shanghai, China",
+			"description": "ELT trainer, manager and podcaster living in China",
+			"url": "https://t.co/qss1OhMkTB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qss1OhMkTB",
+							"expanded_url": "http://www.tefltraininginstitute.com",
+							"display_url": "tefltraininginstitute.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1069,
+			"friends_count": 731,
+			"listed_count": 8,
+			"created_at": "Fri Apr 10 10:08:09 +0000 2015",
+			"favourites_count": 398,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 501,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1430692454408200196/XV3ZYnwz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1430692454408200196/XV3ZYnwz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3153403829/1602471646",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 29,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "7 reasons why students don't talk in breakout rooms - https://t.co/i8m6W1ntOs I'd love to hear other reasons teachers think students don't talk."
+	},
+	{
 		"created_at": "Thu Jul 07 17:58:21 +0000 2022",
 		"id": 1545096247425605600,
 		"id_str": "1545104951378165760",
