50d94d1dabce4a4d9652ead22913819ae26d0140
muranava <muraleeatjena@yahoo.co.uk>
2022-09-11 03:14:41 +0200

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Sat Sep 10 18:19:36 +0000 2022",
+		"id": 1568597755408392200,
+		"id_str": "1568665508005953538",
+		"full_text": "A few of us wrote a letter of support for Uju Anya who has been targeted for her criticism of the Queen’s complicity in colonial atrocities. I hope that applied linguists who have benefited from her work will show their solidarity for @UjuAnya by signing.\n\nhttps://t.co/SU4W6bkPmO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UjuAnya",
+					"name": "Uju Anya",
+					"id": 2284718570,
+					"id_str": "2284718570",
+					"indices": [
+						235,
+						243
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/SU4W6bkPmO",
+					"expanded_url": "https://docs.google.com/forms/d/e/1FAIpQLScUF27p5rQz5TCHg4CaIeqClaPGvjG7Dk8WXxfAQTcJyKRgZQ/viewform",
+					"display_url": "docs.google.com/forms/d/e/1FAI…",
+					"indices": [
+						257,
+						280
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 248343882,
+			"id_str": "248343882",
+			"name": "Nelson Flores",
+			"screen_name": "nelsonlflores",
+			"location": "Philadelphia, PA",
+			"description": "Associate Professor in Educational Linguistics @PennGSE researching race, language & bilingual education; he/el; https://t.co/qVts43XBTK",
+			"url": "https://t.co/HZVBFeAchn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/HZVBFeAchn",
+							"expanded_url": "http://educationallinguist.wordpress.com",
+							"display_url": "educationallinguist.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/qVts43XBTK",
+							"expanded_url": "https://upenn.academia.edu/NelsonFlores",
+							"display_url": "upenn.academia.edu/NelsonFlores",
+							"indices": [
+								113,
+								136
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 23404,
+			"friends_count": 1086,
+			"listed_count": 217,
+			"created_at": "Sun Feb 06 20:09:17 +0000 2011",
+			"favourites_count": 13583,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25817,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1468752679061147650/n8_TMn9f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1468752679061147650/n8_TMn9f_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 264,
+		"favorite_count": 629,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A few of us wrote a letter of support for Uju Anya who has been targeted for her criticism of the Queen’s complicity in colonial atrocities. I hope that applied linguists who have benefited from her work will show their solidarity for @UjuAnya by signing.\n\nhttps://t.co/SU4W6bkPmO"
+	},
+	{
 		"created_at": "Fri Sep 09 07:47:42 +0000 2022",
 		"id": 1568122382749155300,
 		"id_str": "1568144097491181569",
