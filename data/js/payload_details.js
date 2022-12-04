4ca968fb36b687de7c008feff78c7369f4d40980
muranava <muraleeatjena@yahoo.co.uk>
2022-12-04 02:14:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20868,
-	"created_at": "Wed Nov 30 01:14:35 +0000 2022",
+	"tweets": 20869,
+	"created_at": "Sun Dec 04 01:14:35 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_12.js
@@ -0,0 +1,319 @@
+Grailbird.data.tweets_2022_12 = 
+[
+	{
+		"created_at": "Sat Dec 03 09:58:42 +0000 2022",
+		"id": 1598968358153908200,
+		"id_str": "1598980034210824192",
+		"full_text": "We are all getting the fact that these interminable conversations are tools of the racial regime also I hope? https://t.co/LowiAoKNtz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LowiAoKNtz",
+					"expanded_url": "https://twitter.com/i_ammukhtar/status/1598413652587155456",
+					"display_url": "twitter.com/i_ammukhtar/st…",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 22150544,
+			"id_str": "22150544",
+			"name": "alana lentin",
+			"screen_name": "alanalentin",
+			"location": "Gadigal land",
+			"description": "'Hideous Jew'. she/her. Link 🌳 https://t.co/MVXjNgdOZw",
+			"url": "https://t.co/2rmmGX4eiv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2rmmGX4eiv",
+							"expanded_url": "http://www.alanalentin.net",
+							"display_url": "alanalentin.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/MVXjNgdOZw",
+							"expanded_url": "http://linktr.ee/AlanaLentin",
+							"display_url": "linktr.ee/AlanaLentin",
+							"indices": [
+								31,
+								54
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10051,
+			"friends_count": 5540,
+			"listed_count": 109,
+			"created_at": "Fri Feb 27 15:30:13 +0000 2009",
+			"favourites_count": 53795,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 70591,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5E10A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1573513640547737600/iYLmwr6G_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1573513640547737600/iYLmwr6G_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/22150544/1646373070",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "3D1957",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1598413652587155500,
+		"quoted_status_id_str": "1598413652587155456",
+		"quoted_status_permalink": {
+			"url": "https://t.co/LowiAoKNtz",
+			"expanded": "https://twitter.com/i_ammukhtar/status/1598413652587155456",
+			"display": "twitter.com/i_ammukhtar/st…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Dec 01 20:28:06 +0000 2022",
+			"id": 1598413652587155500,
+			"id_str": "1598413652587155456",
+			"full_text": "\"Colourful people\" \n\nThis is the guy they wheeled out to defend Susan Hussey. https://t.co/JsrbKXJHSY",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				77
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1598413616637743000,
+						"id_str": "1598413616637743104",
+						"indices": [
+							78,
+							101
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1598413616637743104/pu/img/Tj0RA_TU2yFs8gIV.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1598413616637743104/pu/img/Tj0RA_TU2yFs8gIV.jpg",
+						"url": "https://t.co/JsrbKXJHSY",
+						"display_url": "pic.twitter.com/JsrbKXJHSY",
+						"expanded_url": "https://twitter.com/I_amMukhtar/status/1598413652587155456/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 369,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 586,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 586,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1598413616637743000,
+						"id_str": "1598413616637743104",
+						"indices": [
+							78,
+							101
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1598413616637743104/pu/img/Tj0RA_TU2yFs8gIV.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1598413616637743104/pu/img/Tj0RA_TU2yFs8gIV.jpg",
+						"url": "https://t.co/JsrbKXJHSY",
+						"display_url": "pic.twitter.com/JsrbKXJHSY",
+						"expanded_url": "https://twitter.com/I_amMukhtar/status/1598413652587155456/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 369,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 586,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 586,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								540,
+								293
+							],
+							"duration_millis": 43786,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1598413616637743104/pu/vid/496x270/_epcrFtz_nf0MoPO.mp4?tag=12"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1598413616637743104/pu/pl/b-5ZmgylVXB0b4fp.m3u8?tag=12&container=fmp4"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1598413616637743104/pu/vid/662x360/rgrBkxAxmrjU4zyX.mp4?tag=12"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1598413616637743104/pu/vid/1080x586/Y-8D7SRi4VVrV8U9.mp4?tag=12"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1238712829798203400,
+				"id_str": "1238712829798203393",
+				"name": "Mukhtar",
+				"screen_name": "I_amMukhtar",
+				"location": "England",
+				"description": "",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16380,
+				"friends_count": 8267,
+				"listed_count": 18,
+				"created_at": "Sat Mar 14 06:25:55 +0000 2020",
+				"favourites_count": 93806,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 40095,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1573001868543926272/bKO-5iv6_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1573001868543926272/bKO-5iv6_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1238712829798203393/1621072329",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 353,
+			"favorite_count": 1320,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 5,
+		"favorite_count": 16,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We are all getting the fact that these interminable conversations are tools of the racial regime also I hope? https://t.co/LowiAoKNtz"
+	}
+]
