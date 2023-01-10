318b05afcf02af339d16db59fbdffe979286def4
muranava <muraleeatjena@yahoo.co.uk>
2023-01-10 02:14:36 +0100

    Added 1 tweets

--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,257 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Mon Jan 09 12:28:39 +0000 2023",
+		"id": 1612422044469059600,
+		"id_str": "1612426120892239875",
+		"full_text": "A @BritishCouncil teacher assisting #AfghanWomen in the classroom. Our teachers enabled learners across 🇦🇫, but when the Taliban took power we left all the teachers behind. Let's get them to safety. #AtRiskTeachers @FCDOGovUK @ukhomeoffice @SuellaBraverman @Mark_Logan_MP https://t.co/BNKHhTm2sO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AfghanWomen",
+					"indices": [
+						36,
+						48
+					]
+				},
+				{
+					"text": "AtRiskTeachers",
+					"indices": [
+						199,
+						214
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BritishCouncil",
+					"name": "British Council",
+					"id": 14732889,
+					"id_str": "14732889",
+					"indices": [
+						2,
+						17
+					]
+				},
+				{
+					"screen_name": "FCDOGovUK",
+					"name": "Foreign, Commonwealth & Development Office",
+					"id": 14353202,
+					"id_str": "14353202",
+					"indices": [
+						215,
+						225
+					]
+				},
+				{
+					"screen_name": "ukhomeoffice",
+					"name": "Home Office",
+					"id": 138037459,
+					"id_str": "138037459",
+					"indices": [
+						226,
+						239
+					]
+				},
+				{
+					"screen_name": "SuellaBraverman",
+					"name": "Suella Braverman MP",
+					"id": 1722829178,
+					"id_str": "1722829178",
+					"indices": [
+						240,
+						256
+					]
+				},
+				{
+					"screen_name": "Mark_Logan_MP",
+					"name": "Mark Logan MP",
+					"id": 1432840233578647600,
+					"id_str": "1432840233578647555",
+					"indices": [
+						257,
+						271
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1612422042501976000,
+					"id_str": "1612422042501976064",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FmB5Tf9XEAA-1FF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmB5Tf9XEAA-1FF.jpg",
+					"url": "https://t.co/BNKHhTm2sO",
+					"display_url": "pic.twitter.com/BNKHhTm2sO",
+					"expanded_url": "https://twitter.com/AtRiskTeachers/status/1612422044469059585/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1008,
+							"h": 756,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1008,
+							"h": 756,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1612422042501976000,
+					"id_str": "1612422042501976064",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FmB5Tf9XEAA-1FF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmB5Tf9XEAA-1FF.jpg",
+					"url": "https://t.co/BNKHhTm2sO",
+					"display_url": "pic.twitter.com/BNKHhTm2sO",
+					"expanded_url": "https://twitter.com/AtRiskTeachers/status/1612422044469059585/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1008,
+							"h": 756,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1008,
+							"h": 756,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1493996938647244800,
+			"id_str": "1493996938647244800",
+			"name": "Joe Seaton",
+			"screen_name": "AtRiskTeachers",
+			"location": "",
+			"description": "Former English Manager & Deputy Director at British Council Afghanistan 2016 - 2020\n\nhttps://t.co/XYhp2h2yKo",
+			"url": "https://t.co/qghBlDAVHB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qghBlDAVHB",
+							"expanded_url": "https://www.atriskteachers.co.uk",
+							"display_url": "atriskteachers.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/XYhp2h2yKo",
+							"expanded_url": "http://atriskteachers.co.uk",
+							"display_url": "atriskteachers.co.uk",
+							"indices": [
+								85,
+								108
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 724,
+			"friends_count": 382,
+			"listed_count": 2,
+			"created_at": "Wed Feb 16 17:13:37 +0000 2022",
+			"favourites_count": 7355,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 410,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1541814447316078592/ZOt4Iz1v_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1541814447316078592/ZOt4Iz1v_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1493996938647244800/1645358520",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 42,
+		"favorite_count": 55,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A @BritishCouncil teacher assisting #AfghanWomen in the classroom. Our teachers enabled learners across 🇦🇫, but when the Taliban took power we left all the teachers behind. Let's get them to safety. #AtRiskTeachers @FCDOGovUK @ukhomeoffice @SuellaBraverman @Mark_Logan_MP https://t.co/BNKHhTm2sO"
+	},
+	{
 		"created_at": "Sat Jan 07 23:20:05 +0000 2023",
 		"id": 1611863079050006500,
 		"id_str": "1611865285568638977",
