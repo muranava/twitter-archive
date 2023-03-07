393ecd87e408a77a7958f03faffc10485a315868
muranava <muraleeatjena@yahoo.co.uk>
2023-03-07 02:14:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20911,
-	"created_at": "Wed Feb 22 01:14:35 +0000 2023",
+	"tweets": 20912,
+	"created_at": "Tue Mar 07 01:14:35 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_03.js
@@ -0,0 +1,130 @@
+Grailbird.data.tweets_2023_03 = 
+[
+	{
+		"created_at": "Mon Mar 06 12:54:40 +0000 2023",
+		"id": 1632726388523368400,
+		"id_str": "1632726388523368455",
+		"full_text": "@basche42 @ianpaulwright @cloudquistador ah another of my faves  : )",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "basche42",
+					"name": "Ben Basche",
+					"id": 865427900,
+					"id_str": "865427900",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "ianpaulwright",
+					"name": "Ian Wright",
+					"id": 308596147,
+					"id_str": "308596147",
+					"indices": [
+						10,
+						24
+					]
+				},
+				{
+					"screen_name": "cloudquistador",
+					"name": "Dwayne Monroe / @dwaynemonroe@mastodon.online",
+					"id": 722810851204272100,
+					"id_str": "722810851204272128",
+					"indices": [
+						25,
+						40
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1632725982342774800,
+		"in_reply_to_status_id_str": "1632725982342774787",
+		"in_reply_to_user_id": 865427900,
+		"in_reply_to_user_id_str": "865427900",
+		"in_reply_to_screen_name": "basche42",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "@muranava@mastodon.social",
+			"url": "https://t.co/mMLqfjUvXb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mMLqfjUvXb",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2113,
+			"friends_count": 2670,
+			"listed_count": 96,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 21021,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 996,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1571418650606813184/ptIm4v-b_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1571418650606813184/ptIm4v-b_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1644829779",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@basche42 @ianpaulwright @cloudquistador ah another of my faves  : )"
+	}
+]
