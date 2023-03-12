eec29400ab2d21220db9f3212b6d8ff785e7d6d7
muranava <muraleeatjena@yahoo.co.uk>
2023-03-12 02:14:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20913,
-	"created_at": "Sat Mar 11 01:14:35 +0000 2023",
+	"tweets": 20914,
+	"created_at": "Sun Mar 12 01:14:35 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,441 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sat Mar 11 11:08:04 +0000 2023",
+		"id": 1634323096889970700,
+		"id_str": "1634511503318556672",
+		"full_text": "reminded of the lengths some BBC employees went to to oppose the corporation's collaboration with the rise of Oswald Mosley. https://t.co/fAQwjhmBEq https://t.co/lafUroZp4f",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fAQwjhmBEq",
+					"expanded_url": "https://twitter.com/Sisyphusa/status/1052609817951567873?t=QrPw7Ss3MGrA9IRhpD4CRw&s=19",
+					"display_url": "twitter.com/Sisyphusa/stat…",
+					"indices": [
+						125,
+						148
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1634323093853225000,
+					"id_str": "1634323093853224960",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/media/Fq5IMeFWwAAD8_y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fq5IMeFWwAAD8_y.jpg",
+					"url": "https://t.co/lafUroZp4f",
+					"display_url": "pic.twitter.com/lafUroZp4f",
+					"expanded_url": "https://twitter.com/Sisyphusa/status/1634323096889970691/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 996,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1634323093853225000,
+					"id_str": "1634323093853224960",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/media/Fq5IMeFWwAAD8_y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fq5IMeFWwAAD8_y.jpg",
+					"url": "https://t.co/lafUroZp4f",
+					"display_url": "pic.twitter.com/lafUroZp4f",
+					"expanded_url": "https://twitter.com/Sisyphusa/status/1634323096889970691/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 996,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1634320729217925000,
+		"in_reply_to_status_id_str": "1634320729217925122",
+		"in_reply_to_user_id": 387088533,
+		"in_reply_to_user_id_str": "387088533",
+		"in_reply_to_screen_name": "Sisyphusa",
+		"user": {
+			"id": 387088533,
+			"id_str": "387088533",
+			"name": "michael richmond",
+			"screen_name": "Sisyphusa",
+			"location": "London, UK",
+			"description": "Some kind of communist. Co-author of Fractured: Race, Class, Gender & the Hatred of Identity Politics, published by Pluto Press. ⬇️ Part of @56aInfoshop.",
+			"url": "https://t.co/qMHTT0rTK1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qMHTT0rTK1",
+							"expanded_url": "https://www.plutobooks.com/9780745346564/fractured/",
+							"display_url": "plutobooks.com/9780745346564/…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9019,
+			"friends_count": 998,
+			"listed_count": 99,
+			"created_at": "Sat Oct 08 13:02:40 +0000 2011",
+			"favourites_count": 36985,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 47002,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000681724790/6f1f340adec043d39e2727434de225ae_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000681724790/6f1f340adec043d39e2727434de225ae_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/387088533/1509053815",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1052609817951567900,
+		"quoted_status_id_str": "1052609817951567873",
+		"quoted_status_permalink": {
+			"url": "https://t.co/fAQwjhmBEq",
+			"expanded": "https://twitter.com/Sisyphusa/status/1052609817951567873?t=QrPw7Ss3MGrA9IRhpD4CRw&s=19",
+			"display": "twitter.com/Sisyphusa/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Oct 17 17:18:31 +0000 2018",
+			"id": 1052609817951567900,
+			"id_str": "1052609817951567873",
+			"full_text": "Did some more digging here, amazing stuff. Specifically about the role of the media in facilitating the rise of British fascism in the 1930s and the wild lengths antifascist proletarians went to in order to spread the word about an anti-Mosley mobilisation in Hyde Park. https://t.co/Eitj0ggQ5Y",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				270
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1052609606239887400,
+						"id_str": "1052609606239887360",
+						"indices": [
+							271,
+							294
+						],
+						"media_url": "http://pbs.twimg.com/media/Dpue3CMXoAAHbcW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dpue3CMXoAAHbcW.jpg",
+						"url": "https://t.co/Eitj0ggQ5Y",
+						"display_url": "pic.twitter.com/Eitj0ggQ5Y",
+						"expanded_url": "https://twitter.com/Sisyphusa/status/1052609817951567873/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1052609606239887400,
+						"id_str": "1052609606239887360",
+						"indices": [
+							271,
+							294
+						],
+						"media_url": "http://pbs.twimg.com/media/Dpue3CMXoAAHbcW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dpue3CMXoAAHbcW.jpg",
+						"url": "https://t.co/Eitj0ggQ5Y",
+						"display_url": "pic.twitter.com/Eitj0ggQ5Y",
+						"expanded_url": "https://twitter.com/Sisyphusa/status/1052609817951567873/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1052609704130728000,
+						"id_str": "1052609704130727937",
+						"indices": [
+							271,
+							294
+						],
+						"media_url": "http://pbs.twimg.com/media/Dpue8u3XUAEqdgU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dpue8u3XUAEqdgU.jpg",
+						"url": "https://t.co/Eitj0ggQ5Y",
+						"display_url": "pic.twitter.com/Eitj0ggQ5Y",
+						"expanded_url": "https://twitter.com/Sisyphusa/status/1052609817951567873/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": 1050809475295309800,
+			"in_reply_to_status_id_str": "1050809475295309826",
+			"in_reply_to_user_id": 387088533,
+			"in_reply_to_user_id_str": "387088533",
+			"in_reply_to_screen_name": "Sisyphusa",
+			"user": {
+				"id": 387088533,
+				"id_str": "387088533",
+				"name": "michael richmond",
+				"screen_name": "Sisyphusa",
+				"location": "London, UK",
+				"description": "Some kind of communist. Co-author of Fractured: Race, Class, Gender & the Hatred of Identity Politics, published by Pluto Press. ⬇️ Part of @56aInfoshop.",
+				"url": "https://t.co/qMHTT0rTK1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/qMHTT0rTK1",
+								"expanded_url": "https://www.plutobooks.com/9780745346564/fractured/",
+								"display_url": "plutobooks.com/9780745346564/…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 9019,
+				"friends_count": 998,
+				"listed_count": 99,
+				"created_at": "Sat Oct 08 13:02:40 +0000 2011",
+				"favourites_count": 36985,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 47002,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/378800000681724790/6f1f340adec043d39e2727434de225ae_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000681724790/6f1f340adec043d39e2727434de225ae_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/387088533/1509053815",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "7f55e839e1715da2",
+				"url": "https://api.twitter.com/1.1/geo/id/7f55e839e1715da2.json",
+				"place_type": "city",
+				"name": "Camberwell",
+				"full_name": "Camberwell, London",
+				"country_code": "GB",
+				"country": "United Kingdom",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-0.1114755,
+								51.4194247
+							],
+							[
+								-0.0297308,
+								51.4194247
+							],
+							[
+								-0.0297308,
+								51.509947
+							],
+							[
+								-0.1114755,
+								51.509947
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 53,
+			"favorite_count": 87,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 33,
+		"favorite_count": 77,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "reminded of the lengths some BBC employees went to to oppose the corporation's collaboration with the rise of Oswald Mosley. https://t.co/fAQwjhmBEq https://t.co/lafUroZp4f"
+	},
+	{
 		"created_at": "Fri Mar 10 11:57:57 +0000 2023",
 		"id": 1634161668593787000,
 		"id_str": "1634161668593786880",
