b83d75fc2c3bc2148aa3d188ce4c97f44a88ecdd
muranava <muraleeatjena@yahoo.co.uk>
2023-03-23 02:14:36 +0100

    Added 2 tweets

--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Wed Mar 22 11:16:43 +0000 2023",
+		"id": 1638499945681502200,
+		"id_str": "1638499945681502209",
+		"full_text": "@tonythorne007 @DrDavidAEvans True that",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			39
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tonythorne007",
+					"name": "Tony Thorne",
+					"id": 762192800,
+					"id_str": "762192800",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "DrDavidAEvans",
+					"name": "Dave Evans",
+					"id": 999018747657678800,
+					"id_str": "999018747657678852",
+					"indices": [
+						15,
+						29
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1638492799224905700,
+		"in_reply_to_status_id_str": "1638492799224905730",
+		"in_reply_to_user_id": 762192800,
+		"in_reply_to_user_id_str": "762192800",
+		"in_reply_to_screen_name": "tonythorne007",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "@muranava@mastodon.social",
+			"url": "https://t.co/mMLqfjUvXb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mMLqfjUvXb",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2111,
+			"friends_count": 2669,
+			"listed_count": 96,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 21071,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1003,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1571418650606813184/ptIm4v-b_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1571418650606813184/ptIm4v-b_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1644829779",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@tonythorne007 @DrDavidAEvans True that"
+	},
+	{
+		"created_at": "Wed Mar 22 09:06:43 +0000 2023",
+		"id": 1638256583250440200,
+		"id_str": "1638467228487090177",
+		"full_text": "This explosive story - broken this morning by @declassifieduk’s chief reporter Phil Miller - is now leading news bulletins around the world. \n\nThe power of independent media.\nhttps://t.co/B4OoB7wSPO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			198
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "declassifiedUK",
+					"name": "Declassified UK",
+					"id": 1084523178444099600,
+					"id_str": "1084523178444099584",
+					"indices": [
+						46,
+						61
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/B4OoB7wSPO",
+					"expanded_url": "https://declassifieduk.org/britain-supplying-depleted-uranium-rounds-to-ukraine/",
+					"display_url": "declassifieduk.org/britain-supply…",
+					"indices": [
+						175,
+						198
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 591982810,
+			"id_str": "591982810",
+			"name": "Matt Kennard",
+			"screen_name": "kennardmatt",
+			"location": "7ea06980fa22e56c3ab41ef109a9f7",
+			"description": "co-founder/investigations @DeclassifiedUK. author of Irregular Army & The Racket. next book Silent Coup out in 2023.",
+			"url": "https://t.co/4iXJNyEk6e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4iXJNyEk6e",
+							"expanded_url": "https://declassifieduk.org/author/matt-kennard/",
+							"display_url": "declassifieduk.org/author/matt-ke…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 59639,
+			"friends_count": 2483,
+			"listed_count": 535,
+			"created_at": "Sun May 27 17:09:30 +0000 2012",
+			"favourites_count": 7591,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 9463,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1627704838170546181/yImELCMZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1627704838170546181/yImELCMZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/591982810/1623071851",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 843,
+		"favorite_count": 1354,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This explosive story - broken this morning by @declassifieduk’s chief reporter Phil Miller - is now leading news bulletins around the world. \n\nThe power of independent media.\nhttps://t.co/B4OoB7wSPO"
+	},
+	{
 		"created_at": "Wed Mar 15 21:07:59 +0000 2023",
 		"id": 1636112029403476000,
 		"id_str": "1636112029403475976",
