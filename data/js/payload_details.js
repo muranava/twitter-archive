9be3105f0ae06045a2d190dbdff25ec35e4921d5
muranava <muraleeatjena@yahoo.co.uk>
2023-03-25 02:14:37 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20919,
-	"created_at": "Fri Mar 24 01:14:35 +0000 2023",
+	"tweets": 20920,
+	"created_at": "Sat Mar 25 01:14:36 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Fri Mar 24 11:55:01 +0000 2023",
+		"id": 1639234361005408300,
+		"id_str": "1639234361005408256",
+		"full_text": "@nateffo @perdricof @whoisernie \"I'm not a Marxist\" why not?! :o\nAs I understand the author's liberalism says capitalism can be reformed? No it can't says Marxists, do u think capitalism can be reformed Nathan?",
+		"truncated": false,
+		"display_text_range": [
+			32,
+			210
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nateffo",
+					"name": "Nate",
+					"id": 2768346873,
+					"id_str": "2768346873",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "perdricof",
+					"name": "Dr. Samantha Hancox-Li",
+					"id": 789536590565511200,
+					"id_str": "789536590565511168",
+					"indices": [
+						9,
+						19
+					]
+				},
+				{
+					"screen_name": "whoisernie",
+					"name": "Ernie P",
+					"id": 295982203,
+					"id_str": "295982203",
+					"indices": [
+						20,
+						31
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1639176660615348200,
+		"in_reply_to_status_id_str": "1639176660615348224",
+		"in_reply_to_user_id": 2768346873,
+		"in_reply_to_user_id_str": "2768346873",
+		"in_reply_to_screen_name": "nateffo",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "@muranava@mastodon.social",
+			"url": "https://t.co/mMLqfjUvXb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mMLqfjUvXb",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2110,
+			"friends_count": 2670,
+			"listed_count": 96,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 21083,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1007,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1571418650606813184/ptIm4v-b_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1571418650606813184/ptIm4v-b_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1644829779",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@nateffo @perdricof @whoisernie \"I'm not a Marxist\" why not?! :o\nAs I understand the author's liberalism says capitalism can be reformed? No it can't says Marxists, do u think capitalism can be reformed Nathan?"
+	},
+	{
 		"created_at": "Thu Mar 23 21:58:08 +0000 2023",
 		"id": 1639023749214392300,
 		"id_str": "1639023749214392320",
