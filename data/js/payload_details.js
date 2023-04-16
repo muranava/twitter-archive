5f352ca137c34784265f3f005e02ce8935f7c5d4
muranava <muraleeatjena@yahoo.co.uk>
2023-04-16 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20934,
-	"created_at": "Sat Apr 15 01:14:35 +0000 2023",
+	"tweets": 20935,
+	"created_at": "Sun Apr 16 01:14:35 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Sat Apr 15 07:38:07 +0000 2023",
+		"id": 1647132740620263400,
+		"id_str": "1647142242987933698",
+		"full_text": "📢 Casework victory 📢\n\nWe supported a teacher at a big chain school whose DOS tried to discipline them - w/o reason and w/o process. \n\nAfter some quick advice to the worker on their rights, the manager swiftly decided to resolve the matter amicably - w/o disciplinary action!✊✊ https://t.co/1914gmsub9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1647132739299164200,
+					"id_str": "1647132739299164161",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FtvKf9_XwAEcJja.png",
+					"media_url_https": "https://pbs.twimg.com/media/FtvKf9_XwAEcJja.png",
+					"url": "https://t.co/1914gmsub9",
+					"display_url": "pic.twitter.com/1914gmsub9",
+					"expanded_url": "https://twitter.com/TeflUnion/status/1647132740620263424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 788,
+							"h": 773,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 788,
+							"h": 773,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1647132739299164200,
+					"id_str": "1647132739299164161",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FtvKf9_XwAEcJja.png",
+					"media_url_https": "https://pbs.twimg.com/media/FtvKf9_XwAEcJja.png",
+					"url": "https://t.co/1914gmsub9",
+					"display_url": "pic.twitter.com/1914gmsub9",
+					"expanded_url": "https://twitter.com/TeflUnion/status/1647132740620263424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 788,
+							"h": 773,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 788,
+							"h": 773,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1130578765472501800,
+			"id_str": "1130578765472501760",
+			"name": "TEFL Workers' Union",
+			"screen_name": "TeflUnion",
+			"location": "London",
+			"description": "We like our students. We like our jobs. We like our workmates. We don’t like struggling to pay the bills. We don’t like insecure employment. Join us!",
+			"url": "https://t.co/K82GNZ9byS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/K82GNZ9byS",
+							"expanded_url": "https://teflworkersunion.org",
+							"display_url": "teflworkersunion.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2505,
+			"friends_count": 275,
+			"listed_count": 19,
+			"created_at": "Mon May 20 20:59:17 +0000 2019",
+			"favourites_count": 2135,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4262,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1130586030803968003/c180EXxo_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1130578765472501760/1558387728",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 22,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "📢 Casework victory 📢\n\nWe supported a teacher at a big chain school whose DOS tried to discipline them - w/o reason and w/o process. \n\nAfter some quick advice to the worker on their rights, the manager swiftly decided to resolve the matter amicably - w/o disciplinary action!✊✊ https://t.co/1914gmsub9"
+	},
+	{
 		"created_at": "Fri Apr 14 08:58:44 +0000 2023",
 		"id": 1646798919509131300,
 		"id_str": "1646800140550717440",
