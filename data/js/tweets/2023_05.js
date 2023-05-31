bc1e3de0fe06a6fece56cd1a83a1bd7848d46fc1
muranava <muraleeatjena@yahoo.co.uk>
2023-05-31 03:14:36 +0200

    Added 1 tweets

--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Tue May 30 14:03:33 +0000 2023",
+		"id": 1663546691180699600,
+		"id_str": "1663546691180699649",
+		"full_text": "@DavidVeevers1 @will_pride15 learned a new word from Domenico Losurdo talking about David Irving that could apply - \"involution\" - \"How are we to explain this startling political &amp; intellectual involution\" War and Revolution. Rethinking the Twentieth Century 2015: 225",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DavidVeevers1",
+					"name": "David Veevers",
+					"id": 1242481795712442400,
+					"id_str": "1242481795712442368",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "will_pride15",
+					"name": "Will Pride",
+					"id": 2429091122,
+					"id_str": "2429091122",
+					"indices": [
+						15,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1663541439052271600,
+		"in_reply_to_status_id_str": "1663541439052271624",
+		"in_reply_to_user_id": 1242481795712442400,
+		"in_reply_to_user_id_str": "1242481795712442368",
+		"in_reply_to_screen_name": "DavidVeevers1",
+		"user": {
+			"id": 18602422,
+			"id_str": "18602422",
+			"name": "Mura Nava",
+			"screen_name": "muranava",
+			"location": "Paris, France",
+			"description": "@muranava@mastodon.social",
+			"url": "https://t.co/mMLqfjUvXb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mMLqfjUvXb",
+							"expanded_url": "http://eflnotes.wordpress.com/",
+							"display_url": "eflnotes.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2104,
+			"friends_count": 2681,
+			"listed_count": 97,
+			"created_at": "Sun Jan 04 14:17:23 +0000 2009",
+			"favourites_count": 21164,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1054,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1571418650606813184/ptIm4v-b_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1571418650606813184/ptIm4v-b_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18602422/1644829779",
+			"profile_link_color": "08780E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@DavidVeevers1 @will_pride15 learned a new word from Domenico Losurdo talking about David Irving that could apply - \"involution\" - \"How are we to explain this startling political &amp; intellectual involution\" War and Revolution. Rethinking the Twentieth Century 2015: 225"
+	},
+	{
 		"created_at": "Thu May 25 17:35:45 +0000 2023",
 		"id": 1661296166431248400,
 		"id_str": "1661788154318270480",
