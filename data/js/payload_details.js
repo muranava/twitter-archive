02eb8be78717c45239a814bb31a2f2955acd40e6
muranava <muraleeatjena@yahoo.co.uk>
2023-06-02 03:14:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20956,
-	"created_at": "Wed May 31 01:14:35 +0000 2023",
+	"tweets": 20957,
+	"created_at": "Fri Jun 02 01:14:35 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_06.js
@@ -0,0 +1,237 @@
+Grailbird.data.tweets_2023_06 = 
+[
+	{
+		"created_at": "Thu Jun 01 19:07:20 +0000 2023",
+		"id": 1664300781019992000,
+		"id_str": "1664347919221157893",
+		"full_text": "\"This place is like hell on Earth.\"\n\n@GuillermoGaldos and Rodrigo Galdos travelled one of the world's most dangerous migrant routes.\n\nThe Darién Gap is full of deadly obstacles, armed gangs and strewn with bodies but record numbers are risking the route in search of a new life. https://t.co/EFqgE0E6rD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GuillermoGaldos",
+					"name": "Guillermo Galdos",
+					"id": 2423981779,
+					"id_str": "2423981779",
+					"indices": [
+						37,
+						53
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1664275207228625000,
+					"id_str": "1664275207228624900",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FxiyV52aUAAjt2P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxiyV52aUAAjt2P.jpg",
+					"url": "https://t.co/EFqgE0E6rD",
+					"display_url": "pic.twitter.com/EFqgE0E6rD",
+					"expanded_url": "https://twitter.com/Channel4News/status/1664300781019992065/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1664275207228625000,
+					"id_str": "1664275207228624900",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FxiyV52aUAAjt2P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxiyV52aUAAjt2P.jpg",
+					"url": "https://t.co/EFqgE0E6rD",
+					"display_url": "pic.twitter.com/EFqgE0E6rD",
+					"expanded_url": "https://twitter.com/Channel4News/status/1664300781019992065/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 61840,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1664275207228624900/vid/540x540/hS9ZWMG4WE5LNVYz.mp4?tag=16"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1664275207228624900/vid/320x320/9zqkAuGCcoiiUPOw.mp4?tag=16"
+							},
+							{
+								"bitrate": 8768000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1664275207228624900/vid/1080x1080/r6JQdcABn4h__WeU.mp4?tag=16"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1664275207228624900/pl/rOopDbYqUgogy0U3.m3u8?tag=16&container=fmp4"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1664275207228624900/vid/720x720/UwgXNdLgzYteEsNA.mp4?tag=16"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14569869,
+			"id_str": "14569869",
+			"name": "Channel 4 News",
+			"screen_name": "Channel4News",
+			"location": "United Kingdom",
+			"description": "Stories that reveal and inspire, challenging expectations. \n\nWeekdays 7-8pm on Channel 4.\n\nSubscribe to our podcast: https://t.co/FSs8pWrXc5",
+			"url": "https://t.co/LBhZvqcoKL",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LBhZvqcoKL",
+							"expanded_url": "http://www.channel4.com/news",
+							"display_url": "channel4.com/news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/FSs8pWrXc5",
+							"expanded_url": "https://www.channel4.com/news/the-fourcast",
+							"display_url": "channel4.com/news/the-fourc…",
+							"indices": [
+								117,
+								140
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2470424,
+			"friends_count": 4376,
+			"listed_count": 10659,
+			"created_at": "Mon Apr 28 14:29:37 +0000 2008",
+			"favourites_count": 551,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 119586,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "2C1C36",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875404558528393216/cTknVhwm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875404558528393216/cTknVhwm_normal.jpg",
+			"profile_link_color": "421174",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FAF0F6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 193,
+		"favorite_count": 354,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"This place is like hell on Earth.\"\n\n@GuillermoGaldos and Rodrigo Galdos travelled one of the world's most dangerous migrant routes.\n\nThe Darién Gap is full of deadly obstacles, armed gangs and strewn with bodies but record numbers are risking the route in search of a new life. https://t.co/EFqgE0E6rD"
+	}
+]
